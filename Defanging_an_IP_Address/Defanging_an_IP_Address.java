384b2f7495dd91d7e02c34098ab80abc12a7867a
ErnestL1n <ernestlin1998@gmail.com>
2020-06-01 15:13:18 +0800

    Leetcode.1108

---
+++ Defanging_an_IP_Address/Defanging_an_IP_Address.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package github.com.ErnestL1n;
+
+/**
+ * @author https://github.com/ErnestL1n
+ *
+ */
+
+//Leetcode.1108
+public class Defanging_an_IP_Address {
+
+	/**
+	 * @param args
+	 */
+	public static String defangIPaddr(String address) {
+		StringBuilder str=new StringBuilder();
+		for(char c:address.toCharArray()) {
+			if(c!='.')str.append(c);
+			else str.append("[.]");
+		}
+		return str.toString();
+	}
+	public static void main(String[] args) {
+		String str=new String("255.100.50.0");
+		System.out.println("Input: address = "+str);
+		System.out.println("Output: "+defangIPaddr(str));
+
+	}
+
+}
