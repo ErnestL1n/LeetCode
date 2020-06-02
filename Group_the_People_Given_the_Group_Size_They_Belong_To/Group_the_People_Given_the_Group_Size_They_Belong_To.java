c8a311d1e79bed90ca9fc5264a5940a847a590fc
ErnestL1n <ernestlin1998@gmail.com>
2020-06-02 22:03:57 +0800

    Leetcode.1282

---
+++ Group_the_People_Given_the_Group_Size_They_Belong_To/Group_the_People_Given_the_Group_Size_They_Belong_To.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package github.com.ErnestL1n;
+import java.util.*;
+/**
+ * @author https://github.com/ErnestL1n
+ *
+ */
+
+//Leetcode.1282
+public class Group_the_People_Given_the_Group_Size_They_Belong_To {
+
+	/**
+	 * @param args
+	 */
+	public static List<List<Integer>> groupThePeople(int[] groupSizes){
+		int size=groupSizes.length;
+		HashMap<Integer,List<Integer>> map=new HashMap<>();
+		//to be returned
+		List<List<Integer>> answer=new ArrayList<>();
+		
+		for(int i=0;i<size;i++) {
+			int current=groupSizes[i];
+			
+			//[] for each group
+			List<Integer> temp=new ArrayList<>();
+			//get(current) => return List<Integer>,finding the set at temp[k]
+			if(map.containsKey(current))temp=map.get(current);
+			//add into new temp[k]
+			temp.add(i);
+			
+			//update map
+			map.put(current, temp);
+			if(temp.size()==current) {
+				answer.add(temp);
+				//remove key
+				map.remove(current);
+			}
+			
+		}
+		return answer;
+		
+	}
+	public static void main(String[] args) {
+		int[] groupSizes=new int[] {3,3,3,3,3,1,3};
+		System.out.println("Input: groupSizes = "+Arrays.toString(groupSizes));
+		System.out.println("Output is "+groupThePeople(groupSizes));
+
+	}
+
+}
