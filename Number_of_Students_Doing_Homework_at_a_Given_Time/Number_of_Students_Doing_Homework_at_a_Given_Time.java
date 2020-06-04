26e45b5f97e807518db138dc6f18e34542c6475c
ErnestL1n <ernestlin1998@gmail.com>
2020-06-04 22:52:57 +0800

    Leetcode.1450

---
+++ Number_of_Students_Doing_Homework_at_a_Given_Time/Number_of_Students_Doing_Homework_at_a_Given_Time.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package github.com.ErnestL1n;
+
+import java.util.Arrays;
+
+/**
+ * @author https://github.com/ErnestL1n
+ *
+ */
+
+//Leetcode.1450
+public class Number_of_Students_Doing_Homework_at_a_Given_Time {
+
+	/**
+	 * @param args
+	 */
+	 public static int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+	        int count=0;
+	        for(int i=0;i<startTime.length;i++)
+	            if(endTime[i]>=queryTime&&startTime[i]<=queryTime)count++;
+	        return count;
+	    }
+	public static void main(String[] args) {
+		int[] startTime=new int[]{1,2,3};
+		int[] endTime=new int[] {3,2,7};
+		int queryTime=4;
+		System.out.println("Given startTime array:"+Arrays.toString(startTime)+ ",endTime array:"+Arrays.toString(endTime));
+		System.out.println("Output is "+busyStudent(startTime,endTime,queryTime));
+	}
+}
