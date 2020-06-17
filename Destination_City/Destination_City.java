22bb74ec6c19e32c4e3715aab92c972622e7d848
ErnestL1n <ernestlin1998@gmail.com>
2020-06-17 20:33:15 +0800

    Leetcode.1436

---
+++ Destination_City/Destination_City.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package github.com.ErnestL1n;
+import java.util.*;
+/**
+ * @author https://github.com/ErnestL1n
+ *
+ */
+//Leetcode.1436
+public class Destination_City {
+
+	/**
+	 * @param args
+	 */
+	//this is 2ms solution published by https://leetcode.com/hobiter/
+	public static String destCity(List<List<String>> paths) {
+		HashMap<String,Boolean> map=new HashMap<>();
+		for(List<String> city:paths) {
+			//if city isn't destination, set its value as false, else,true
+			map.put(city.get(0),false);
+			if(!map.containsKey(city.get(1)))map.put(city.get(1),true);
+		}
+		for(String city:map.keySet())
+			if(map.get(city))return city;
+		return null;
+	}
+	public static void main(String[] args) {
+		String[] cities=new String[]{"London","New York","New York","Lima","Lima","Sao Paulo"};
+		List<List<String>> paths=new ArrayList<>();
+		for(int i=0;i<cities.length;i+=2)paths.add(Arrays.asList(cities[i],cities[i+1]));
+		System.out.println("City pairs are "+paths);
+		System.out.println("the destination city is "+destCity(paths));
+		
+		
+
+	}
+
+}
