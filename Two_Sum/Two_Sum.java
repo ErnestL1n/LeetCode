caaaf52605c98071a9ea00545f301feac0c60986
ErnestL1n <ernestlin1998@gmail.com>
2020-06-19 00:23:02 +0800

    Leetcode.1

--- Two_Sum/Two_Sum.java
+++ Two_Sum/Two_Sum.java
@@ -12,7 +12,7 @@ import java.util.*;
 //Its idea is putting into HashMap not at the first time,but at the time when it doesn't find in the HashMap
 public class Two_Sum {
 	//return indices
-	private static int[] Two_sum_bruteforce(int[] nums,int value) {
+	public static int[] Two_sum_bruteforce(int[] nums,int value) {
 		//naive approach O(n^2)
 		for(int i=0;i<nums.length;i++)
 			for(int j=i+1;j<nums.length;j++) {
@@ -23,24 +23,12 @@ public class Two_Sum {
 		return new int[] {};
 	}
 	
-	private static int[] Two_sum_OnePassHashmap(int nums[],int value) {
-		//One pass Hash Table:O(n)
-		Map<Integer,Integer> numMap=new HashMap<>();
-		for(int i=0;i<nums.length;i++) {
-			int complement=value-nums[i];
-			
-			//if complement exists
-			if(numMap.containsKey(complement))return new int[] {i,numMap.get(complement)};
-			else numMap.put(nums[i], i);
-		}
-		return new int[] {};
+
 	
-	}
-	
-	private static int[] Two_sum_TwoPassHashmap(int nums[],int target) {
+	public static int[] Two_sum_TwoPassHashmap(int nums[],int target) {
 		//Two pass Hash Table:O(n) a little faster than above one
-		//at first iteration,it must do into map
 		HashMap<Integer,Integer> map=new HashMap<>();
+		//at first iteration,it must do into map
 		map.put(nums[0],0);
 		for(int i=1;i<nums.length;i++) {
 			if(map.containsKey(target-nums[i]))
@@ -51,7 +39,10 @@ public class Two_Sum {
 	}
 	
 	public static void main(String[] args) {
-		
+		int[] nums=new int[] {2, 7, 11, 15 };
+		int target=9;
+		System.out.println("Given nums is "+Arrays.toString(nums)+" , target is "+target);
+		System.out.println("Indices of the two numbers such that they add up to a specific target are "+Arrays.toString(Two_sum_TwoPassHashmap(nums,target)));
 	}
 	
 }
