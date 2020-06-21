a477c581dfc7f86f2d0bda2d996a6b98d6a2f6ca
ErnestL1n <ernestlin1998@gmail.com>
2020-06-21 23:26:02 +0800

    Leetcode.1221

--- Split_a_String_in_Balanced_Strings/Split_a_String_in_Balanced_Strings.java
+++ Split_a_String_in_Balanced_Strings/Split_a_String_in_Balanced_Strings.java
@@ -16,24 +16,24 @@ public class Split_a_String_in_Balanced_Strings {
 	//constraint:
 	//1 <= s.length <= 1000
 	//s[i] = 'L' or 'R'
-	public static int balancedStringSplit(String s) {
-        int count=0,ln=0,rn=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='R')
-                rn++;
-            if(s.charAt(i)=='L')
-                ln++;
-            if(ln!=0&&rn==ln)
-                count++;
-        }
-        return count;
-    }
+	 public static int balancedStringSplit(String s) {
+	        int count=0,leftcount=0,rightcount=0;
+	        for(int i=0;i<s.length();i++){
+	            if(s.charAt(i)=='R')
+	                rightcount++;
+	            if(s.charAt(i)=='L')
+	                leftcount++;
+	            if(rightcount!=0&&rightcount==leftcount)
+	                count++;
+	        }
+	        return count;
+	    }
 	
 	
 	public static void main(String[] args) {
 		String s="RLRRRLLRLL";
-		System.out.println("Input: s = "+s);
-		System.out.println("Output: "+balancedStringSplit(s));
+		System.out.println("Given a balanced string : "+s+ " ,split it in the maximum amount of balanced strings");
+		System.out.println("the maximum amount of splitted balanced strings is "+balancedStringSplit(s));
 	}
 
 }
