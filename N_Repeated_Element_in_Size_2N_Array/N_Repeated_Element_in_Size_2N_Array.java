e645a25747c6d186849562453a9e8209fe7a130f
ErnestL1n <ernestlin1998@gmail.com>
2020-06-23 14:54:31 +0800

    Leetcode.961

--- N_Repeated_Element_in_Size_2N_Array/N_Repeated_Element_in_Size_2N_Array.java
+++ N_Repeated_Element_in_Size_2N_Array/N_Repeated_Element_in_Size_2N_Array.java
@@ -15,7 +15,14 @@ public class N_Repeated_Element_in_Size_2N_Array {
 	/**
 	 * @param args
 	 */
-    public static int repeatedNTimes(int[] A) {
+    public static int repeatedNTimesMethod1(int[] A) {
+        int[] count = new int[10000];
+        for (int a : A)
+            if (count[a]++ == 1)
+                return a;
+        return -1;
+    }
+    public static int repeatedNTimesMethod2(int[] A) {
         int i = 0, j = 0, n = A.length;
         while (i == j || A[i] != A[j]) {
             i = (int)(Math.random() * n);
@@ -27,7 +34,7 @@ public class N_Repeated_Element_in_Size_2N_Array {
 		int[] A=new int[] {1,2,3,3};
 		int sizeN=A.length/2;
 		System.out.println("2N size array is : "+Arrays.toString(A));
-		System.out.println("the element repeated N="+sizeN+" times is "+repeatedNTimes(A));
+		System.out.println("the element repeated N="+sizeN+" times is "+repeatedNTimesMethod1(A));
 
 	}
 
