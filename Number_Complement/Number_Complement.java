2f28b9cca29162458598be121e82ee457fa00ad5
ErnestL1n <ernestlin1998@gmail.com>
2020-06-27 22:30:44 +0800

    Leetcode.476

--- Number_Complement/Number_Complement.java
+++ Number_Complement/Number_Complement.java
@@ -16,6 +16,8 @@ public class Number_Complement {
 	 */
 	//The given integer num is guaranteed to fit within the range of a 32-bit signed integer.
 	//You could assume no leading zero bit in the integer¡¦s binary representation.
+	//1 ms,38.8 MB
+	
 	public static int complement(int num) {
 		
 		//count bits with total
@@ -30,15 +32,23 @@ public class Number_Complement {
 		
 	}
 	
+	//much faster approach
+	//0 ms,36 MB
+	
+	public static int complementQuick(int num) {
+		int mask=(Integer.highestOneBit(num)<<1)-1;
+		return mask^num;
+	}
+	
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Enter the number to get its complement");
+		System.out.println("Enter the number to get its binary complement");
 		Scanner keyboard=new Scanner(System.in);
 		int N=keyboard.nextInt();
-		int comp=complement(N);
+		int comp=complementQuick(N);
 		System.out.println("The binary representation of "+N+" is "
-				+ Integer.toBinaryString(N) +" (no leading zero bits), and its complement is (Leading zeros)"
+				+ Integer.toBinaryString(N) +" (no leading zero bits), and its binary complement is 0"
 				+Integer.toBinaryString(comp)+" which is "+comp);
 	}
 
