bf26257dc7889c04e59f1ff59c1d15314b5e4c7b
ErnestL1n <ernestlin1998@gmail.com>
2020-06-28 13:03:51 +0800

    Leetcode.1365

--- Smaller_Than_the_Current_Number/Smaller_Than_the_Current_Number.java
+++ Smaller_Than_the_Current_Number/Smaller_Than_the_Current_Number.java
@@ -26,22 +26,45 @@ public class Smaller_Than_the_Current_Number {
         for(int i=0;i<nums.length;i++)temp[i]=count[nums[i]];
         return temp;
     }
+	
+	
+	//using HashMap
+	public static int[] smallerNumbersThanCurrentHash(int[] nums) {
+		HashMap<Integer,Integer> map=new HashMap<>();
+		int[] temp=nums.clone();
+		Arrays.sort(temp);
+		for(int i=0;i<nums.length;i++)map.putIfAbsent(temp[i], i);
+		for(int i=0;i<nums.length;i++)temp[i]=map.get(nums[i]);
+		return temp;
+	}
+	
+	//O(n) without sorting
+	public static int[] smallerNumbersThanCurrentHashNosort(int[] nums) {
+		int[] res=new int[nums.length];
+		int[] count=new int[101];
+		for(int i=0;i<nums.length;i++)count[nums[i]]++;
+		for(int i=1;i<100;i++)count[i]+=count[i-1];
+		for(int i=0;i<nums.length;i++) {
+			if(nums[i]==0)res[i]=0;
+			else
+				//how many numbers are there before my value
+				res[i]=count[nums[i]-1];
+		}
+		return res;
+	}
+    
+	
+	
 	public static void main(String[] args) {
 		
-		//Explanation: 
-		//For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
-		//For nums[1]=1 does not exist any smaller number than it.
-		//For nums[2]=2 there exist one smaller number than it (1). 
-		//For nums[3]=2 there exist one smaller number than it (1). 
-		//For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
 		
 		int array1[]= {8,1,2,2,3};
-		System.out.println("Input: nums = "+Arrays.toString(array1));
-		System.out.println("Output: "+Arrays.toString(smallerNumbersThanCurrent(array1)));
+		System.out.println("Given the array1 = "+Arrays.toString(array1));
+		System.out.println("for each nums[i] find out how many numbers in the array are smaller than it: "+Arrays.toString(smallerNumbersThanCurrentHashNosort(array1)));
 		
 		int array2[]= {7,7,7,7};
-		System.out.println("Input: nums = "+Arrays.toString(array2));
-		System.out.println("Output: "+Arrays.toString(smallerNumbersThanCurrent(array2)));
+		System.out.println("Given the array2 = "+Arrays.toString(array2));
+		System.out.println("for each nums[i] find out how many numbers in the array are smaller than it: "+Arrays.toString(smallerNumbersThanCurrentHashNosort(array2)));
 		
 	}
 	
