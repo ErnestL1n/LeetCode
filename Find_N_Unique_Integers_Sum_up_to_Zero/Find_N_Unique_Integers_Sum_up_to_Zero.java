50f230554bf98b6b2672238561415cf15383c236
ErnestL1n <ernestlin1998@gmail.com>
2020-06-30 11:20:21 +0800

    Leetcode.1304

--- Find_N_Unique_Integers_Sum_up_to_Zero/Find_N_Unique_Integers_Sum_up_to_Zero.java
+++ Find_N_Unique_Integers_Sum_up_to_Zero/Find_N_Unique_Integers_Sum_up_to_Zero.java
@@ -15,6 +15,7 @@ public class Find_N_Unique_Integers_Sum_up_to_Zero {
 	/**
 	 * @param args
 	 */
+	/*
 	public static int[] sumZero(int n) {
         //x=1 -1 2 -2.....
         int temp[]=new int[n];
@@ -29,11 +30,27 @@ public class Find_N_Unique_Integers_Sum_up_to_Zero {
             temp[n-1]=0;
         return temp;
     }
+	*/
+	
+	//much brilliant solution
+	//  n=6
+	//  i =0,1,2,3,4,5
+	//a[i]=-5,-3,-1,1,3,5
+	//=>a[i]=i*2-n+1
+	//n==odd is OK
+	
+	
+	public static int[] sumZero(int n) {
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)a[i]=i*2-n+1;
+		return a;
+	}
 	
 	public static void main(String[] args) {
 		int n=5;
-		System.out.println("Input: n = "+n);
-		System.out.println("Output array is "+Arrays.toString(sumZero(n)));
+		System.out.println("Given an integer n :"+n);
+		System.out.println("return any array containing n unique integers such that they add up to 0 :"+Arrays.toString(sumZero(n)));
 	}
 
 }
+
