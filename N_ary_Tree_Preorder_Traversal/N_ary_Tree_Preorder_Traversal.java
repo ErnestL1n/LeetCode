ef1878a719f109821cd26cc5ecf15cc276d15246
ErnestL1n <ernestlin1998@gmail.com>
2020-07-02 22:06:06 +0800

    Leetcode.589

--- N_ary_Tree_Preorder_Traversal/N_ary_Tree_Preorder_Traversal.java
+++ N_ary_Tree_Preorder_Traversal/N_ary_Tree_Preorder_Traversal.java
@@ -15,12 +15,13 @@ class Node{
 	public Node() {}
 
     public Node(int _val) {
-    	val = _val;
+    	this.val = _val;
+    	this.children=new LinkedList<>();
     	}
 
     public Node(int _val, List<Node> _children) {
-    	val = _val;
-    	children = _children;
+    	this.val = _val;
+    	this.children = _children;
     	}
 }
 public class N_ary_Tree_Preorder_Traversal {
@@ -28,6 +29,64 @@ public class N_ary_Tree_Preorder_Traversal {
 	/**
 	 * @param args
 	 */
+	
+	public static Node BuildTree(Integer[] nodes) {
+		//Integer[] nodes=new Integer[] {1,null,3,2,4,null,5,6};
+		if(nodes==null||nodes.length==0)return null;
+		Queue<Node> treenodequeue=new LinkedList<>();
+		Queue<Integer> integerqueue=new LinkedList<>();
+		Node root=new Node(nodes[0]);
+		treenodequeue.offer(root);
+		for(int i=1;i<nodes.length;i++)integerqueue.offer(nodes[i]);
+		while(!integerqueue.isEmpty()){
+			Node current=treenodequeue.poll();
+			integerqueue.remove(null);
+			while(integerqueue.peek()!=null) {
+				Integer tempvalue=integerqueue.poll();
+				Node tempnode=new Node(tempvalue);
+				current.children.add(tempnode);
+				treenodequeue.offer(tempnode);
+			}
+		}
+		return root;
+	}
+	
+	public static int getTreeSize(Node root) {
+		if(root==null)return 0;
+		int totalcount=1;
+		Queue<Node> queue=new LinkedList<>();
+		queue.offer(root);
+		while(!queue.isEmpty()) {
+			Node current=queue.poll();
+			if(current.children.size()!=0) {
+				totalcount+=current.children.size();
+				queue.addAll(current.children);
+			}
+		}
+		return totalcount;
+	}
+	
+	
+	
+	
+	public static void printTree(Node root) {
+		if(root==null)return;
+		Queue<Node> treenodequeue=new LinkedList<>();
+		int size=getTreeSize(root);
+		treenodequeue.offer(root);
+		System.out.println("I am root , my value is "+root.val);
+		size--;
+		while(size!=0) {
+			int index=1;
+			Node current=treenodequeue.poll();
+			for(int i=0;i<current.children.size();i++){
+				System.out.println("I am "+index+"th child of "+current.val+" , my value is "+current.children.get(i).val);
+				index++;
+				size--;
+				treenodequeue.offer(current.children.get(i));
+			}
+		}
+	}
 	
 	
 	//recursive solution
@@ -44,7 +103,7 @@ public class N_ary_Tree_Preorder_Traversal {
 	//Iterative solution
 	//Iterative version is using stack
 	//preorder=>push rightmost child into stack first
-	public List<Integer> preorderIterative(Node root){
+	public static List<Integer> preorderIterative(Node root){
 	    List<Integer> answer=new ArrayList<>();
 	    if(root==null)return answer;
 	    Stack<Node> stack=new Stack<>();
@@ -62,10 +121,18 @@ public class N_ary_Tree_Preorder_Traversal {
 	    }
 	    return answer;
 	}
+
 	
 	public static void main(String[] args) {
-		
-
+		Integer[] nodes=new Integer[] {1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14};
+		System.out.println("Given an n-ary tree :"+Arrays.toString(nodes));
+		Node root=BuildTree(nodes);
+		printTree(root);
+		System.out.println("\nreturn the preorder traversal of its nodes' values");
+		System.out.println("\nNary-Tree input serialization is represented in their level order traversal,");
+		System.out.println ("each group of children is separated by the null value\n");
+		System.out.println("Thus the preorder traversal is "+preorderIterative(root));
+        
 	}
 
 }
