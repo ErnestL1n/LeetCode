e0fa64ea1b849a588424288e6cd08143f552e203
ErnestL1n <ernestlin1998@gmail.com>
2020-07-04 23:09:14 +0800

    Leetcode.1395

--- Count_Number_of_Teams/Count_Number_of_Teams.java
+++ Count_Number_of_Teams/Count_Number_of_Teams.java
@@ -31,6 +31,53 @@ public class Count_Number_of_Teams {
 	        }
 	        return count;
 	    }
+    
+    
+    private static int[] arr;
+    public static int numTeamsDP(int[] rating) {
+        arr = rating;
+        return findLarge() + findSmall();
+    }
+    private static int findLarge() {
+        int[] dp = new int[arr.length];
+        // first loop, count of smaller on the left;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i]++;
+                }
+            }
+        }
+        //second loop, collct count of subsequence ending of i;
+        int res = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    res += dp[j];
+                }
+            }
+        }
+        return res;
+    }
+    private static int findSmall() {  // same as count Larger
+        int[] dp = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    dp[i]++;
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    res += dp[j];
+                }
+            }
+        }
+        return res;
+    }
 	
 	public static void main(String[] args) {
 		int[] rating1=new int[] {2,5,3,4,1};
@@ -38,11 +85,11 @@ public class Count_Number_of_Teams {
 		int[] rating3=new int[] {1,2,3,4};
 		
 		System.out.println("Input  1 is:"+Arrays.toString(rating1));
-		System.out.println("Output 1 is:"+numTeams(rating1));
+		System.out.println("Output 1 is:"+numTeamsDP(rating1));
 		System.out.println("Input  2 is:"+Arrays.toString(rating2));
-		System.out.println("Output 2 is:"+numTeams(rating2));
+		System.out.println("Output 2 is:"+numTeamsDP(rating2));
 		System.out.println("Input  3 is:"+Arrays.toString(rating3));
-		System.out.println("Output 3 is:"+numTeams(rating3));
+		System.out.println("Output 3 is:"+numTeamsDP(rating3));
 	}
 	
 
