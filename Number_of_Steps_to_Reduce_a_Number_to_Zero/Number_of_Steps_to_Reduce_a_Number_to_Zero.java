0d797db1db97581ff0adde8c29d5afb2202b8261
ErnestL1n <ernestlin1998@gmail.com>
2020-07-06 22:26:08 +0800

    Leetcode.1342

--- Number_of_Steps_to_Reduce_a_Number_to_Zero/Number_of_Steps_to_Reduce_a_Number_to_Zero.java
+++ Number_of_Steps_to_Reduce_a_Number_to_Zero/Number_of_Steps_to_Reduce_a_Number_to_Zero.java
@@ -13,19 +13,20 @@ public class Number_of_Steps_to_Reduce_a_Number_to_Zero {
 	/**
 	 * @param args
 	 */
-	public static int numberOfSteps(int num) {
-		int answer=0;
-		while(num>0) {
-			if(num%2==0)num/=2;
-			else num-=1;
-			answer++;
-		}
-		return answer;
-	}
+    public static int numberOfSteps (int num) {
+        if(num==0)return 0;
+        int n=num,res=0;
+        while(n>0){
+            if(n%2==0)n/=2;
+            else --n;
+            ++res;
+        }
+        return res;
+    }
 	public static void main(String[] args) {
 		int num=14;
-		System.out.println("Input: num :"+num);
-		System.out.println("Output "+numberOfSteps(num));
+		System.out.println("Given a non-negative integer : "+num);
+		System.out.println("Number of steps to reduce it to zero is : "+numberOfSteps(num));
 
 	}
 
