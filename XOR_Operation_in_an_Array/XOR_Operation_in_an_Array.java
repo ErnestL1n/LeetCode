bd927fdcd1bf78f7f519ddaac6e7fb7d10761f9e
ErnestL1n <ernestlin1998@gmail.com>
2020-07-08 23:41:04 +0800

    Leetcode.1486

---
+++ XOR_Operation_in_an_Array/XOR_Operation_in_an_Array.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package github.com.ErnestL1n;
+
+/**
+ * @author https://github.com/ErnestL1n
+ *
+ */
+public class XOR_Operation_in_an_Array {
+
+	/**
+	 * @param args
+	 */
+	public static int xorOperation(int n, int start) {
+        int res=0;
+        for(int i=0;i<n;++i)
+            res^=(start+2*i);
+        return res;
+    }
+	
+	public static void main(String[] args) {
+		int n=4,start=3;
+		System.out.println("Given an integer n "+n+" and an integer start."+start);
+		System.out.println("Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.");
+		System.out.println("Return the bitwise XOR of all elements of nums.");
+		System.out.println(" => "+xorOperation(n,start));
+	}
+
+}
