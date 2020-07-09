5d5e1ed5959ea3d837a8ea030c287a2fb61a0be5
ErnestL1n <ernestlin1998@gmail.com>
2020-07-09 23:42:05 +0800

    Leetcode.1207

---
+++ Unique_Number_of_Occurrences/Unique_Number_of_Occurrences.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package github.com.ErnestL1n;
+import java.util.*;
+/**
+ * @author https://github.com/ErnestL1n
+ *
+ */
+//Leetcode.1207
+public class Unique_Number_of_Occurrences {
+
+	/**
+	 * @param args
+	 */
+	
+	//Great Idea is provided by:https://leetcode.com/rock/
+	public static boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int a : arr)
+            count.put(a, 1 + count.getOrDefault(a, 0));
+        return count.size() == new HashSet<>(count.values()).size();
+    }
+	
+	
+	//more concise and readable version is provided by:https://leetcode.com/dontsovigor1/  &  https://leetcode.com/kelvinkkl
+	public static boolean uniqueOccurrencesreadable(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return true;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> unique = new HashSet<>();
+        for (int val : arr) {
+            map.put(val, map.getOrDefault(val, 0) + 1);
+        }
+        for (int val : map.values()) {
+            /*if (unique.contains(val)) {
+                return false;
+            } else {
+                unique.add(val);
+            }*/
+        	if(!unique.add(val)) return false; // the number represent the occurrences already exists in the hashset.
+        }
+        return true;
+}
+	 
+	 
+	public static void main(String[] args) {
+		
+
+	}
+
+}
