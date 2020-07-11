0411595509fc2c3d4cc8b6ecb0b37400a98f486b
ErnestL1n <ernestlin1998@gmail.com>
2020-07-11 22:13:53 +0800

    Leetcode.1002

--- Find_Common_Characters/Find_Common_Characters.java
+++ Find_Common_Characters/Find_Common_Characters.java
@@ -37,12 +37,44 @@ public class Find_Common_Characters {
        }
         return res;
     }
+	
+    public static int[] arr = new int[26];
+    public static List<String> commonCharsStraightForward(String[] A) {
+        List<String> list = new ArrayList<String>();
+        if (A == null || A.length == 0) return list;
+        int[] arr_ori = new int[26];
+        for (char c : A[0].toCharArray()) {
+            arr_ori[c - 'a']++;
+        }
+        for (int i = 1; i < A.length; i++) {
+            countValid(A[i], arr_ori);
+        }
+        for (int i = 0; i < 26; i++) {
+            if (arr_ori[i] != 0) {
+                for (int j = 0; j < arr_ori[i]; j++) {
+                    list.add((char)(i+'a') + "");
+                }
+            }
+        }
+        return list;
+    }
+    private static void countValid(String str, int[] arr_ori) {
+        for (char c : str.toCharArray()) {
+            arr[c - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (arr_ori[i] != 0 && arr[i] != arr_ori[i]) {
+                arr_ori[i] = Math.min(arr_ori[i], arr[i]);
+            }
+        }
+        arr = new int[26];
+    } 
 	public static void main(String[] args) {
 		String[] A=new String[] {"bella","label","roller"};
 		System.out.println("Given an array A of strings made only from lowercase letters");
 		System.out.println(Arrays.toString(A));
 		System.out.println("return a list of all characters that show up in all strings within the list (including duplicates)");
-		System.out.println(commonChars(A));
+		System.out.println(commonCharsStraightForward(A));
 	}
 
 }
