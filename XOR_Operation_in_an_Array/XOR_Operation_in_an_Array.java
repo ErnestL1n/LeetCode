fabd2822c3da9f1d829e0b8dc7429f1503d29d35
ErnestL1n <ernestlin1998@gmail.com>
2020-07-11 23:02:16 +0800

    Leetcode.1486

--- XOR_Operation_in_an_Array/XOR_Operation_in_an_Array.java
+++ XOR_Operation_in_an_Array/XOR_Operation_in_an_Array.java
@@ -20,12 +20,36 @@ public class XOR_Operation_in_an_Array {
         return res;
     }
 	
+	
+	//trick implementation is provided by:https://leetcode.com/coder206/
+	private static int xorOperationB(int n, int start)
+    {
+        if (n % 2 == 0)
+            return (n / 2) & 1;
+        else
+            return ((n / 2) & 1) ^ (start + n - 1);
+    }
+    private static int xorOperationA(int n, int start)
+    {
+        if ((start & 1)==1)
+            return (start - 1) ^ xorOperationB(n + 1, start - 1);
+        else
+            return xorOperationB(n, start);
+    }    
+    public static int xorOperationTrick(int n, int start)
+    {
+        int ret = 2 * xorOperationA(n, start / 2);
+        if ((n & start & 1)==1) ret++;
+        return ret;
+    }
+    
+	
 	public static void main(String[] args) {
 		int n=4,start=3;
 		System.out.println("Given an integer n "+n+" and an integer start."+start);
 		System.out.println("Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.");
 		System.out.println("Return the bitwise XOR of all elements of nums.");
-		System.out.println(" => "+xorOperation(n,start));
+		System.out.println("=> "+xorOperationTrick(n,start));
 	}
 
 }
