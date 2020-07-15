e43d749ea64609ff781e23bba30b31f5ff241472
ErnestL1n <ernestlin1998@gmail.com>
2020-07-15 23:39:47 +0800

    Leetcode.338

--- Counting_Bits/Counting_Bits.java
+++ Counting_Bits/Counting_Bits.java
@@ -23,12 +23,24 @@ public class Counting_Bits {
         return res;
     }
 	
+	public static int[] countBitsDP(int num) {
+	    int result[] = new int[num + 1];
+	    int offset = 1;
+	    for (int index = 1; index < num + 1; ++index){
+	        if (offset * 2 == index){
+	            offset *= 2;
+	        }
+	        result[index] = result[index - offset] + 1;
+	    }
+	    return result;
+	}
+	
 	public static void main(String[] args) {
 		int num=5;
 		System.out.println("Given a non negative integer number num : "+num);
 		System.out.println("For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's "
 				+ "in their binary representation and return them as an array.");
-		System.out.println(Arrays.toString(countBits(num)));
+		System.out.println(Arrays.toString(countBitsDP(num)));
 
 	}
 
