0c12581ff26ea4583ae8d90c4b7410c71d59852a
ErnestL1n <ernestlin1998@gmail.com>
2020-07-17 23:03:25 +0800

    Leetcode.1002

--- Find_Common_Characters/Find_Common_Characters.java
+++ Find_Common_Characters/Find_Common_Characters.java
@@ -16,59 +16,61 @@ public class Find_Common_Characters {
 	//provided by:https://leetcode.com/sntz95/
 	//HashMap
 	//45ms
-	public static List<String> commonChars(String[] A) {
-        List<String> res = new ArrayList<String>(); 
-        if(A == null || A.length == 0) return res; 
-        Map<Character,Integer> init=new HashMap<>();
-        for(int i=0;i<A[0].length();i++){
-            init.put(A[0].charAt(i),1+init.getOrDefault(A[0].charAt(i),0));
+    public static List<String> commonChars(String[] A) {
+        List<String> res=new ArrayList<>();
+        if(A==null||A.length==0)return res;
+        Map<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<A[0].length();++i)
+            map.put(A[0].charAt(i),1+map.getOrDefault(A[0].charAt(i),0));
+        for(int i=0;i<A.length;++i){
+            Map<Character,Integer> temp=new HashMap<>();
+            for(int j=0;j<A[i].length();++j){
+                char curr=A[i].charAt(j);
+                //update value with temp
+                if(map.containsKey(curr))
+                    temp.put(curr,Math.min(map.get(curr),(1+temp.getOrDefault(curr,0))));
+            }
+            map=temp;
         }
-        for(int i=1;i<A.length;i++){
-            HashMap<Character,Integer> temp=new HashMap<>();
-            for(int j = 0; j < A[i].length(); j++){
-            char curr = A[i].charAt(j); 
-            if(init.containsKey(curr)) temp.put(curr, Math.min(init.get(curr), temp.getOrDefault(curr, 0)+1)); 
+        for(char c:map.keySet()){
+            for(int i=0;i<map.get(c);i++)
+                res.add(c+"");
         }
-        init = temp;
-        }
-        
-       for(char c : init.keySet()){
-        for(int i = 0; i < init.get(c); i++) res.add(c + ""); 
-       }
         return res;
     }
 	
-    public static int[] arr = new int[26];
+	
+	//second implementation is provided by https://leetcode.com/raokatie/
+    
+    private static int[] arr=new int[26];
     public static List<String> commonCharsStraightForward(String[] A) {
-        List<String> list = new ArrayList<String>();
-        if (A == null || A.length == 0) return list;
-        int[] arr_ori = new int[26];
-        for (char c : A[0].toCharArray()) {
-            arr_ori[c - 'a']++;
-        }
-        for (int i = 1; i < A.length; i++) {
-            countValid(A[i], arr_ori);
-        }
-        for (int i = 0; i < 26; i++) {
-            if (arr_ori[i] != 0) {
-                for (int j = 0; j < arr_ori[i]; j++) {
-                    list.add((char)(i+'a') + "");
-                }
+        List<String> res=new ArrayList<>();
+        if(A==null||A.length==0)return res;
+        int[] arr_ori=new int[26];
+        for(char c:A[0].toCharArray())
+            ++arr_ori[c-'a'];
+        for(int i=1;i<A.length;++i)
+            countValid(A[i],arr_ori);
+        for(int i=0;i<26;++i){
+            while(arr_ori[i]!=0){
+                res.add((char)(i+'a')+"");
+                --arr_ori[i];
             }
         }
-        return list;
+        return res;
     }
-    private static void countValid(String str, int[] arr_ori) {
-        for (char c : str.toCharArray()) {
-            arr[c - 'a']++;
+    private static void countValid(String str,int[] arr_ori){
+        for(char c:str.toCharArray())
+            ++arr[c-'a'];
+        for(int i=0;i<26;++i){
+            if(arr_ori[i]!=0&&arr[i]!=arr_ori[i])
+                arr_ori[i]=Math.min(arr[i],arr_ori[i]);
         }
-        for (int i = 0; i < 26; i++) {
-            if (arr_ori[i] != 0 && arr[i] != arr_ori[i]) {
-                arr_ori[i] = Math.min(arr_ori[i], arr[i]);
-            }
-        }
-        arr = new int[26];
-    } 
+        //refresh
+        arr=new int[26];
+    }
+    
+    
 	public static void main(String[] args) {
 		String[] A=new String[] {"bella","label","roller"};
 		System.out.println("Given an array A of strings made only from lowercase letters");
