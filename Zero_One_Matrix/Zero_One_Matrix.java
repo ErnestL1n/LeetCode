863d4b31a7b15104131769d9e2e9fb130c8fb881
ErnestL1n <ernestlin1998@gmail.com>
2020-07-19 21:51:52 +0800

    Leetcode.542

--- Zero_One_Matrix/Zero_One_Matrix.java
+++ Zero_One_Matrix/Zero_One_Matrix.java
@@ -131,6 +131,34 @@ public class Zero_One_Matrix {
         
         return dis;
     }
+    
+  
+    public int[][] updateMatrixDP2(int[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int[][] dp = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                Arrays.fill(dp[i], 10000);
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == 0) dp[i][j] = 0;
+                    else {
+                        if (i > 0) dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
+                        if (j > 0) dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+                    }
+                }
+            }
+
+            for (int i = m - 1; i >= 0; i--) {
+                for (int j = n - 1; j >= 0; j--) {
+                    if (i < m - 1) dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] + 1);
+                    if (j < n - 1) dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] + 1);
+                }
+            }
+
+            return dp;
+        }
+    
+    
 	public static void main(String[] args) {
 		int[][] matrix=new int[][] {{0,0,0}, {0,1,0}, {1,1,1}};
 		System.out.println("Input array is :");
