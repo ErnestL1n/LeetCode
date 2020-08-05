12619733a41436f32c81d69f8a3381def6677553
ErnestL1n <ernestlin1998@gmail.com>
2020-08-05 23:44:33 +0800

    Leetcode.1455

--- Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence/Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.java
+++ Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence/Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.java
@@ -36,7 +36,8 @@ public class Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence {
     public int isPrefixOfWord(String sentence, String searchWord) {
         String[] words = sentence.split(" ");
         for (int i=1; i<=words.length;++i)
-            if(words[i-1].indexOf(searchWord)==0) return i; 
+            if(words[i-1].indexOf(searchWord)==0) //if (words[i - 1].startsWith(searchWord)) 
+            	return i; 
         return -1; 
     } 
 	
