ca673de349eef9d5927c03eca3ec09604a236e56
ErnestL1n <ernestlin1998@gmail.com>
2020-08-12 23:22:32 +0800

    Leetcode.1547

--- Minimum_Cost_to_Cut_a_Stick/Minimum_Cost_to_Cut_a_Stick.cpp
+++ Minimum_Cost_to_Cut_a_Stick/Minimum_Cost_to_Cut_a_Stick.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int dp[102][102];
-    int minCost(int n, vector<int>& cuts) {
+    int dptopdown[102][102];
+    int minCostTopDown(int n, vector<int>& cuts) {
         cuts.push_back(0);
         cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
@@ -10,12 +10,27 @@ public:
     int dfs(vector<int>& cuts,int i,int j){
         if(j-i<=1)
             return 0;
-        if(!dp[i][j]){
-            dp[i][j]=INT_MAX;
+        if(!dptopdown[i][j]){
+            dptopdown[i][j]=INT_MAX;
             for(auto k=i+1;k<j;++k)
-                dp[i][j]=min(dp[i][j],
+                dptopdown[i][j]=min(dptopdown[i][j],
                             cuts[j]-cuts[i]+dfs(cuts,i,k)+dfs(cuts,k,j));
         }
-        return dp[i][j];
+        return dptopdown[i][j];
     }
+	
+	
+	int minCostButtomUp(int n, vector<int>& cuts){
+		cuts.push_back(0);
+		cuts.push_back(n);
+		sort(cuts.begin(),cuts.end());
+		int size=cuts.size();
+		vector<vector<int>> dp(size,vector<int>(size));
+		for(int i=size-1;i>=0;--i)
+            for(int j=i+1;j<size;++j)
+                for(int k=i+1;k<j;++k)
+                    dp[i][j]=min(dp[i][j]==0?INT_MAX:dp[i][j],
+                                cuts[j]-cuts[i]+dp[i][k]+dp[k][j]);
+        return dp[0][size-1];
+	}
 };
