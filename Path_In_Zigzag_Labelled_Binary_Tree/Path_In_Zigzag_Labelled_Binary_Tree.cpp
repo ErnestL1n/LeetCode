72851c038371f5b8146d0f44954a1e5bf643d1a3
ErnestL1n <ernestlin1998@gmail.com>
2020-08-21 23:56:35 +0800

    Leetcode.1104

---
+++ Path_In_Zigzag_Labelled_Binary_Tree/Path_In_Zigzag_Labelled_Binary_Tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> pathInZigZagTree(int label) {
+        int level=0;
+        while(1<<level<=label)++level;
+        vector<int> res(level);
+        while(label!=0){
+            res[level-1]=label;
+            label = ((1 << level) - 1 + (1 << (level - 1)) - label) / 2; 
+            level--;
+        }
+        return res;
+    }
+};
