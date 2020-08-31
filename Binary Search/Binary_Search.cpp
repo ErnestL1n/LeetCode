1a7de9dc9092ae0127993b4544f99762324d4a17
ErnestL1n <ernestlin1998@gmail.com>
2020-08-31 23:29:30 +0800

    Leetcode.704

---
+++ Binary Search/Binary_Search.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(nums.size()==0)
+            return -1;
+        return binarysearch(nums,0,nums.size()-1,target);
+    }
+    int binarysearch(vector<int>& nums,int left,int right,int target){
+        if(left==right)
+            return nums[left]==target?left:-1;
+        else{
+            int mid=(left+right)/2;
+            if(target<=nums[mid])return binarysearch(nums,left,mid,target);
+            else return binarysearch(nums,mid+1,right,target);
+        }
+    }
+};
+
+/*non-recursive version
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                right=mid-1;
+            else
+                left=mid+1;
+        }
+        return -1;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
