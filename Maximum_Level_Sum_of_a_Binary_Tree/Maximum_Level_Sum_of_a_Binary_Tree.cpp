35f3e8d1a739f337467cbdce97e5e9a9a1c40e81
ErnestL1n <ernestlin1998@gmail.com>
2020-09-03 23:55:24 +0800

    Leetcode.1161

---
+++ Maximum_Level_Sum_of_a_Binary_Tree/Maximum_Level_Sum_of_a_Binary_Tree.cpp
@@ -0,0 +1,37 @@
+//level order
+int maxLevelSum(TreeNode* r) {
+  vector<TreeNode*> q{r};
+  int msum = 0, lvl = 0, msum_lvl = 0;
+  while (!q.empty()) {
+      vector<TreeNode*> q1;
+      int sum = 0;
+      ++lvl;
+      for (auto n : q) {
+          sum += n->val;
+          if (n->left != nullptr) 
+              q1.push_back(n->left);
+          if (n->right != nullptr) 
+              q1.push_back(n->right);
+      }
+      msum_lvl = sum < msum ? msum_lvl : lvl;
+      msum = max(sum, msum);
+      swap(q, q1);
+  }
+  return msum_lvl;
+}
+
+/* DFS
+vector<int> sums;
+void dfs(TreeNode* r, size_t lvl) {
+  if (r != nullptr) {
+    sums.resize(max(sums.size(), lvl));
+    sums[lvl - 1] += r->val;
+    dfs(r->left, lvl + 1);
+    dfs(r->right, lvl + 1);
+  }
+}
+int maxLevelSum(TreeNode* r) {
+  dfs(r, 1);
+  return distance(begin(sums), max_element(begin(sums), end(sums))) + 1;
+}
+*/
--- Maximum_Level_Sum_of_a_Binary_Tree/Maximum_Level_Sum_of_a_Binary_Tree.java
+++ Maximum_Level_Sum_of_a_Binary_Tree/Maximum_Level_Sum_of_a_Binary_Tree.java
@@ -56,6 +56,30 @@ public class Maximum_Level_Sum_of_a_Binary_Tree {
         return maxlevel;
     }
 	
+	//  BFS version
+	/*
+    public int maxLevelSum(TreeNode root) {
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        int level=1, sum=root.val, l=1;
+        while(!q.isEmpty()) {
+            int size=q.size(), cur=0;
+            for(int i=0;i<size;i++) {
+                TreeNode tmp=q.poll();
+                cur+=tmp.val;
+                if(tmp.left!=null) q.offer(tmp.left);
+                if(tmp.right!=null) q.offer(tmp.right);
+            }
+            if(cur>sum) {
+                sum=cur;
+                level=l;
+            }
+            l++;
+        }
+        return level;
+    }
+	*/
+	
 	public static TreeNode Build(Integer[] array) {
         if (array == null || array.length==0) {
             return null;
