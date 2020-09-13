0d8b0681800f84bb9f03a9fd31017c54e368e218
ErnestL1n <ernestlin1998@gmail.com>
2020-09-13 23:59:03 +0800

    Leetcode.1582

---
+++ Special Positions in a Binary Matrix/Special Positions in a Binary Matrix.java
@@ -0,0 +1,41 @@
+/* HashMap
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int res=0;
+        var row=new HashMap<Integer,Integer>();
+        var column=new HashMap<Integer,Integer>();
+        for(int i=0;i<mat.length;++i)
+            for(int j=0;j<mat[0].length;++j){
+                if(mat[i][j]==1){
+                    row.put(i,row.getOrDefault(i,0)+1);
+                    column.put(j,column.getOrDefault(j,0)+1);
+                }
+            }           
+        for(int i=0;i<mat.length;++i)
+            for(int j=0;j<mat[0].length;++j){
+                if(mat[i][j]==1&&row.get(i)==1&&column.get(j)==1)
+                    ++res;
+            }
+        return res;
+    }
+}
+*/
+
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int res=0;
+        int[] row=new int[mat.length],column=new int[mat[0].length];
+        for(int i=0;i<mat.length;++i)
+            for(int j=0;j<mat[0].length;++j){
+                if(mat[i][j]==1){
+                    ++row[i];++column[j];
+                }
+            }           
+        for(int i=0;i<mat.length;++i)
+            for(int j=0;j<mat[0].length;++j){
+                if(mat[i][j]==1&&row[i]==1&&column[j]==1)
+                    ++res;
+            }
+        return res;
+    }
+}
