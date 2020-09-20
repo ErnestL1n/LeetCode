eec0412babb57269a03c2bc4a7e63cb12b166c37
ErnestL1n <ernestlin1998@gmail.com>
2020-09-20 23:17:22 +0800

    Leetcode.1584

--- Min Cost to Connect All Points/Min Cost to Connect All Points.java
+++ Min Cost to Connect All Points/Min Cost to Connect All Points.java
@@ -1,37 +1,45 @@
 class Solution {
-    int[] prts;  //parents for union-find
-    public int minCostConnectPoints(int[][] ps) {
-        int m = ps.length, islands = m, res = 0, dist[][] = new int[m][m];  // dist is cache for distance of each edge.
-        prts = new int[m];
-        for (int i = 0;  i < m; i++) prts[i] = i;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> dist[a[0]][a[1]] - dist[b[0]][b[1]]);
-        for (int i = 0; i < m; i++) 
-            for (int j = i + 1; j < m; j++) {
-                dist[i][j] = Math.abs(ps[i][0] - ps[j][0]) + Math.abs(ps[i][1] - ps[j][1]);
-                pq.offer(new int[]{i, j});
+    public int minCostConnectPoints(int[][] points) {
+        int n=points.length;
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        for(int i=0;i<n-1;++i)
+            for(int j=i+1;j<n;++j){
+                int dist=Math.abs(points[i][0]-points[j][0])+Math.abs(points[i][1]-points[j][1]);
+                pq.offer(new int[]{dist,i,j});
             }
-        while (!pq.isEmpty() && islands > 1) {
-            int[] p = pq.poll();
-            if (union(p[0], p[1])) {
-                res += dist[p[0]][p[1]];
-                islands--;
-            }
+        int res=0;
+        Union un=new Union(n);
+        while(!un.United()&&!pq.isEmpty()){
+            int[] edge=pq.poll();
+            int p1=edge[1];
+            int p2=edge[2];
+            if(un.Unite(p1,p2))
+                res+=edge[0];
         }
         return res;
     }
-    
-    private boolean union(int i, int j) {
-        int ip = find(i), jp = find(j);
-        if (ip == jp) return false;
-        prts[ip] = jp;
-        return true;
-    }
-    
-    private int find(int i) {
-        int ip = prts[i];
-        if (ip != i) {
-            prts[i] = find(ip);
+    private class Union{
+        int components;
+        int[] component;
+        Union(){};
+        Union(int n){
+            components=n;
+            component=new int[n+1];
+            for(int i=0;i<=n;++i)
+                component[i]=i;
         }
-        return prts[i];
+        int Find(int a){
+            if(component[a]!=a)
+                component[a]=Find(component[a]);
+            return component[a];
+        }
+        boolean Unite(int a,int b){
+            if(Find(a)==Find(b))
+                return false;
+            --components;
+            component[Find(a)]=b;
+            return true;
+        }
+        boolean United(){return components==1;}
     }
 }
