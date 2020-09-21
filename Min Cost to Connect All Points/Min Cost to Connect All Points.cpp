c50bfdbb3a23ed9273b39d5372d8d5c1930e5ee8
ErnestL1n <ernestlin1998@gmail.com>
2020-09-21 23:31:59 +0800

    Leetcode.1584

--- Min Cost to Connect All Points/Min Cost to Connect All Points.cpp
+++ Min Cost to Connect All Points/Min Cost to Connect All Points.cpp
@@ -114,3 +114,51 @@ int minCostConnectPoints(vector<vector<int>>& ps) {
     return res;
 }
 */
+
+/*
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        int n=points.length;
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        for(int i=0;i<n-1;++i)
+            for(int j=i+1;j<n;++j){
+                int dist=Math.abs(points[i][0]-points[j][0])+Math.abs(points[i][1]-points[j][1]);
+                pq.offer(new int[]{dist,i,j});
+            }
+        int res=0;
+        Union un=new Union(n);
+        while(!un.United()&&!pq.isEmpty()){
+            int[] edge=pq.poll();
+            int p1=edge[1];
+            int p2=edge[2];
+            if(un.Unite(p1,p2))
+                res+=edge[0];
+        }
+        return res;
+    }
+    private class Union{
+        int components;
+        int[] component;
+        Union(){};
+        Union(int n){
+            components=n;
+            component=new int[n+1];
+            for(int i=0;i<=n;++i)
+                component[i]=i;
+        }
+        int Find(int a){
+            if(component[a]!=a)
+                component[a]=Find(component[a]);
+            return component[a];
+        }
+        boolean Unite(int a,int b){
+            if(Find(a)==Find(b))
+                return false;
+            --components;
+            component[Find(a)]=b;
+            return true;
+        }
+        boolean United(){return components==1;}
+    }
+}
+*/
