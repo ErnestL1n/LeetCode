d0ce092586ebd1f374a948b0b696c0253eb912e0
ErnestL1n <ernestlin1998@gmail.com>
2020-09-30 23:49:50 +0800

    Leetcode.46

---
+++ Permutations/Permutations.cpp
@@ -0,0 +1,48 @@
+//input:[1,2,3]
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        help(0,nums,result);
+        return result;
+    }
+    void help(int begin,vector<int>& nums,vector<vector<int>>& result){
+        if(begin==nums.size()){
+            result.push_back(nums);
+            return;
+        }
+		//output:[[3,1,2],[3,2,1],[2,3,1],[2,1,3],[1,3,2],[1,2,3]]
+        for(int i=nums.size()-1;i>=begin;--i){
+            swap(nums[i],nums[begin]);
+            help(begin+1,nums,result);
+            swap(nums[i],nums[begin]);
+        }
+        return;
+    }
+};
+
+
+//output:
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        help(0,nums,res);
+        return res;
+    }
+    void help(int begin,vector<int>& nums,vector<vector<int>>& res){
+        if(begin==nums.size())
+        {
+            res.push_back(nums);
+            return;
+        }
+		//output:[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,2,1],[3,1,2]]
+        for(int i=begin;i<nums.size();++i){
+            swap(nums[begin],nums[i]);
+            help(begin+1,nums,res);
+            swap(nums[begin],nums[i]);
+        }
+        return;
+    }
+};
