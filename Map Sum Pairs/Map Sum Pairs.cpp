6dbb149dc5b734571e0e714a5ff6994a33325104
ErnestL1n <ernestlin1998@gmail.com>
2020-10-07 23:51:08 +0800

    Leetcode.677

--- Map Sum Pairs/Map Sum Pairs.cpp
+++ Map Sum Pairs/Map Sum Pairs.cpp
@@ -1,18 +1,27 @@
 class MapSum {
 public:
-    /** Initialize your data structure here. */    
+    struct trie{
+        trie* ch[26]={};
+        int sum=0;
+    }root;
+    unordered_map<string,int>pairs;
+    /** Initialize your data structure here. */
+    MapSum() {
+        
+    }
+    
     void insert(string key, int val) {
-        mp[key] = val;
+        auto p=&root;
+        for(auto i=0;i<key.size();p->sum+=val-pairs[key],++i)
+            p=p->ch[key[i]-'a']=p->ch[key[i]-'a']==nullptr?new trie():p->ch[key[i]-'a'];
+        pairs[key]=val;
     }
     
     int sum(string prefix) {
-        int sum = 0, n = prefix.size();
-        for (auto it = mp.lower_bound(prefix); it != mp.end() && it->first.substr(0, n) == prefix; it++) 
-            sum += it->second;
-        return sum;
+        auto p=&root;
+        for(auto i=0;i<prefix.size()&&p!=nullptr;p = p->ch[prefix[i] - 'a'], ++i);
+        return p==nullptr?0:p->sum;
     }
-private:
-    map<string, int> mp;
 };
 
 /**
