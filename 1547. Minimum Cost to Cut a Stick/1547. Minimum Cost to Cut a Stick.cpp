84dae0083697f9f5987b5563a8fe9e5abb847201
ErnestL1n <ernestlin1998@gmail.com>
2020-10-23 00:55:08 +0800

    Leetcode.1547

--- 1547. Minimum Cost to Cut a Stick/1547. Minimum Cost to Cut a Stick.cpp
+++ 1547. Minimum Cost to Cut a Stick/1547. Minimum Cost to Cut a Stick.cpp
@@ -22,15 +22,16 @@ public:
 	
 	int minCostButtomUp(int n, vector<int>& cuts){
 		cuts.push_back(0);
-		cuts.push_back(n);
-		sort(cuts.begin(),cuts.end());
-		int size=cuts.size();
-		vector<vector<int>> dp(size,vector<int>(size));
-		for(int i=size-1;i>=0;--i)
-            for(int j=i+1;j<size;++j)
-                for(int k=i+1;k<j;++k)
-                    dp[i][j]=min(dp[i][j]==0?INT_MAX:dp[i][j],
-                                cuts[j]-cuts[i]+dp[i][k]+dp[k][j]);
+        cuts.push_back(n);
+        sort(cuts.begin(),cuts.end());
+        int size=cuts.size();
+        vector<vector<int>> dp(size,vector<int>(size,0));
+        for(int i=0;i<size;++i)
+            for(int j=i-1;j>=0;--j)
+                for(int k=j+1;k<i;++k)
+                    dp[j][i]=min(dp[j][i]==0?INT_MAX:dp[j][i],cuts[i]-cuts[j]+
+                                dp[j][k]+dp[k][i]);
+                    
         return dp[0][size-1];
-	}
+    }
 };
