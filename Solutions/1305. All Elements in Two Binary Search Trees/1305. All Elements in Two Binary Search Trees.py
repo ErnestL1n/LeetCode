4bf297d8a887226a69b4a5f478a579b849a001db
ErnestL1n <ernestlin1998@gmail.com>
2020-11-19 01:03:40 +0800

    Update 1305. All Elements in Two Binary Search Trees.py

--- Solutions/1305. All Elements in Two Binary Search Trees/1305. All Elements in Two Binary Search Trees.py
+++ Solutions/1305. All Elements in Two Binary Search Trees/1305. All Elements in Two Binary Search Trees.py
@@ -33,23 +33,23 @@ class Solution:
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
-        def pushleft(s: List[int],root: TreeNode):
-            while root!=None:
-                s.append(root)
-                root=root.left
-        s1,s2=[],[]
-        pushleft(s1,root1)
-        pushleft(s2,root2)
-        res=[]
-        while s1 or s2:
-            s=s2 if not s1 else s1 if not s2 else s1 if s1[-1].val<s2[-1].val else s2
-            n=s.pop()
-            res.append(n.val)
-            if n.right!=None:
-                pushleft(s,n.right)
-        return res
+# class Solution:
+    # def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        # def pushleft(s: List[int],root: TreeNode):
+            # while root!=None:
+                # s.append(root)
+                # root=root.left
+        # s1,s2=[],[]
+        # pushleft(s1,root1)
+        # pushleft(s2,root2)
+        # res=[]
+        # while s1 or s2:
+            # s=s2 if not s1 else s1 if not s2 else s1 if s1[-1].val<s2[-1].val else s2
+            # n=s.pop()
+            # res.append(n.val)
+            # if n.right!=None:
+                # pushleft(s,n.right)
+        # return res
         
         
     
