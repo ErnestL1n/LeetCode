2a64c0140f0128825bc81141039e512e6c12a7d5
ErnestL1n <ernestlin1998@gmail.com>
2020-12-05 00:30:18 +0800

    update

---
+++ Solutions/2. Add Two Numbers/2. Add Two Numbers.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ type ListNode struct {
+    Val int
+    Next* ListNode
+ }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    head:=&ListNode{Val:0}
+    var extra int=0
+    curr:=head
+    for l1!=nil || l2!=nil || extra!=0 {
+        if l1!=nil{
+            extra+=l1.Val
+            l1=l1.Next
+        }
+        if l2!=nil{
+            extra+=l2.Val
+            l2=l2.Next
+        }
+        curr.Next=&ListNode{Val:extra%10}
+        curr=curr.Next
+        extra/=10
+    }
+    return head.Next
+}
---
+++ Solutions/35. Search Insert Position/35. Search Insert Position.go
@@ -0,0 +1,12 @@
+func searchInsert(nums []int, target int) int {
+    lo,hi:=0,len(nums)-1
+    for lo<=hi{
+        mid:=(lo+hi)>>1
+        if nums[mid]<target{
+            lo=mid+1
+        }else{
+            hi=mid-1
+        }
+    }
+    return lo
+}
