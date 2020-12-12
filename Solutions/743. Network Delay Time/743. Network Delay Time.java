0a847ec807be4f0e957cee8e517ac67b978a0479
ErnestL1n <ernestlin1998@gmail.com>
2020-12-12 23:40:10 +0800

    update

--- Solutions/743. Network Delay Time/743. Network Delay Time.java
+++ Solutions/743. Network Delay Time/743. Network Delay Time.java
@@ -1,34 +1,34 @@
 class Solution {
     public int networkDelayTime(int[][] times, int N, int K) {
-        Map<Integer, Map<Integer,Integer>> map = new HashMap<>();
-        for(int[] time : times){
-            map.putIfAbsent(time[0], new HashMap<>());
-            map.get(time[0]).put(time[1], time[2]);
-        }
-        
-        //distance, node into pq
-        Queue<int[]> pq = new PriorityQueue<>((a,b) -> (a[0] - b[0]));
-        
-        pq.add(new int[]{0, K});
-        
-        boolean[] visited = new boolean[N+1];
-        int res = 0;
-        
+        List<List<int[]>> graph=new ArrayList<>();
+        for(int i=0;i<=N;++i)
+            graph.add(new ArrayList<>());
+        for(int[] time:times){
+            graph.get(time[0]).add(new int[]{time[1],time[2]});
+        } 
+        boolean[] visited=new boolean[N+1];
+        int[] d=new int[N+1];
+        Arrays.fill(d,Integer.MAX_VALUE);
+        d[0]=d[K]=0;
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->(a[0]-b[0]));
+        pq.offer(new int[]{0,K});
         while(!pq.isEmpty()){
-            int[] cur = pq.remove();
-            int curNode = cur[1];
-            int curDist = cur[0];
-            if(visited[curNode]) continue;
-            visited[curNode] = true;
-            res = curDist;
-            N--;
-            if(map.containsKey(curNode)){
-                for(int next : map.get(curNode).keySet()){
-                    pq.add(new int[]{curDist + map.get(curNode).get(next), next});
-                }
-            }
+            int[] curr=pq.poll();
+            int dis=curr[0],node=curr[1];
+            if(visited[node])
+                continue;          
+            for(var to:graph.get(node)){
+                int v=to[0],w=to[1];
+                if(dis+w<d[v]){
+                    d[v]=dis+w;
+                    pq.offer(new int[]{d[v],v});
+                } 
+            }    
+            visited[node]=true;
         }
-        return N == 0 ? res : -1;
-            
+        int res=Integer.MIN_VALUE;
+        for(int i=0;i<d.length;++i)
+            res=res<d[i]?d[i]:res;
+        return res==Integer.MAX_VALUE?-1:res;
     }
 }
