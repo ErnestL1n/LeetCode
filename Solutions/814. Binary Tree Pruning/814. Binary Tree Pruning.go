a9b8db136e546cefca4283fa61212408897dff1a
ErnestL1n <ernestlin1998@gmail.com>
2020-12-29 00:22:37 +0800

    update

---
+++ Solutions/814. Binary Tree Pruning/814. Binary Tree Pruning.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pruneTree(root *TreeNode) *TreeNode {
+    if root!=nil{
+        root.Left=pruneTree(root.Left)
+        root.Right=pruneTree(root.Right)
+        if root.Val==0 && root.Left==nil && root.Right==nil{
+            root=nil
+        }
+    }
+    return root
+}
