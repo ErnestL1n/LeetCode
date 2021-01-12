84fcacdf8aeaceed3d99d5a1557f4a8cdf8281d9
ErnestL1n <ernestlin1998@gmail.com>
2021-01-12 23:12:53 +0800

    update

--- Solutions/1202. Smallest String With Swaps/1202. Smallest String With Swaps.cpp
+++ Solutions/1202. Smallest String With Swaps/1202. Smallest String With Swaps.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int find(vector<int>& ds,int i){
+    int find(vector<int>& ds ,int i){
         return ds[i]<0?i:ds[i]=find(ds,ds[i]);
     }
     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
-        vector<int> ds(s.size(),-1);
         vector<vector<int>> m(s.size());
-        for(auto p:pairs){
+        vector<int> ds(s.size(),-1);
+        for(auto& p:pairs){
             auto i=find(ds,p[0]),j=find(ds,p[1]);
-            if(i!=j)
+            if(i!=j){
+                ds[i]+=ds[j];
                 ds[j]=i;
+            }
         }
         for(int i=0;i<s.size();++i)
             m[find(ds,i)].push_back(i);
-        for(auto ids:m){
+        for(auto& ids:m){
             string ss="";
-            for(auto id:ids)
+            for(int id:ids)
                 ss+=s[id];
-            sort(begin(ss),end(ss));
+            sort(ss.begin(),ss.end());
             for(int i=0;i<ids.size();++i)
                 s[ids[i]]=ss[i];
         }
