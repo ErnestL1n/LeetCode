5a35f2f0c9e00eac2400a3199a53e975aff9b257
ErnestL1n <ernestlin1998@gmail.com>
2021-01-14 23:14:54 +0800

    update

--- Solutions/1337. The K Weakest Rows in a Matrix/1337. The K Weakest Rows in a Matrix.cpp
+++ Solutions/1337. The K Weakest Rows in a Matrix/1337. The K Weakest Rows in a Matrix.cpp
@@ -12,3 +12,39 @@ public:
         return res;
     }
 };
+
+
+
+// priority queue + binary search
+typedef pair<int,int> pr;
+class Solution {
+public:
+    struct cmp{
+        bool operator()(const pr& a,const pr& b){
+            return a.first!=b.first?a.first>b.first:a.second>b.second;
+        }
+    };
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        priority_queue<pr,vector<pr>,cmp> pq;
+        vector<int> res;
+        for(int i=0;i<mat.size();++i){
+            pq.push({numOnes(mat[i]),i});
+        }
+        while(k-->0){
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        return res;
+    }
+    int numOnes(vector<int>& row){
+        int l=0,r=row.size()-1;
+        while(l<=r){
+            int m=(l+r)/2;
+            if(row[m]==1)
+                l=m+1;
+            else
+                r=m-1;
+        }
+        return l;
+    }
+};
