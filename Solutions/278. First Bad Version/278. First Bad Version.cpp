584ccf692a82da10375104e39e733256d21efb7a
ErnestL1n <ernestlin1998@gmail.com>
2021-01-15 23:55:16 +0800

    update

--- Solutions/278. First Bad Version/278. First Bad Version.cpp
+++ Solutions/278. First Bad Version/278. First Bad Version.cpp
@@ -1,9 +1,17 @@
-public int firstBadVersion(int n) {
-    int start = 1, end = n;
-    while (start < end) {
-        int mid = start + (end-start) / 2;
-        if (!isBadVersion(mid)) start = mid + 1;
-        else end = mid;            
-    }        
-    return start;
-}
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        unsigned l=1,r=n;
+        while(l<=r){
+            unsigned m=(l+r)/2;
+            if(isBadVersion(m))
+                r=m-1;
+            else
+                l=m+1;
+        }
+        return l;
+    }
+};
