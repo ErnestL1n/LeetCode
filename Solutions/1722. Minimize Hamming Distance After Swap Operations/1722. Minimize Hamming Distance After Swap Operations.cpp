0aabc1774e05bf378c03937fac1c793506dd69e5
ErnestL1n <ernestlin1998@gmail.com>
2021-01-15 23:31:49 +0800

    update

--- Solutions/1722. Minimize Hamming Distance After Swap Operations/1722. Minimize Hamming Distance After Swap Operations.cpp
+++ Solutions/1722. Minimize Hamming Distance After Swap Operations/1722. Minimize Hamming Distance After Swap Operations.cpp
@@ -4,22 +4,23 @@ public:
         return ds[i]<0?i:ds[i]=find(ds,ds[i]);
     }
     int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
-        int sz=source.size(),res=0;
-        vector<int> ds(sz,-1);
+        int res=0;
+        vector<int> ds(source.size(),-1);
         for(auto& sw:allowedSwaps){
             int i=find(ds,sw[0]),j=find(ds,sw[1]);
             if(i!=j){
+                if(ds[i]>ds[j])
+                    swap(i,j);
                 ds[i]+=ds[j];
                 ds[j]=i;
             }
         }
-        vector<unordered_multiset<int>> cycles(sz);
-        for(int i=0;i<sz;++i)
+        vector<multiset<int>> cycles(source.size());
+        for(int i=0;i<source.size();++i)
             if(source[i]!=target[i])
                 cycles[find(ds,i)].insert(source[i]);
-        for(int i=0;i<sz;++i){
+        for(int i=0;i<target.size();++i){
             if(source[i]!=target[i]){
-                // cycle here represents a hash multiset
                 auto& cycle=cycles[find(ds,i)];
                 auto it=cycle.find(target[i]);
                 if(it!=cycle.end())
