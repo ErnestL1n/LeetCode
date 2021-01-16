ec37dbc5399b5dd7070962f4278dd44e47e65eb2
ErnestL1n <ernestlin1998@gmail.com>
2021-01-16 23:13:46 +0800

    update

---
+++ Solutions/1424. Diagonal Traverse II/1424. Diagonal Traverse II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        vector<int> res;
+        map<int,vector<int>> m;
+        int diaN=0;
+        for(int i=0;i<nums.size();++i)
+            for(int j=0;j<nums[i].size();++j){
+                m[i+j].push_back(nums[i][j]);
+                diaN=max(diaN,i+j);
+            }
+        for(int i=0;i<=diaN;++i)
+            for(auto it=m[i].rbegin();it!=m[i].rend();++it)
+                res.push_back(*it);
+        return res;
+    }
+};
