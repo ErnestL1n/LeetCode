542390f055f229d36711dab248bcd801150ddd19
ErnestL1n <ernestlin1998@gmail.com>
2021-01-18 00:53:24 +0800

    update

--- Solutions/1727. Largest Submatrix With Rearrangements/1727. Largest Submatrix With Rearrangements.cpp
+++ Solutions/1727. Largest Submatrix With Rearrangements/1727. Largest Submatrix With Rearrangements.cpp
@@ -14,3 +14,24 @@ public:
         return res;
     }
 };
+
+
+// update , or copy into new vector
+class Solution {
+public:
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int res=0;
+        for(int i=0;i<matrix.size();++i){
+            for(int j=0;j<matrix[0].size();++j){
+                if(i>0&&matrix[i][j]!=0)
+                    matrix[i][j]=matrix[i-1][j]+1;
+            }
+            vector<int> foo;
+            foo=matrix[i];
+            sort(begin(foo),end(foo),greater<int>());
+            for(int col=0;col<matrix[0].size();++col)
+                res=max(res,(col+1)*foo[col]);
+        }
+        return res;
+    }
+};
