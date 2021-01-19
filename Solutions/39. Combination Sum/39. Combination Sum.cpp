7fa1888f30bd5954b136911d6d6da5115f3f1738
ErnestL1n <ernestlin1998@gmail.com>
2021-01-19 23:47:21 +0800

    update

---
+++ Solutions/39. Combination Sum/39. Combination Sum.cpp
@@ -0,0 +1,26 @@
+typedef vector<vector<int>> VVI;
+typedef vector<int> VI;
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& c, int t) {
+        VVI res;
+        VI path;
+        foo(res,path,c,t,0);
+        return res;
+    }
+    void foo(VVI& res,VI& path,VI& c,int t,int begin){
+        if(t<=0){
+            if(!t){
+                res.push_back(path);
+                return;
+            }
+            return;
+        }
+        for(int i=begin;i<c.size();++i){
+            path.push_back(c[i]);
+            foo(res,path,c,t-c[i],i);
+            path.pop_back();
+        }
+        return;
+    }
+};
