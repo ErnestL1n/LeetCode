33e87d8f82d03ced7e01ad8457c368665ef03a97
ErnestL1n <ernestlin1998@gmail.com>
2021-01-25 21:50:09 +0800

    update

--- Solutions/129. Sum Root to Leaf Numbers/129. Sum Root to Leaf Numbers.cpp
+++ Solutions/129. Sum Root to Leaf Numbers/129. Sum Root to Leaf Numbers.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //Recursive
 class Solution {
 public:
     int sumNumbers(TreeNode* root) {
@@ -19,5 +20,30 @@ public:
         if(n->right==nullptr&&n->left==nullptr)
             return s*10+n->val;
         return sum(n->left,s*10+n->val)+sum(n->right,s*10+n->val);
+    }
+};
+
+//BFS queue
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        queue<TreeNode*> q;
+        int res=0;
+        if(!root)return 0;
+        q.push(root);
+        while(q.size()){
+            auto curr=q.front();q.pop();
+            if(curr->left==nullptr&&curr->right==nullptr)
+                res+=curr->val;
+            if(curr->left){
+                curr->left->val+=10*curr->val;
+                q.push(curr->left);
+            }
+            if(curr->right){
+                curr->right->val+=10*curr->val;
+                q.push(curr->right);
+            }
+        }
+        return res;
     }
 };
