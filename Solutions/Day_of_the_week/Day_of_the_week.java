20b9c9a440b086c2398b20f817b22cf13c824868
ErnestL1n <ernestlin1998@gmail.com>
2021-01-26 21:59:41 +0800

    Update Day_of_the_week.java

--- Solutions/Day_of_the_week/Day_of_the_week.java
+++ Solutions/Day_of_the_week/Day_of_the_week.java
@@ -16,18 +16,18 @@ public class Day_of_the_week {
 	 */
 	public static String dayOfTheWeek(int d, int m, int y) {
 		if (m < 3) {
-            m += 12;
-            y -= 1;
-        }
-        int c = y / 100;
-        y = y % 100;
-        int w = (c / 4 - 2 * c + y + y / 4 + 13 * (m + 1) / 5 + d - 1) % 7;
-        return days[(w + 7) % 7];
+            		m += 12;
+            		y -= 1;
+        	}
+        	int c = y / 100;
+        	y = y % 100;
+        	int w = (c / 4 - 2 * c + y + y / 4 + 13 * (m + 1) / 5 + d - 1) % 7;
+        	return days[(w + 7) % 7];
 	}
 	public static void main(String[] args) {
 		int day=31,month=8,year=2019;
 		System.out.println("day = "+day+" , month = "+month+", year = "+year);
-        System.out.println("Today is "+dayOfTheWeek(day,month,year));
+        	System.out.println("Today is "+dayOfTheWeek(day,month,year));
 	}
 
 }
