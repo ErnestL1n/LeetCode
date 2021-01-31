f46d81bce8d9cfaba6cb68369628c1c1c0b4fc4d
ErnestL1n <ernestlin1998@gmail.com>
2021-01-31 16:50:55 +0800

    update

--- Solutions/1745. Palindrome Partitioning IV/1745. Palindrome Partitioning IV.cpp
+++ Solutions/1745. Palindrome Partitioning IV/1745. Palindrome Partitioning IV.cpp
@@ -18,3 +18,23 @@ public:
         return true;
     }
 };
+
+//DP 
+class Solution {
+public:
+    bool isPal[2000][2000]={};
+    bool checkPartitioning(string s) {
+        for(int i=s.size()-1;i>=0;--i){
+            for(int j=i;j<s.size();++j)
+                isPal[i][j]=s[i]==s[j]?(i+1>=j)||isPal[i+1][j-1]:false;
+        }
+        for(int i=2;i<s.size();++i){
+            if(isPal[i][s.size()-1]){
+                for(int j=1;j<i;++j)
+                    if(isPal[0][j-1]&&isPal[j][i-1])
+                        return true;
+            }
+        }
+        return false;
+    }
+};
