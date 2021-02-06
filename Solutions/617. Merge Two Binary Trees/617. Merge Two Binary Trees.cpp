f1fcd70eeeee4bf492b2fc4bfbc88aa8fb567e27
ErnestL1n <ernestlin1998@gmail.com>
2021-02-06 23:30:22 +0800

    update

--- Solutions/617. Merge Two Binary Trees/617. Merge Two Binary Trees.cpp
+++ Solutions/617. Merge Two Binary Trees/617. Merge Two Binary Trees.cpp
@@ -20,3 +20,32 @@ public:
         return t1;
     }
 };
+
+
+
+typedef array<TreeNode*,2> ar;
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if(!t1)return t2?t2:nullptr;
+        if(!t2)return t1;
+        stack<ar> stk;
+        stk.push({t1,t2});
+        while(stk.size()){
+            auto& curr=stk.top();
+            stk.pop();
+            if(!curr[0]||!curr[1])
+                continue;
+            curr[0]->val+=curr[1]->val;
+            if(!curr[0]->left&&curr[1]->left)
+                curr[0]->left=curr[1]->left;
+            else
+                stk.push({curr[0]->left,curr[1]->left});
+            if(!curr[0]->right&&curr[1]->right)
+                curr[0]->right=curr[1]->right;
+            else
+                stk.push({curr[0]->right,curr[1]->right});
+        }
+        return t1;
+    }
+};
