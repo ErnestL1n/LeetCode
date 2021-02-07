e826863ead8f3e1da3066de4a67ff1696f2ec443
ErnestL1n <ernestlin1998@gmail.com>
2021-02-08 00:10:17 +0800

    update

--- Solutions/905. Sort Array By Parity/905. Sort Array By Parity.cpp
+++ Solutions/905. Sort Array By Parity/905. Sort Array By Parity.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& A) {
         for(int i=0,j=0;j<A.size();++j){
-            if(A[j]%2==0){
-                int tmp=A[i];
-                A[i++]=A[j];
-                A[j]=tmp;
-            }
+            if(A[j]%2==0)
+                swap(A[j],A[i++]);
         }
         return A;
     }
--- Solutions/905. Sort Array By Parity/905. Sort Array By Parity.java
+++ Solutions/905. Sort Array By Parity/905. Sort Array By Parity.java
@@ -1,55 +1,16 @@
-/**
- * 
- */
-package github.com.ErnestL1n;
-
-import java.util.Arrays;
-
-/**
- * @author https://github.com/ErnestL1n
- *
- */
-//Leetcode.905
-public class Sort_Array_By_Parity {
-
-	/**
-	 * @param args
-	 */
-	/*
-	public static int[] sortArrayByParity(int[] A) {
-		straight forward
-		int[] temp=new int[A.length];
-		int evencount=0,oddstart=0;
-		for(int i=0;i<A.length;i++) 
-			if(A[i]%2==0)
-				temp[evencount++]=A[i];
-		oddstart+=evencount;
-		for(int i=0;i<A.length;i++)
-			if(A[i]%2!=0)
-				temp[oddstart++]=A[i];
-		return temp;
-		
-	}
-	*/
-	
-	//may use much memory space,provided by https://leetcode.com/lee215/
-	public static int[] sortArrayByParity(int[] A) {
-		for(int i=0,j=0;j<A.length;j++) {
-			if(A[j]%2==0) {
-				int temp=A[i];
-				A[i++]=A[j];
-				A[j]=temp;
-			}
-		}
-		return A;
-	}
-	
-	public static void main(String[] args) {
-		int[] A=new int[]{3,1,2,4};
-		System.out.println("Given an array A of non-negative integers "+ Arrays.toString(A));
-
-		System.out.println("You may return any answer array that satisfies this condition: all the even elements of A, followed by all the odd elements of A.");
-		
-		System.out.println("Given an array A of non-negative integers "+ Arrays.toString(sortArrayByParity(A)));
-	}
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        for(int i=0,j=0;j<A.length;++j){
+            if(A[j]%2==0){
+                swap(A,i,j);
+                ++i;
+            }
+        }
+        return A;
+    }
+    private void swap(int[] a,int i,int j){
+        int tmp=a[i];
+        a[i]=a[j];
+        a[j]=tmp;
+    }
 }
