8528112d0d131c1290d3a74447f7a2b173c8b23a
ErnestL1n <ernestlin1998@gmail.com>
2021-02-07 23:45:58 +0800

    update

--- Solutions/1518. Water Bottles/1518. Water Bottles.cpp
+++ Solutions/1518. Water Bottles/1518. Water Bottles.cpp
@@ -1,6 +1,24 @@
+//Trick
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
         return numBottles+(numBottles-1)/(numExchange-1);
+    }
+};
+
+
+
+//straight forward
+class Solution {
+public:
+    int numWaterBottles(int bts, int ex) {
+        int res=bts;
+        while(bts>=ex){
+            int rem=bts%ex;
+            bts/=ex;
+            res+=bts;
+            bts+=rem;
+        }
+        return res;
     }
 };
