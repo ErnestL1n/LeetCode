bc40e2598cc857c061502fd1ea42cae322ba615b
ErnestL1n <ernestlin1998@gmail.com>
2021-02-08 23:31:30 +0800

    update

--- Solutions/1000. Minimum Cost to Merge Stones/1000. Minimum Cost to Merge Stones.java
+++ Solutions/1000. Minimum Cost to Merge Stones/1000. Minimum Cost to Merge Stones.java
@@ -1,27 +1,19 @@
-/**
- * 
- */
-package github.com.ErnestL1n;
-import java.util.*;
-/**
- * @author https://github.com/ErnestL1n
- *
- */
-//Leetcode.1553
-public class Minimum_Number_of_Days_to_Eat_N_Oranges {
-
-	/**
-	 * @param args
-	 */
-    private Map<Integer,Integer> dp=new HashMap<>();
-    public int minDays(int n) {
-        if(n<=1)
-            return n;
-        if(!dp.containsKey(n))
-            dp.put(n,1+Math.min(n%2+minDays(n/2),n%3+minDays(n/3)));
-        return dp.get(n);
+class Solution {
+    public int mergeStones(int[] stones, int K) {
+        int n=stones.length;
+        if((n-1)%(K-1)!=0)return -1;
+        int[] prefix=new int[n+1];
+        for(int i=1;i<=n;++i)
+            prefix[i]=prefix[i-1]+stones[i-1];
+        int[][] dp=new int[n][n];
+        for(int len=K;len<=n;++len)
+            for(int i=0,j=i+len-1;j<n;++i,++j){
+                dp[i][j]=Integer.MAX_VALUE;
+                for(int mid=i;mid<j;mid+=K-1)
+                    dp[i][j]=Math.min(dp[i][j],dp[i][mid]+dp[mid+1][j]);
+                if((j-i)%(K-1)==0)
+                    dp[i][j]+=prefix[j+1]-prefix[i];
+            }
+        return dp[0][n-1];
     }
-	public static void main(String[] args) {
-	}
-
 }
