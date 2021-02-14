c888fa5df59502ced6dd89c2be17f9165aba08ee
ErnestL1n <ernestlin1998@gmail.com>
2021-02-14 23:45:29 +0800

    update

--- Solutions/1758. Minimum Changes To Make Alternating Binary String/1758. Minimum Changes To Make Alternating Binary String.cpp
+++ Solutions/1758. Minimum Changes To Make Alternating Binary String/1758. Minimum Changes To Make Alternating Binary String.cpp
@@ -10,3 +10,16 @@ public:
         return min(cnt1,cnt2);
     }
 };
+
+
+//Trick 
+class Solution {
+public:
+    int minOperations(string s) {
+        int res=0,n=s.size();
+        for(int i=0;i<n;++i)
+            if(s[i]-'0'!=i%2)
+                ++res;
+        return min(res,n-res);
+    }
+};
