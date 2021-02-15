5de7a2fc6cbc4b56558605725ab82dabdc280fcd
ErnestL1n <ernestlin1998@gmail.com>
2021-02-15 17:54:29 +0800

    Update 278. First Bad Version.cpp

--- Solutions/278. First Bad Version/278. First Bad Version.cpp
+++ Solutions/278. First Bad Version/278. First Bad Version.cpp
@@ -4,14 +4,14 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        unsigned l=0,r=n;
-        while(l<=r){
-            unsigned m=(l+r)/2;
-            if(isBadVersion(m))
-                r=m-1;
+        unsigned lo=1,hi=n;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(isBadVersion(mid))
+                hi=mid-1;
             else
-                l=m+1;
+                lo=mid+1;
         }
-        return l;
+        return lo;
     }
 };
