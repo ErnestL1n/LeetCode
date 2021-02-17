6449c15e1a24843740fb8159e03818736b3b2b37
ErnestL1n <ernestlin1998@gmail.com>
2021-02-17 13:32:13 +0800

    update

--- Solutions/9. Palindrome Number/9. Palindrome Number.cpp
+++ Solutions/9. Palindrome Number/9. Palindrome Number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-        bool isPalindrome(int x) {
-		    if (x<0 || (x!=0 && x%10==0)) return false;
-		    int rev = 0;
-		    while (x>rev){
-		    	rev = rev*10 + x%10;
-		    	x = x/10;
-		    }
-		    return (x==rev || x==rev/10);
-	}
+    bool isPalindrome(int x) {
+        if(x<0 || (x!=0&&x%10==0))
+            return false;
+        int half=0;
+        while(x>half){
+            half=x%10+half*10;
+            x/=10;
+        }
+        return x==half || x==half/10;
+    }
 };
-
 
 // int to string method
 class Solution {
