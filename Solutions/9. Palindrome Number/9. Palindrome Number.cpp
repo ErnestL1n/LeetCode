3816ad1d8cb88ca350700e110cd837e1b52a27a1
ErnestL1n <ernestlin1998@gmail.com>
2021-02-17 10:43:46 +0800

    update

--- Solutions/9. Palindrome Number/9. Palindrome Number.cpp
+++ Solutions/9. Palindrome Number/9. Palindrome Number.cpp
@@ -10,3 +10,16 @@ public:
 		    return (x==rev || x==rev/10);
 	}
 };
+
+
+// int to string method
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s=to_string(x);
+        for(int i=0,j=s.size()-1;i<=j;++i,--j)
+            if(s[i]!=s[j])
+                return false;
+        return true;
+    }
+};
