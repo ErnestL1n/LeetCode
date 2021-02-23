12310a38c16118c15d6dadc223518c2a61a7efaa
ErnestL1n <ernestlin1998@gmail.com>
2021-02-23 23:01:43 +0800

    update

--- Solutions/1207. Unique Number of Occurrences/1207. Unique Number of Occurrences.cpp
+++ Solutions/1207. Unique Number of Occurrences/1207. Unique Number of Occurrences.cpp
@@ -1,10 +1,30 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-      unordered_map<int, int> m;
-      unordered_set<int> s;
-      for (auto n : arr) ++m[n];
-      for (auto& p : m) s.insert(p.second);
-      return m.size() == s.size();
+        unordered_map<int,int> m;
+        unordered_set<int> s;
+        for(int a:arr)
+            ++m[a];
+        for(auto& p:m)
+            s.insert(p.second);
+        // cout <<m.size() << "、" <<s.size() <<"\n";
+        // arr=[1,2,2,1,1,3]  =>  3、3 
+        // arr=[1,2]  =>  2、1
+        return s.size()==m.size();
+    }
+};
+
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> m;
+        unordered_set<int> s;
+        for(int a:arr)
+            ++m[a];
+        for(auto& p:m)
+            if(!s.insert(p.second).second)
+                return false;
+        return true;
     }
 };
