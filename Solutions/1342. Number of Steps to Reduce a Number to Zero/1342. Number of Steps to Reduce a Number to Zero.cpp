5d2fc122658d5aeb615cab61ffa25c8894935b83
ErnestL1n <ernestlin1998@gmail.com>
2021-02-24 23:51:53 +0800

    update

--- Solutions/1342. Number of Steps to Reduce a Number to Zero/1342. Number of Steps to Reduce a Number to Zero.cpp
+++ Solutions/1342. Number of Steps to Reduce a Number to Zero/1342. Number of Steps to Reduce a Number to Zero.cpp
@@ -4,3 +4,16 @@ public:
         return num ? __builtin_popcount(num) + 31 - __builtin_clz(num) : 0;
     }
 };
+
+class Solution {
+public:
+    int numberOfSteps (int num) {
+		if(!num) return 0;
+        int res = 0;
+        while(num) {
+            res += (num & 1) ? 2 : 1;
+            num >>= 1;
+        }
+        return res - 1;
+    }
+};
