2e1fcf50de2d1c0fb623bd09182700234127a6c3
ErnestL1n <ernestlin1998@gmail.com>
2021-02-25 23:38:00 +0800

    update

--- Solutions/1512. Number of Good Pairs/1512. Number of Good Pairs.cpp
+++ Solutions/1512. Number of Good Pairs/1512. Number of Good Pairs.cpp
@@ -10,3 +10,25 @@ public:
         return res;
     }
 };
+
+
+//using map
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int,int> m;
+        int res=0;
+        for(auto n:nums)
+            res+=m[n]++;
+        return res;
+    }
+};
+
+//one line
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& A) {
+        return accumulate(A.begin(), A.end(), 0, [count = unordered_map<int, int> {}] (auto a, auto b) mutable { return a + count[b]++;  });
+    }
+};
