99d210f4fcc5b5d0430e518fe305512ad89aa5d3
ErnestL1n <ernestlin1998@gmail.com>
2021-03-03 23:54:25 +0800

    update

--- Solutions/938. Range Sum of BST/938. Range Sum of BST.cpp
+++ Solutions/938. Range Sum of BST/938. Range Sum of BST.cpp
@@ -41,3 +41,12 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(!root) return 0;
+        return (low<=root->val&&root->val<=high?root->val:0)+rangeSumBST(root->left,low,high)+rangeSumBST(root->right,low,high);
+    }
+};
+
