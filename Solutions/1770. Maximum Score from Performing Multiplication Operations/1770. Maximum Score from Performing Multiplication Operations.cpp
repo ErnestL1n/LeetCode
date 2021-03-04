9a135f387d10195daed2fd9a044f825063cb6a64
ErnestL1n <ernestlin1998@gmail.com>
2021-03-04 22:47:27 +0800

    update

--- Solutions/1770. Maximum Score from Performing Multiplication Operations/1770. Maximum Score from Performing Multiplication Operations.cpp
+++ Solutions/1770. Maximum Score from Performing Multiplication Operations/1770. Maximum Score from Performing Multiplication Operations.cpp
@@ -20,19 +20,37 @@ public:
 
 class Solution {
 public:
-    int dp[1001][1001] = {};
-    int dfs(vector<int>& nums, vector<int>& mults, int l, int i) {
-        if (i >= mults.size())
-            return 0;
-        if (!dp[l][i]) {
-            int r = nums.size() - 1 - (i - l);
-            dp[l][i] = max(nums[l] * mults[i] + dfs(nums, mults, l + 1, i + 1), 
-                nums[r] * mults[i] + dfs(nums, mults, l, i + 1));
-        }
-        return dp[l][i];
+    int dp[1000][1000]={};
+    int maximumScore(vector<int>& nums, vector<int>& muls) {
+        return dfs(nums,muls,0,0);
     }
-    int maximumScore(vector<int>& nums, vector<int>& mults) {
-        return dfs(nums, mults, 0, 0);
+    int dfs(vector<int>& nums,vector<int>&muls,int i,int l){
+        if(i>=muls.size())
+            return 0;
+        if(!dp[i][l]){
+            int r=nums.size()-1-(i-l);
+            dp[i][l]=max(nums[l]*muls[i]+dfs(nums,muls,i+1,l+1),
+                        nums[r]*muls[i]+dfs(nums,muls,i+1,l));
+        }
+        return dp[i][l];
+    }
+};
+
+
+class Solution {
+public:
+    int dp[1000][1000]={};
+    int maximumScore(vector<int>& nums, vector<int>& muls) {
+        return dfs(nums,muls,0,nums.size()-1,0);
+    }
+    int dfs(vector<int>& nums,vector<int>& muls,int lo,int hi,int n){
+        if(n==muls.size())
+            return 0;
+        if(!dp[lo][n]){
+            dp[lo][n]=max(dfs(nums,muls,lo+1,hi,n+1)+nums[lo]*muls[n],
+                  dfs(nums,muls,lo,hi-1,n+1)+nums[hi]*muls[n]);
+        }
+        return dp[lo][n];
     }
 };
 
