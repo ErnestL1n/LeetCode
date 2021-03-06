0a5df892fe3054adf458cdd9dc1652081dae07e5
ErnestL1n <ernestlin1998@gmail.com>
2021-03-06 15:49:01 +0800

    update

--- Solutions/1043. Partition Array for Maximum Sum/1043. Partition Array for Maximum Sum.cpp
+++ Solutions/1043. Partition Array for Maximum Sum/1043. Partition Array for Maximum Sum.cpp
@@ -1,3 +1,5 @@
+// O(N) space 
+// O(NK) time
 class Solution {
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int BIGK) {
@@ -14,5 +16,27 @@ public:
             dp[i]=best;
         }
         return dp[n];
+    }
+};
+
+
+// O(K) space 
+// O(NK) time
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int BIGK) {
+        int n=arr.size();
+        vector<int> dp(BIGK);
+        //dp[0]=1 ,best=max(dp[i-k]+k*maxcurr,best)
+        //dp[i]=best
+        for(int i=1;i<=n;++i){
+            int currMax=0,best=0;
+            for(int k=1;k<=BIGK&&k<=i;++k){
+                currMax=max(currMax,arr[i-k]);
+                best=max(best,dp[(i-k)%BIGK]+k*currMax);
+            }
+            dp[i%BIGK]=best;
+        }
+        return dp[n%BIGK];
     }
 };
