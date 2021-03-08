57818ed877849e8674b27be80afddf748b136170
ErnestL1n <ernestlin1998@gmail.com>
2021-03-08 23:33:40 +0800

    update

--- Solutions/1785. Minimum Elements to Add to Form a Given Sum/1785. Minimum Elements to Add to Form a Given Sum.cpp
+++ Solutions/1785. Minimum Elements to Add to Form a Given Sum/1785. Minimum Elements to Add to Form a Given Sum.cpp
@@ -11,3 +11,12 @@ public:
         return res;
     }
 };
+
+
+// 1-liner
+class Solution {
+public:
+    int minElements(vector<int>& nums, int limit, int goal) {
+        return (abs(accumulate(begin(nums),end(nums),0l)-goal)+limit-1)/limit;
+    }
+};
