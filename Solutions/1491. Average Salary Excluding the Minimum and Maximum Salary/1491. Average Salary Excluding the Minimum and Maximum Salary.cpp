25afcf663ce9c33b0c30cf17b21cf2284a9122ec
ErnestL1n <ernestlin1998@gmail.com>
2021-03-09 21:59:02 +0800

    update

--- Solutions/1491. Average Salary Excluding the Minimum and Maximum Salary/1491. Average Salary Excluding the Minimum and Maximum Salary.cpp
+++ Solutions/1491. Average Salary Excluding the Minimum and Maximum Salary/1491. Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -1,7 +1,17 @@
+// O(nlogn) sort
 class Solution {
 public:
     double average(vector<int>& salary) {
         sort(begin(salary),end(salary));
         return (double)accumulate(begin(salary)+1,end(salary)-1,0)/(salary.size()-2);
+    }
+};
+
+
+// with std::min_element and std::max_element
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        return (accumulate(begin(salary),end(salary),0.0)-*max_element(begin(salary),end(salary))-*min_element(begin(salary),end(salary)))/(salary.size()-2);
     }
 };
