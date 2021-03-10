fb8bbd5a2411e87421cdde2590ef26bacb5bc3b3
ErnestL1n <ernestlin1998@gmail.com>
2021-03-11 00:02:44 +0800

    update

--- Solutions/1253. Reconstruct a 2-Row Binary Matrix/1253. Reconstruct a 2-Row Binary Matrix.cpp
+++ Solutions/1253. Reconstruct a 2-Row Binary Matrix/1253. Reconstruct a 2-Row Binary Matrix.cpp
@@ -22,6 +22,22 @@ public:
             if(upper<0 || lower<0)
                 return vector<vector<int>>{};
         }
-        return upper==0&&lower==0?res:vector<vector<int>>{};
+        return upper==0&&lower==0?res:vector<vector<int>>();
+    }
+};
+
+
+//simplify
+
+class Solution {
+public:
+    vector<vector<int>> reconstructMatrix(int upper, int lower, vector<int>& colsum) {
+        int n=colsum.size();
+        vector<vector<int>> res(2,vector<int>(n));
+        for(int i=0;i<n;upper-=res[0][i],lower-=res[1][i],++i){
+            res[0][i]=colsum[i]==2 || (colsum[i]==1&&lower<upper);
+            res[1][i]=colsum[i]==2 || (colsum[i]==1&&!res[0][i]);
+        }
+        return upper==0&&lower==0?res:vector<vector<int>>();
     }
 };
