5630687e5a32571f22dd6e934fe45316591c8dd6
ErnestL1n <ernestlin1998@gmail.com>
2021-03-12 00:29:55 +0800

    update

--- Solutions/1304. Find N Unique Integers Sum up to Zero/1304. Find N Unique Integers Sum up to Zero.cpp
+++ Solutions/1304. Find N Unique Integers Sum up to Zero/1304. Find N Unique Integers Sum up to Zero.cpp
@@ -1,3 +1,25 @@
+//simple version
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> res;
+        int i=0,j=1;
+        if(n%2==1){
+            res.push_back(0);
+            ++i;
+        }
+        for(;i<n;i+=2,++j)
+            res.push_back(j),res.push_back(-j);
+        return res;
+    }
+};
+
+
+
+
+
+
+
 class Solution {
 public:
     vector<int> sumZero(int n) {
