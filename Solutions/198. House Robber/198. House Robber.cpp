021ee7e760ed643ca7dad35fd5f65e30c766394e
ErnestL1n <ernestlin1998@gmail.com>
2021-03-12 23:05:04 +0800

    update

--- Solutions/198. House Robber/198. House Robber.cpp
+++ Solutions/198. House Robber/198. House Robber.cpp
@@ -1,3 +1,35 @@
+// TLE naive version
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        return rob(nums,nums.size()-1);
+    }
+    int rob(vector<int>& nums,int i){
+        if(i<0)
+            return 0;
+        return max(nums[i]+rob(nums,i-2),rob(nums,i-1));
+    }
+};
+
+//top down version Recursive + memo
+class Solution {
+public:
+    vector<int> dp;
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        dp.assign(n,-1);
+        return rob(nums,n-1);
+    }
+    int rob(vector<int>& nums,int i){
+        if(i<0)
+            return 0;
+        if(dp[i]>=0)
+            return dp[i];
+        return dp[i]=max(rob(nums,i-1),rob(nums,i-2)+nums[i]);
+    }
+};
+
+//bottom up version Iterative + memo
 class Solution {
 public:
     int rob(vector<int>& nums) {
