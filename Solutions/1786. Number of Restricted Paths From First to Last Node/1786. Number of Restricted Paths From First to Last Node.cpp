b00ba49a898c2bf1dcf1641a5b78a8e370d0f32b
ErnestL1n <ernestlin1998@gmail.com>
2021-03-13 23:46:13 +0800

    update

--- Solutions/1786. Number of Restricted Paths From First to Last Node/1786. Number of Restricted Paths From First to Last Node.cpp
+++ Solutions/1786. Number of Restricted Paths From First to Last Node/1786. Number of Restricted Paths From First to Last Node.cpp
@@ -1,51 +1,37 @@
-//TLE
+//dfs+bfs(dijkstra)
 typedef pair<int,int> pr;
 class Solution {
 public:
     int mod=1e9+7;
     int countRestrictedPaths(int n, vector<vector<int>>& edges) {
-        if(n==1)
-            return 0;
         vector<vector<pr>> G(n+1);
+        vector<int> dist(n+1),dp(n+1,-1);
         for(auto& e:edges){
             G[e[0]].push_back({e[1],e[2]});
             G[e[1]].push_back({e[0],e[2]});
         }
-        vector<int> dist=dijkstra(G,n);
-        vector<int> dp(n+1);
-        return dfs(1,n,G,dist,dp);
-    }
-    vector<int> dijkstra(vector<vector<pr>>& G,int n){
-        vector<int> dist(n+1,INT_MAX);
-        dist[n]=0;
         priority_queue<pr,vector<pr>,greater<pr>> pq;
         pq.push({0,n});
         while(pq.size()){
-            auto curr=pq.top();
-            pq.pop();
-            int d=curr.first,u=curr.second;
-            if(d!=dist[u])
-                continue;
-            for(auto& nei:G[u]){
-                int v=nei.first,w=nei.second;
-                if(dist[v]>dist[u]+w){
-                    dist[v]=dist[u]+w;
-                    pq.push({dist[v],v});
+            auto [dis,i]=pq.top();pq.pop();
+            for(auto [j,w]:G[i]){
+                if(j!=n&&(dist[j]==0||dist[j]>dist[i]+w)){
+                    dist[j]=dist[i]+w;
+                    pq.push({dist[j],j});
                 }
             }
         }
-        return dist;
+        return dfs(G,dist,dp,n);
     }
-    int dfs(int src,int n,vector<vector<pr>>& G,vector<int>& dist,vector<int>& dp){
-        if(dp[src])return dp[src];
-        if(src==n)return 1;
-        int res=0;
-        for(auto& nei:G[src]){
-            int v=nei.first,w=nei.second;
-            if(dist[src]>dist[v]){
-                res=(res+dfs(v,n,G,dist,dp))%mod;
-            }
+    int dfs(vector<vector<pr>>& G,vector<int>& dist,vector<int>& dp,int i){
+        if(i==1)
+            return 1;
+        if(dp[i]==-1){
+            dp[i]=0;
+            for(auto [j,w]:G[i])
+                if(dist[i]<dist[j])
+                    dp[i]=(dp[i]+dfs(G,dist,dp,j))%mod;
         }
-        return dp[src]=res;
+        return dp[i];
     }
 };
