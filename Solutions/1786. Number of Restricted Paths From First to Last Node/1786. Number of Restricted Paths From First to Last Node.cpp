67b693ec038545b698d7211a0da81787a67cbd14
ErnestL1n <ernestlin1998@gmail.com>
2021-03-13 23:58:41 +0800

    update

--- Solutions/1786. Number of Restricted Paths From First to Last Node/1786. Number of Restricted Paths From First to Last Node.cpp
+++ Solutions/1786. Number of Restricted Paths From First to Last Node/1786. Number of Restricted Paths From First to Last Node.cpp
@@ -24,6 +24,9 @@ public:
         return dfs(G,dist,dp,n);
     }
     int dfs(vector<vector<pr>>& G,vector<int>& dist,vector<int>& dp,int i){
+        //we begin from n
+        //thus distanceToLastNode(n) must less than 
+        //distanceToLastNode(1~n-1) to form a path
         if(i==1)
             return 1;
         if(dp[i]==-1){
@@ -33,5 +36,54 @@ public:
                     dp[i]=(dp[i]+dfs(G,dist,dp,j))%mod;
         }
         return dp[i];
+    }
+};
+
+
+
+//make dijkstra's algorithm a function
+typedef pair<int,int> pr;
+class Solution {
+public:
+    int mod=1e9+7;
+    int countRestrictedPaths(int n, vector<vector<int>>& edges) {
+        vector<vector<pr>> G(n+1);
+        vector<int> dp(n+1,-1);
+        for(auto& e:edges){
+            G[e[0]].push_back({e[1],e[2]});
+            G[e[1]].push_back({e[0],e[2]});
+        }
+        vector<int> dist=dijkstra(G,n);
+        return dfs(G,dist,dp,n);
+    }
+    vector<int> dijkstra(vector<vector<pr>>& G,int n){
+        vector<int> dist(n+1);
+        priority_queue<pr,vector<pr>,greater<pr>> pq;
+        pq.push({0,n});
+        while(pq.size()){
+            auto [dis,i]=pq.top();pq.pop();
+            for(auto [j,w]:G[i]){
+                if(j!=n&&(dist[j]==0||dist[j]>dist[i]+w)){
+                    dist[j]=dist[i]+w;
+                    pq.push({dist[j],j});
+                }
+            }
+        }
+        return dist;
+    }
+    int dfs(vector<vector<pr>>& G,vector<int>& dist,vector<int>& dp,int i){
+        //we begin from n
+        //thus distanceToLastNode(n) must less than 
+        //distanceToLastNode(1~n-1) to form a path
+        if(i==1)
+            return 1;
+        if(dp[i]==-1){
+            dp[i]=0;
+            for(auto [j,w]:G[i]){
+                if(dist[j]>dist[i])
+                    dp[i]+=dfs(G,dist,dp,j);       
+            }
+        }
+        return dp[i]%mod;
     }
 };
