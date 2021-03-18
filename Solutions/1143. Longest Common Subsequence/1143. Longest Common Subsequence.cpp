5c54b089dc075827eeabaa1e4f494a0b084e60c0
ErnestL1n <ernestlin1998@gmail.com>
2021-03-18 23:47:08 +0800

    update

--- Solutions/1143. Longest Common Subsequence/1143. Longest Common Subsequence.cpp
+++ Solutions/1143. Longest Common Subsequence/1143. Longest Common Subsequence.cpp
@@ -22,7 +22,7 @@ class Solution {
 public:
     int longestCommonSubsequence(string s1, string s2) {
         int n1=s1.size(),n2=s2.size();
-        vector<vector<short>> dp(2,vector<short>(1+(n1>n2?n1:n2)));
+        vector<vector<short>> dp(2,vector<short>(1+n2));
         for(int i=1;i<=n1;++i)
             for(int j=1;j<=n2;++j){
                 if(s1[i-1]==s2[j-1])
