b951e3bbe4e4ed325f7d8c83c109c5897bda8216
ErnestL1n <ernestlin1998@gmail.com>
2021-03-18 23:42:17 +0800

    update

--- Solutions/1143. Longest Common Subsequence/1143. Longest Common Subsequence.cpp
+++ Solutions/1143. Longest Common Subsequence/1143. Longest Common Subsequence.cpp
@@ -1,3 +1,4 @@
+//bottom up
 class Solution {
 public:
     int longestCommonSubsequence(string s1, string s2) {
@@ -30,5 +31,26 @@ public:
                     dp[i%2][j]=max(dp[(i-1)%2][j],dp[i%2][j-1]);
             }
         return dp[n1%2][n2];
+    }
+};
+
+
+//top down
+class Solution {
+public:
+    int longestCommonSubsequence(string& s1, string& s2) {
+        int n1=s1.size(),n2=s2.size();
+        vector<vector<short>> dp(n1+1,vector<short>(n2+1,-1));
+        return lcs(dp,s1,s2,n1,n2);
+    }
+    short lcs(vector<vector<short>>& dp,string& s1,string& s2,int i,int j){
+        if(i==0 or j==0)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        if(s1[i-1]==s2[j-1])
+            return dp[i][j]=1+lcs(dp,s1,s2,i-1,j-1);
+        else
+            return dp[i][j]=max(lcs(dp,s1,s2,i,j-1),lcs(dp,s1,s2,i-1,j));
     }
 };
