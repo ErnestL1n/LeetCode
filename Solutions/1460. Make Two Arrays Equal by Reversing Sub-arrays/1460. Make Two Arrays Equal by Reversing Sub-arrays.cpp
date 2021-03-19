2802a44352e7ab4e02d8ee76ae78409aa520d184
ErnestL1n <ernestlin1998@gmail.com>
2021-03-19 23:50:26 +0800

    update

--- Solutions/1460. Make Two Arrays Equal by Reversing Sub-arrays/1460. Make Two Arrays Equal by Reversing Sub-arrays.cpp
+++ Solutions/1460. Make Two Arrays Equal by Reversing Sub-arrays/1460. Make Two Arrays Equal by Reversing Sub-arrays.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        int record[1001]={};
+        for(int i=0;i<target.size();++i){
+            ++record[target[i]];
+            --record[arr[i]];
+        }
+        for(int i=0;i<1001;++i)
+            if(record[i]!=0)
+                return false;
+        return true;
+    }
+};
+
+//multiset
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
         return unordered_multiset<int>(target.begin(),target.end())==unordered_multiset<int>(arr.begin(),arr.end());
     }
 };
