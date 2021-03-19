5548cab65700b3632917b2efa254b2260d860f98
ErnestL1n <ernestlin1998@gmail.com>
2021-03-20 00:01:33 +0800

    update

--- Solutions/1277. Count Square Submatrices with All Ones/1277. Count Square Submatrices with All Ones.cpp
+++ Solutions/1277. Count Square Submatrices with All Ones/1277. Count Square Submatrices with All Ones.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<mat.size();++i)
             for(int j=0;j<mat[0].size();++j){
                 if(mat[i][j]>0&&i>0&&j>0)
-                    mat[i][j]+=min(mat[i-1][j-1],min(mat[i][j-1],mat[i-1][j]))+1;
+                    mat[i][j]=min({mat[i-1][j-1],mat[i][j-1],mat[i-1][j]})+1;
                 res+=mat[i][j];
             }
         return res;
