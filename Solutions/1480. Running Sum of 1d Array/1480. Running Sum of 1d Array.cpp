58324352cfbd7ebc0a8d0421bc10c6fd169e1a53
ErnestL1n <ernestlin1998@gmail.com>
2021-03-21 00:01:47 +0800

    update

--- Solutions/1480. Running Sum of 1d Array/1480. Running Sum of 1d Array.cpp
+++ Solutions/1480. Running Sum of 1d Array/1480. Running Sum of 1d Array.cpp
@@ -9,3 +9,11 @@ public:
 		return nums;
     }
 };
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        partial_sum(begin(nums), end(nums), begin(nums));
+        return nums;
+    }
+};
