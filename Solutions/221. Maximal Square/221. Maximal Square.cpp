d3e0d2ec4871b8179c14ff42efa571c926daf087
ErnestL1n <ernestlin1998@gmail.com>
2021-03-20 23:51:11 +0800

    update

--- Solutions/221. Maximal Square/221. Maximal Square.cpp
+++ Solutions/221. Maximal Square/221. Maximal Square.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& m) {
-        vector<vector<int>> dp(m.size(),vector<int>(m[0].size()));
-        int res=0;
-        for(int i=0;i<m.size();++i)
-            for(int j=0;j<m[0].size();++j){
-                if(m[i][j]-'0' && i>0 && j>0)
-                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+    int maximalSquare(vector<vector<char>>& mat) {
+        int m=mat.size(),n=mat[0].size();
+        int edge=0;
+        vector<vector<int>> dp(m,vector<int>(n));
+        for(int i=0;i<m;++i)
+            for(int j=0;j<n;++j){
+                if(i>0&&j>0&&mat[i][j]=='1')
+                    dp[i][j]=min({dp[i-1][j-1],dp[i][j-1],dp[i-1][j]})+1;
                 else
-                    dp[i][j]=m[i][j]-'0';
-                res=max(res,dp[i][j]);
+                    dp[i][j]=mat[i][j]=='1'?1:0;
+                edge=max(edge,dp[i][j]);
             }
-        return res*res;
+        return pow(edge,2);
     }
 };
