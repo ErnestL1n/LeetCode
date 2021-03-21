7b060e241eeba9dc848916ccbe01107c50910b43
ErnestL1n <ernestlin1998@gmail.com>
2021-03-21 23:25:23 +0800

    update

---
+++ Solutions/1801. Number of Orders in the Backlog/1801. Number of Orders in the Backlog.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int getNumberOfBacklogOrders(vector<vector<int>>& orders) {
+        priority_queue<vector<int>> buy;
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> sell;
+        for(auto& o:orders){
+            if(o[2]==0)
+                buy.push(o);
+            else
+                sell.push(o);
+            while(buy.size()&&sell.size()&&buy.top()[0]>=sell.top()[0]){
+                int k=min(buy.top()[1],sell.top()[1]);
+                vector<int> tmp=buy.top();buy.pop();
+                tmp[1]-=k;
+                if(tmp[1]>0)buy.push(tmp);
+                tmp=sell.top();sell.pop();
+                tmp[1]-=k;
+                if(tmp[1]>0)sell.push(tmp);
+            }
+        }
+        int res=0,mod=1e9+7;
+        while(sell.size())
+            res=(res+sell.top()[1])%mod,sell.pop();
+        while(buy.size())
+            res=(res+buy.top()[1])%mod,buy.pop();
+        return res;
+    }
+};
