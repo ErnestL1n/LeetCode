94cdc30ea7c63d24cc63119c3eab4ae56885f936
ErnestL1n <ernestlin1998@gmail.com>
2021-03-25 23:59:41 +0800

    update

--- Solutions/1039. Minimum Score Triangulation of Polygon/1039. Minimum Score Triangulation of Polygon.cpp
+++ Solutions/1039. Minimum Score Triangulation of Polygon/1039. Minimum Score Triangulation of Polygon.cpp
@@ -1,20 +1,27 @@
+//top down
 class Solution {
 public:
-    int dpTopDown[50][50];
-    int minScoreTriangulationTopDown(vector<int>& A) {
-        return help(A,0,A.size()-1);
+    int dp[50][50]={};
+    int minScoreTriangulation(vector<int>& values) {
+        return foo(values,0,values.size()-1);
     }
-    int help(vector<int>& arr,int i,int j){
-        if(j-i<=1)return 0;
-        if(!dpTopDown[i][j]){
-            dpTopDown[i][j]=INT_MAX;
+    int foo(vector<int>& values,int i,int j){
+        if(j-i<=1)
+            return 0;
+        if(!dp[i][j]){
+            dp[i][j]=INT_MAX;
             for(int k=i+1;k<j;++k)
-            dpTopDown[i][j]=min(dpTopDown[i][j],help(arr,i,k)+help(arr,k,j)+arr[i]*arr[k]*arr[j]);
+                dp[i][j]=min(dp[i][j],dp[i][k]+dp[k][j]+values[i]*values[k]*values[j]);
         }
-        return dpTopDown[i][j];
+        return dp[i][j];
     }
-	int minScoreTriangulationBottomUp(vector<int>& A) {
-		vector<vector<int>> dp(50,vector<int>(50));
+};
+
+//buttom up
+class Solution {
+public:
+    int minScoreTriangulation(vector<int>& A) {
+        vector<vector<int>> dp(50,vector<int>(50));
         for(int i=A.size()-1;i>=0;--i)
             for(int j=i+1;j<A.size();++j)
                 for(int k=i+1;k<j;++k)
