978480471471edf1b543dcb7a373548b62d776bc
ErnestL1n <ernestlin1998@gmail.com>
2021-03-25 23:54:38 +0800

    update

--- Solutions/1039. Minimum Score Triangulation of Polygon/1039. Minimum Score Triangulation of Polygon.java
+++ Solutions/1039. Minimum Score Triangulation of Polygon/1039. Minimum Score Triangulation of Polygon.java
@@ -1,45 +1,30 @@
-/**
- * 
- */
-package github.com.ErnestL1n;
-
-/**
- * @author https://github.com/ErnestL1n
- *
- */
-//Leetcode.1039
-public class Minimum_Score_Triangulation_of_Polygon {
-
-	/**
-	 * @param args
-	 */
-	private int[][] dpTopDown=new int[50][50];
-	public int minScoreTriangulationTopDown(int[] A) {
-        return help(A,0,A.length-1);
+//top down
+class Solution {
+    int[][] dp=new int[50][50];
+    public int minScoreTriangulation(int[] values) {
+        return foo(values,0,values.length-1);
     }
-	private int help(int[] a,int i,int j) {
-		if(j-i<=1)return 0;
-		if(dpTopDown[i][j]==0) {
-			dpTopDown[i][j]=Integer.MAX_VALUE;
-			for(int k=i+1;k<j;++k)
-				dpTopDown[i][j]=Math.min(dpTopDown[i][j], a[i]*a[k]*a[j]+help(a,i,k)+help(a,k,j));
-		}
-		return dpTopDown[i][j];
-	}
-	
-	public int minScoreTriangulationButtomUp(int[] A) {
-        int[][] dp=new int[50][50];
-        for(int i=A.length-1;i>=0;--i)
-            for(int j=i+1;j<A.length;++j)
+    private int foo(int[] values,int i,int j){
+        if(j-i<=1)
+            return 0;
+        if(dp[i][j]==0){
+            dp[i][j]=Integer.MAX_VALUE;
+            for(int k=i+1;k<j;++k)
+                dp[i][j]=Math.min(dp[i][j],values[i]*values[j]*values[k]+foo(values,i,k)+foo(values,k,j));
+        }
+        return dp[i][j];
+    }
+}
+
+//buttom up
+class Solution {
+    public int minScoreTriangulation(int[] values) {
+        int dp[][]=new int[50][50];
+        for(int i=values.length-1;i>=0;--i)
+            for(int j=i+1;j<values.length;++j)
                 for(int k=i+1;k<j;++k)
                     dp[i][j]=Math.min(dp[i][j]==0?Integer.MAX_VALUE:dp[i][j],
-                                     dp[i][k]+dp[k][j]+A[i]*A[k]*A[j]);
-        return dp[0][A.length-1];
+                                dp[i][k]+dp[k][j]+values[i]*values[k]*values[j]);
+        return dp[0][values.length-1];
     }
-	
-	
-	public static void main(String[] args) {
-		
-	}
-
 }
