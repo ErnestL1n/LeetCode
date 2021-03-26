a368d831dbfa72bd5c3c8cd04b4fad2d5af8b376
ErnestL1n <ernestlin1998@gmail.com>
2021-03-26 23:05:05 +0800

    update

---
+++ Solutions/1047. Remove All Adjacent Duplicates In String/1047. Remove All Adjacent Duplicates In String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res="";
+        for(auto& ch:s){
+            if(res.size()&&res.back()==ch)
+                res.pop_back();
+            else
+                res.push_back(ch);
+        }
+        return res;
+    }
+};
