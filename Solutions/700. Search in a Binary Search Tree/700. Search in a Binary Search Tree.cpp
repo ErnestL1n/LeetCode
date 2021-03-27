a7e48fd7e3ade3a5758ae08758983700e3638fc4
ErnestL1n <ernestlin1998@gmail.com>
2021-03-27 22:44:01 +0800

    update

--- Solutions/700. Search in a Binary Search Tree/700. Search in a Binary Search Tree.cpp
+++ Solutions/700. Search in a Binary Search Tree/700. Search in a Binary Search Tree.cpp
@@ -12,12 +12,11 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if(!root||root->val==val)
+        if(!root or root->val==val)
             return root;
-        else if(val<root->val)
+        if(root->val>val)
             return searchBST(root->left,val);
-        else
-            return searchBST(root->right,val);
+        return searchBST(root->right,val);
     }
 };
 
