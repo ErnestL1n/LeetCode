b693216e7453040aba953974cc5a0274cece4a7d
ErnestL1n <ernestlin1998@gmail.com>
2021-03-27 23:43:13 +0800

    update

--- Solutions/1582. Special Positions in a Binary Matrix/1582. Special Positions in a Binary Matrix.cpp
+++ Solutions/1582. Special Positions in a Binary Matrix/1582. Special Positions in a Binary Matrix.cpp
@@ -1,3 +1,22 @@
+//HashMap solution 
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int m=mat.size(),n=mat[0].size();
+        unordered_map<int,int> row,col;
+        for(int i=0;i<m;++i)
+            for(int j=0;j<n;++j)
+                if(mat[i][j]==1)
+                    ++row[i],++col[j];
+        int res=0;
+        for(int i=0;i<m;++i)
+            for(int j=0;j<n;++j)
+                if(mat[i][j]==1 && row[i]==1 &&col[j]==1)
+                    ++res;
+        return res;
+    }
+};
+
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
