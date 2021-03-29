5317ab45040a70b467bf93f93825762bbe67181e
ErnestL1n <ernestlin1998@gmail.com>
2021-03-29 22:49:14 +0800

    update

--- Solutions/617. Merge Two Binary Trees/617. Merge Two Binary Trees.cpp
+++ Solutions/617. Merge Two Binary Trees/617. Merge Two Binary Trees.cpp
@@ -13,8 +13,8 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        if(!t1)return t2?t2:nullptr;
-        if(!t2)return t1;
+        if(!t1 or !t2)
+            return t1?t1:t2?t2:nullptr;
         t1->left=mergeTrees(t1->left,t2->left);
         t1->right=mergeTrees(t1->right,t2->right);
         t1->val+=t2->val;
@@ -23,29 +23,19 @@ public:
 };
 
 
+
 //stack iterative
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        if(!t1)
-            return t2?t2:nullptr;
+        if(!t1 or !t2)
+            return t1?t1:t2?t2:nullptr;
         stack<array<TreeNode*,2>> stk;
         stk.push({t1,t2});
         while(stk.size()){
             auto curr=stk.top(); //copy value 
             stk.pop();
-            if(!curr[0]||!curr[1])
+            if(!curr[1])
                 continue;
             curr[0]->val+=curr[1]->val;
             if(!curr[0]->right)
