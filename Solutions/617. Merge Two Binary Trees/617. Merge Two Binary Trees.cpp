6502237580b69df0c4724173bae8886d2ff712a7
ErnestL1n <ernestlin1998@gmail.com>
2021-03-29 22:23:40 +0800

    update

--- Solutions/617. Merge Two Binary Trees/617. Merge Two Binary Trees.cpp
+++ Solutions/617. Merge Two Binary Trees/617. Merge Two Binary Trees.cpp
@@ -35,27 +35,26 @@ public:
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-typedef array<TreeNode*,2> ar;
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        if(!t1)return t2?t2:nullptr;
-        if(!t2)return t1; 
-        stack<ar> stk;
+        if(!t1)
+            return t2?t2:nullptr;
+        stack<array<TreeNode*,2>> stk;
         stk.push({t1,t2});
         while(stk.size()){
-            auto curr=stk.top();
+            auto curr=stk.top(); //copy value 
             stk.pop();
             if(!curr[0]||!curr[1])
                 continue;
             curr[0]->val+=curr[1]->val;
             if(!curr[0]->right)
                 curr[0]->right=curr[1]->right;
-            else 
+            else
                 stk.push({curr[0]->right,curr[1]->right});
             if(!curr[0]->left)
                 curr[0]->left=curr[1]->left;
-            else 
+            else
                 stk.push({curr[0]->left,curr[1]->left});
         }
         return t1;
