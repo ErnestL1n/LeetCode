fa0a7eef682076ceb373bdee8a2cf44c642627cb
ErnestL1n <ernestlin1998@gmail.com>
2021-03-30 23:53:46 +0800

    update

--- Solutions/1806. Minimum Number of Operations to Reinitialize a Permutation/1806. Minimum Number of Operations to Reinitialize a Permutation.cpp
+++ Solutions/1806. Minimum Number of Operations to Reinitialize a Permutation/1806. Minimum Number of Operations to Reinitialize a Permutation.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int reinitializePermutation(int n) {
+        int res=0;
+        vector<int> init(n),a(n);
+        iota(begin(init),end(init),0),iota(begin(a),end(a),0);
+        while(res==0 or a!=init){
+            for(int i=0;i<n;++i){
+                if(i%2==0)
+                    a[i]=a[i/2];
+                else
+                    a[i]=a[n/2+(i-1)/2];
+            }
+            ++res;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int reinitializePermutation(int n) {
         int res = 0, i = 1;
         while (res == 0 || i > 1) {
             if (i < n / 2)
