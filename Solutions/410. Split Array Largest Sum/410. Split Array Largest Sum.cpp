0b9574e8bf806a5fed3a40fcf8a54fd7774faba0
ErnestL1n <ernestlin1998@gmail.com>
2021-03-31 23:52:39 +0800

    update

---
+++ Solutions/410. Split Array Largest Sum/410. Split Array Largest Sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int m) {
+        int Max=0,sum=0;
+        for(int num:nums){
+            Max=max(Max,num);
+            sum+=num;
+        }
+        if(m==1)
+            return sum;
+        int l=Max,r=sum;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(valid(nums,mid,m))
+                r=mid-1;
+            else
+                l=mid+1;
+        }
+        return l;
+    }
+    bool valid(vector<int>& nums,int mid,int m){
+        int cnt=1,total=0;
+        for(int num:nums){
+            total+=num;
+            if(total>mid){
+                total=num;
+                ++cnt;
+                if(cnt>m)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
