0289a0bbab99f74134fe632dc0c23b942ba6ec78
ErnestL1n <ernestlin1998@gmail.com>
2021-04-05 13:28:33 +0800

    update

--- Solutions/1161. Maximum Level Sum of a Binary Tree/1161. Maximum Level Sum of a Binary Tree.java
+++ Solutions/1161. Maximum Level Sum of a Binary Tree/1161. Maximum Level Sum of a Binary Tree.java
@@ -1,127 +1,66 @@
 /**
- * 
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
  */
-package github.com.ErnestL1n;
-
-/**
- * @author https://github.com/ErnestL1n
- *
- */
-import java.util.*;
-//Leetcode.1161
-//reference:https://stackoverflow.com/users/11625748/steven   
-
-class TreeNode{
-    int value;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int value) { this.value = value;}
-    TreeNode(int value, TreeNode left, TreeNode right) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-     }
-}
-
-
-public class Maximum_Level_Sum_of_a_Binary_Tree {
-
-	/**
-	 * @param args
-	 */
-	public static int maxLevelSum(TreeNode root) {
-        Queue<TreeNode> queue=new LinkedList<>();
-        if(root==null)return 0;
-        queue.offer(root);
-        int temp=Integer.MIN_VALUE;
-        int treelevel=0;
-        int maxlevel=0;
-        int sum=0;
-        while(!queue.isEmpty()){
-            treelevel++;
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                TreeNode node=queue.poll();
-                sum+=node.value;
-                if(node.left!=null)queue.offer(node.left);
-                if(node.right!=null)queue.offer(node.right);
+//BFS , level order traversal
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        int maxsum=Integer.MIN_VALUE,level=0,maxlevel=0;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(q.size()>0){
+            ++level;
+            int sum=0,qsz=q.size();
+            while(qsz>0){
+                --qsz;
+                var tmp=q.poll();
+                sum+=tmp.val;
+                if(tmp.left!=null)
+                    q.offer(tmp.left);
+                if(tmp.right!=null)
+                    q.offer(tmp.right);
             }
-            if(sum>temp){
-                temp=sum;
-                maxlevel=treelevel;
+            if(maxsum<sum){
+                maxsum=sum;
+                maxlevel=level;
             }
-            sum=0;
         }
         return maxlevel;
     }
-	
-	//  BFS version
-	/*
-    public int maxLevelSum(TreeNode root) {
-        Queue<TreeNode> q=new LinkedList<>();
-        q.offer(root);
-        int level=1, sum=root.val, l=1;
-        while(!q.isEmpty()) {
-            int size=q.size(), cur=0;
-            for(int i=0;i<size;i++) {
-                TreeNode tmp=q.poll();
-                cur+=tmp.val;
-                if(tmp.left!=null) q.offer(tmp.left);
-                if(tmp.right!=null) q.offer(tmp.right);
-            }
-            if(cur>sum) {
-                sum=cur;
-                level=l;
-            }
-            l++;
-        }
-        return level;
-    }
-	*/
-	
-	public static TreeNode Build(Integer[] array) {
-        if (array == null || array.length==0) {
-            return null;
-        }
-
-        Queue<TreeNode> treeNodeQueue = new LinkedList<>();
-        Queue<Integer>  integerQueue  = new LinkedList<>();
-        for (int i = 1; i < array.length; i++) {
-            integerQueue.offer(array[i]);
-        }
-
-        TreeNode root = new TreeNode(array[0]);
-        treeNodeQueue.offer(root);
-        System.out.println("I am root ,value is "+root.value);
-        while (!integerQueue.isEmpty()){
-        	//Binary tree begins from left
-            Integer leftVal  = integerQueue.isEmpty() ? null : integerQueue.poll();
-            Integer rightVal = integerQueue.isEmpty() ? null : integerQueue.poll();
-            TreeNode current = treeNodeQueue.poll();
-            if (leftVal !=null) {
-                    TreeNode left = new TreeNode(leftVal);
-                    current.left = left;
-                    treeNodeQueue.offer(left);
-                    System.out.println("I am leftchild  of value "+current.value+" , my value is "+left.value);
-            }
-            if (rightVal !=null){
-                    TreeNode right = new TreeNode(rightVal);
-                    current.right = right;
-                    treeNodeQueue.offer(right);
-                    System.out.println("I am rightchild of value "+current.value+" , my value is "+right.value);
-            }
-        }
-        return root;
-    }
-	
-	
-	
-	public static void main(String[] args) {
-		Integer[] array = new Integer[]{1,7,0,7,-8,null,null};
-		System.out.println("Input  array is: \n"+Arrays.toString(array));
-		System.out.println("\nMaxSum level is "+maxLevelSum(Build(array)));
-
-	}
-
 }
+
+
+	
+//DFS
+class Solution {
+    List<Integer> list=new ArrayList<>();
+    public int maxLevelSum(TreeNode root) {
+        dfs(root,0);
+        int maxlevel=0;
+        for(int i=0;i<list.size();++i)
+            if(list.get(i)>list.get(maxlevel))
+                maxlevel=i;
+        return maxlevel+1;
+    }
+    private void dfs(TreeNode root,int level){
+        if(root==null)return;
+        if(level==list.size())list.add(root.val);
+        else list.set(level,list.get(level)+root.val);
+        dfs(root.left,level+1);
+        dfs(root.right,level+1);
+    }
+}
+
+	
+
