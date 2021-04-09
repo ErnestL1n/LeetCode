ddaada6beb00a1aa1002e839216ed0f19b3ec3ca
ErnestL1n <ernestlin1998@gmail.com>
2021-04-09 23:46:07 +0800

    update

--- Solutions/102. Binary Tree Level Order Traversal/102. Binary Tree Level Order Traversal.java
+++ Solutions/102. Binary Tree Level Order Traversal/102. Binary Tree Level Order Traversal.java
@@ -35,3 +35,21 @@ class Solution {
         return res;
     }
 }
+
+//DFS
+class Solution {
+    private List<List<Integer>> res=new ArrayList<>();
+    public List<List<Integer>> levelOrder(TreeNode root){
+        dfs(root,0);
+        return res;
+    }
+    private void dfs(TreeNode root,int depth){
+        if(root==null)
+        return;
+        if(res.size()==depth)
+        res.add(new ArrayList<>());
+        res.get(depth).add(root.val);
+        dfs(root.left,depth+1);
+        dfs(root.right,depth+1);
+    }
+}
