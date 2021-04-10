2b71d9a5fcf34d6e46d5b1f115d1e4c04cf53634
ErnestL1n <ernestlin1998@gmail.com>
2021-04-10 23:51:51 +0800

    update

---
+++ Solutions/1450. Number of Students Doing Homework at a Given Time/1450. Number of Students Doing Homework at a Given Time.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int cnt=0;
+        for(int i=0;i<startTime.size();++i)
+            if(endTime[i]>=queryTime and startTime[i]<=queryTime)
+                ++cnt;
+        return cnt;
+    }
+};
