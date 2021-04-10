531513fb64dbddf3ba9d3171035cf7a8c112cb17
ErnestL1n <ernestlin1998@gmail.com>
2021-04-10 23:58:05 +0800

    update

--- Solutions/1295. Find Numbers with Even Number of Digits/1295. Find Numbers with Even Number of Digits.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
-        cnt=0
-        for i in range(len(startTime)):
-            if endTime[i]>=queryTime and startTime[i]<=queryTime:
-                cnt+=1
-        return cnt
