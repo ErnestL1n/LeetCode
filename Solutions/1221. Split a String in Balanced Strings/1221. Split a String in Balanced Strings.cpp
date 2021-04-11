eac4ecb6bf4de22d73ffe90128f2892e09cd54e1
ErnestL1n <ernestlin1998@gmail.com>
2021-04-12 00:01:56 +0800

    update

--- Solutions/1221. Split a String in Balanced Strings/1221. Split a String in Balanced Strings.cpp
+++ Solutions/1221. Split a String in Balanced Strings/1221. Split a String in Balanced Strings.cpp
@@ -13,3 +13,19 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int res=0,lcnt=0,rcnt=0;
+        for(int i=0;i<s.size();++i){
+            if(s[i]=='R')
+                ++rcnt;
+            else if(s[i]=='L')
+                ++lcnt;
+            if(rcnt!=0 and rcnt==lcnt)
+                ++res;
+        }
+        return res;
+    }
+};
