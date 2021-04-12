259650fe50bdb443e15ebf5c59c992f26fca8cfd
ErnestL1n <ernestlin1998@gmail.com>
2021-04-12 23:33:09 +0800

    update

--- Solutions/1823. Find the Winner of the Circular Game/1823. Find the Winner of the Circular Game.cpp
+++ Solutions/1823. Find the Winner of the Circular Game/1823. Find the Winner of the Circular Game.cpp
@@ -15,3 +15,13 @@ public:
         return i  + 1;
     }
 };
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        int res=0;
+        for(int i=1;i<=n;++i)
+            res=(res+k)%i;
+        return res+1;
+    }
+};
