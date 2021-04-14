90853bcebbfcd44b38061906204b7d5a35eabc66
ErnestL1n <ernestlin1998@gmail.com>
2021-04-14 23:39:05 +0800

    update

--- Solutions/1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree/1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
+++ Solutions/1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree/1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
@@ -19,3 +19,24 @@ public:
         getTargetCopy(original->right,cloned->right,target);
     }
 };
+
+
+//queue
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        if(!original)return nullptr;
+        queue<pair<TreeNode*,TreeNode*>> q;
+        q.push({original,cloned});
+        while(q.size()){
+            auto cur=q.front();q.pop();
+            if(cur.first==target)
+                return cur.second;
+            if(cur.first->left)
+                q.push({cur.first->left,cur.second->left});
+            if(cur.first->right)
+                q.push({cur.first->right,cur.second->right});   
+        }
+        return nullptr;
+    }
+};
