1002545421567fa048be2d9da54bc2a991c6137b
ErnestL1n <ernestlin1998@gmail.com>
2021-04-20 23:58:19 +0800

    update

--- Solutions/509. Fibonacci Number/509. Fibonacci Number.java
+++ Solutions/509. Fibonacci Number/509. Fibonacci Number.java
@@ -23,3 +23,18 @@ class Solution {
 		return dp[n];
 	}
 }
+
+class Solution {
+	private Integer[] dp = new Integer[31];
+	public int fib(int n) {
+		if(n<=1)return n;
+		dp[0]=0;
+		dp[1]=1;
+		return memo(n);
+	}
+	private int memo(int n) {
+		if(dp[n]!=null)return dp[n];
+		dp[n]=memo(n-1)+memo(n-2);
+		return memo(n);
+	}
+}
