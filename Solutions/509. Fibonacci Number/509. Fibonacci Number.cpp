d8035dd0163466450692cb91a315b58b9e26e9db
ErnestL1n <ernestlin1998@gmail.com>
2021-04-21 00:15:00 +0800

    update

--- Solutions/509. Fibonacci Number/509. Fibonacci Number.cpp
+++ Solutions/509. Fibonacci Number/509. Fibonacci Number.cpp
@@ -25,3 +25,23 @@ public:
         return dp[n];
     }
 };
+
+//Top-Down Approach using Memoization
+class Solution {
+public:
+    vector<int> dp;
+    int fib(int n) {
+        dp=vector<int>(31,-1);
+        if(n<=1)
+            return n;
+        dp[0]=0;
+        dp[1]=1;
+        return memo(n);
+    }
+    int memo(int n){
+        if(dp[n]!=-1)
+            return dp[n];
+        dp[n]=memo(n-1)+memo(n-2);
+        return memo(n);
+    }
+};
--- Solutions/509. Fibonacci Number/509. Fibonacci Number.java
+++ Solutions/509. Fibonacci Number/509. Fibonacci Number.java
@@ -28,13 +28,15 @@ class Solution {
 class Solution {
 	private Integer[] dp = new Integer[31];
 	public int fib(int n) {
-		if(n<=1)return n;
+		if(n<=1)
+			return n;
 		dp[0]=0;
 		dp[1]=1;
 		return memo(n);
 	}
 	private int memo(int n) {
-		if(dp[n]!=null)return dp[n];
+		if(dp[n]!=null)
+			return dp[n];
 		dp[n]=memo(n-1)+memo(n-2);
 		return memo(n);
 	}
