3bf5536739f7b5db09ea23a756661c2fa3d8800a
ErnestL1n <ernestlin1998@gmail.com>
2021-04-21 00:16:14 +0800

    update

--- Solutions/509. Fibonacci Number/509. Fibonacci Number.cpp
+++ Solutions/509. Fibonacci Number/509. Fibonacci Number.cpp
@@ -45,3 +45,23 @@ public:
         return memo(n);
     }
 };
+
+//Iterative Top-Down Approach
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1)
+            return n;
+        if(n==2)
+            return 1;
+        int cur=0;
+        int prev1=1;
+        int prev2=1;
+        for(int i=3;i<=n;++i){
+            cur=prev1+prev2;
+            prev2=prev1;
+            prev1=cur;
+        }
+        return cur;
+    }
+};
