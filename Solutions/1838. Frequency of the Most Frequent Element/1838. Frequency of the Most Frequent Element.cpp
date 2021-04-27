b2a234c4065a7f13c4eb27a1f52f758edeb939b5
ErnestL1n <ernestlin1998@gmail.com>
2021-04-27 23:25:16 +0800

    update

--- Solutions/1838. Frequency of the Most Frequent Element/1838. Frequency of the Most Frequent Element.cpp
+++ Solutions/1838. Frequency of the Most Frequent Element/1838. Frequency of the Most Frequent Element.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    int maxFrequency(vector<int>& A, long k) {
+        int i = 0, j;
+        sort(A.begin(), A.end());
+        for (j = 0; j < A.size(); ++j) {
+            k += A[j];
+            if (k < (long)A[j] * (j - i + 1))
+                k -= A[i++];
+        }
+        return j - i;
+    }
+};
+
+class Solution {
+public:
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
         int incre=0;
