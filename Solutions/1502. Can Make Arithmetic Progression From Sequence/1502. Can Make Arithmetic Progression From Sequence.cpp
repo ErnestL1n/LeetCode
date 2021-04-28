7943ccc0b14fdceea8edef124640aff0f13a5fad
ErnestL1n <ernestlin1998@gmail.com>
2021-04-28 23:56:34 +0800

    update

--- Solutions/1502. Can Make Arithmetic Progression From Sequence/1502. Can Make Arithmetic Progression From Sequence.cpp
+++ Solutions/1502. Can Make Arithmetic Progression From Sequence/1502. Can Make Arithmetic Progression From Sequence.cpp
@@ -20,3 +20,15 @@ public:
         return true;
     }
 };
+
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        for(int i=1;arr.size()-1;++i)
+            if(arr[i-1]-arr[i]!=arr[i]-arr[i+1])
+                return false;
+        return true;
+    }
+};
