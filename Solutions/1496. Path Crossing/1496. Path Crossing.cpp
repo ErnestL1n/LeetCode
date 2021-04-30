bafcb784340395a45ee75e6078a708d1ec9cc5cd
ErnestL1n <ernestlin1998@gmail.com>
2021-04-30 23:55:38 +0800

    update

--- Solutions/1496. Path Crossing/1496. Path Crossing.cpp
+++ Solutions/1496. Path Crossing/1496. Path Crossing.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     bool isPathCrossing(string path) {
-        int x = 0,y=0;
-        set<pair<int,int>>s;
+        int x=0,y=0;
+        set<pair<int,int>> s;
         s.insert({0,0});
-        for(char p: path){
-            if(p == 'N') 
+        for(const auto& p:path){
+            if(p=='N')
                 ++y;
-            else if(p == 'S')
+            else if(p=='S')
                 --y;
-            else if(p == 'E') ++x;
-            else --x;
-            
-            if(s.find({x,y}) != s.end()) return true;
-            else s.insert({x,y});
+            else if(p=='E')
+                ++x;
+            else
+                --x;
+            if(s.find({x,y})!=s.end())
+                return true;
+            s.insert({x,y});
         }
         return false;
     }
