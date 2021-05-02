1621974bc816a78d2e3687bd8cd2a9aecca305c7
ErnestL1n <ernestlin1998@gmail.com>
2021-05-02 23:09:23 +0800

    update

--- Solutions/1844. Replace All Digits with Characters/1844. Replace All Digits with Characters.cpp
+++ Solutions/1844. Replace All Digits with Characters/1844. Replace All Digits with Characters.cpp
@@ -6,3 +6,13 @@ public:
         return s;
     }
 };
+
+//simplify
+class Solution {
+public:
+    string replaceDigits(string s) {
+        for(int i=1;i<s.size();i+=2)
+            s[i]+=s[i-1]-'0';
+        return s;
+    }
+};
