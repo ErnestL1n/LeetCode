0b217d1aca296f67db1dac5e508b2419d192aecc
ErnestL1n <ernestlin1998@gmail.com>
2021-05-04 22:53:40 +0800

    update

--- Solutions/448. Find All Numbers Disappeared in an Array/448. Find All Numbers Disappeared in an Array.cpp
+++ Solutions/448. Find All Numbers Disappeared in an Array/448. Find All Numbers Disappeared in an Array.cpp
@@ -11,3 +11,17 @@ public:
         return res;
     }
 };
+
+
+//HashSet
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> res;
+        unordered_set<int> s(nums.begin(),nums.end());
+        for(int i=1;i<=nums.size();++i)
+            if(s.find(i)==s.end())
+                res.push_back(i);
+        return res;
+    }
+};
