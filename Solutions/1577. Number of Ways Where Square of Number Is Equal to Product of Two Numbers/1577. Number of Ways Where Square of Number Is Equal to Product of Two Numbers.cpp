927cb525aa9f5fa88118ab0d9d18e05e0c47d0cb
ErnestL1n <ernestlin1998@gmail.com>
2021-05-05 23:57:20 +0800

    update

--- Solutions/1577. Number of Ways Where Square of Number Is Equal to Product of Two Numbers/1577. Number of Ways Where Square of Number Is Equal to Product of Two Numbers.cpp
+++ Solutions/1577. Number of Ways Where Square of Number Is Equal to Product of Two Numbers/1577. Number of Ways Where Square of Number Is Equal to Product of Two Numbers.cpp
@@ -22,3 +22,21 @@ public:
         return res/2;
     }
 };
+
+class Solution {
+public:
+        int numTriplets(vector<int>& n1, vector<int>& n2) {
+        return accumulate(begin(n1), end(n1), 0, [&](int s, long n) { return s + twoProduct(n * n, n2); }) 
+            + accumulate(begin(n2), end(n2), 0, [&](int s, long n) { return s +  twoProduct(n * n, n1); });
+    }
+    int twoProduct(long i, vector<int> &n) {
+        unordered_map<int, int> m;
+        int cnt = 0;
+        for (auto v : n) {
+            if (i % v == 0)
+                cnt += m[i / v];
+            ++m[v];
+        }
+        return cnt;
+    } 
+};
