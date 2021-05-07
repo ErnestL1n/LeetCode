acf94788f37cdd7bf19ef8fffc6e6dec3bd14c69
ErnestL1n <ernestlin1998@gmail.com>
2021-05-07 23:42:17 +0800

    update

---
+++ Solutions/9. Palindrome Number/9. Palindrome Number.go
@@ -0,0 +1,11 @@
+func isPalindrome(x int) bool {
+    if x<0 || (x!=0&&x%10==0){
+        return false
+    }
+    half:=0
+    for x>half{
+        half=half*10+x%10
+        x/=10
+    }
+    return half==x || half/10==x
+}
