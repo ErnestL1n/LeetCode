1092d52b549408d71da204804c0adb4ab50c8beb
ErnestL1n <ernestlin1998@gmail.com>
2021-05-07 23:42:22 +0800

    update

---
+++ Solutions/9. Palindrome Number/9. Palindrome Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       if(x<0||x%10==0) 
+           return false;
+        int half=0;
+        while(x>half){
+            half=half*10+x%10;
+            x/=10;
+        }
+        return half==x || half/10==x;
+    }
+}
