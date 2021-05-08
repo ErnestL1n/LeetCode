af0296a48bf61ec448c70f31350be4cd01c70af6
ErnestL1n <ernestlin1998@gmail.com>
2021-05-08 22:51:22 +0800

    update

--- Solutions/40. Combination Sum II/40. Combination Sum II.cpp
+++ Solutions/40. Combination Sum II/40. Combination Sum II.cpp
@@ -3,22 +3,25 @@ typedef vector<int> VI;
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
         VVI res;
         VI path;
+        sort(begin(candidates),end(candidates));
         foo(candidates,target,res,path,0);
         return res;
     }
-    void foo(VI& candidates,int target,VVI& res,VI& path,int start){
-        if(!target){
-            res.push_back(path);
+    void foo(VI& c,int t,VVI& res,VI& path,int begin){
+        if(t<=0){
+            if(t==0){
+                res.push_back(path);
+                return;
+            }
             return;
         }
-        if(target<0){return;}
-        for(int i=start;i<candidates.size();++i){
-            if(i>start&&candidates[i]==candidates[i-1])continue;
-            path.push_back(candidates[i]);
-            foo(candidates,target-candidates[i],res,path,i+1);
+        for(int i=begin;i<c.size();++i){
+            if(i>begin and c[i]==c[i-1])
+                continue;
+            path.push_back(c[i]);
+            foo(c,t-c[i],res,path,i+1);
             path.pop_back();
         }
     }
