9f69329a1be96925f3e28bbdeb80f35333dac1a9
ErnestL1n <ernestlin1998@gmail.com>
2021-05-10 00:32:37 +0800

    update

--- Solutions/1855. Maximum Distance Between a Pair of Values/1855. Maximum Distance Between a Pair of Values.py
+++ Solutions/1855. Maximum Distance Between a Pair of Values/1855. Maximum Distance Between a Pair of Values.py
@@ -1,5 +1,26 @@
 class Solution:
     def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        def bs(x,nums):
+            l,r=0,len(nums)-1
+            while l<=r:
+                mid=(l+r)//2
+                if x>nums[mid]:
+                    r=mid-1
+                else:
+                    l=mid+1
+            return r
+        res=0
+        for i in range(len(nums1)):
+            tmp=bs(nums1[i],nums2)
+            if tmp<i:
+                continue
+            if res<tmp-i:
+                res=tmp-i
+        return res
+
+        
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
         i,j,n1,n2,res=0,0,len(nums1),len(nums2),0
         while i<n1 and j<n2:
             if nums1[i]>nums2[j]:
