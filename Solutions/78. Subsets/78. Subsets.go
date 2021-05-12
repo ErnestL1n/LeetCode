b3522567d3a64f64d5703379acd79ec849de2f1e
ErnestL1n <ernestlin1998@gmail.com>
2021-05-12 23:59:23 +0800

    update

---
+++ Solutions/78. Subsets/78. Subsets.go
@@ -0,0 +1,14 @@
+func subsets(nums []int) [][]int {
+    var res [][]int
+    var tmp []int
+    foo(nums,&res,tmp,0)
+    return res
+}
+func foo(nums []int,res *[][]int,tmp []int,begin int){
+    (*res)=append(*res,tmp)
+    for i:=begin;i<len(nums);i+=1{
+        tmp=append(tmp,nums[i])
+        foo(nums,res,tmp,i+1)
+        tmp=tmp[:len(tmp)-1]
+    }
+}
