5fcd3938b8ebff3d242d41ef38dd53e479d55384
ErnestL1n <ernestlin1998@gmail.com>
2021-05-12 23:50:18 +0800

    update

--- Solutions/78. Subsets/78. Subsets.py
+++ Solutions/78. Subsets/78. Subsets.py
@@ -1,11 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def foo(res,tmp,begin):
-            res.append(tmp)
+        def foo(nums,res,tmp,begin):
+            res.append(tmp.copy())
             for i in range(begin,len(nums)):
                 tmp.append(nums[i])
-                foo(res,tmp,begin+1)
+                foo(nums,res,tmp,i+1)
                 tmp.pop()
-        res,tmp=[[]],[]
-        foo(nums,res,tmp)
+        res,tmp=[],[]
+        foo(nums,res,tmp,0)
         return res
