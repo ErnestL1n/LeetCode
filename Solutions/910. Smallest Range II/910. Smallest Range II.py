9be50a1ca02dd34c261495dcce7ff7e535952be5
ErnestL1n <ernestlin1998@gmail.com>
2021-05-14 23:59:39 +0800

    update

--- Solutions/910. Smallest Range II/910. Smallest Range II.py
+++ Solutions/910. Smallest Range II/910. Smallest Range II.py
@@ -2,7 +2,7 @@ class Solution:
     def smallestRangeII(self, nums: List[int], k: int) -> int:
         nums.sort()
         res,right,left=nums[len(nums)-1]-nums[0],nums[len(nums)-1]-k,nums[0]+k,
-        for i in range(0,len(nums));
+        for i in range(len(nums)-1):
             m_ax=max(right,nums[i]+k)
             m_in=min(left,nums[i+1]-k)
             res=min(res,m_ax-m_in)
