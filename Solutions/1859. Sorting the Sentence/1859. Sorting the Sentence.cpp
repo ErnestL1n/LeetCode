4ce8a6585f9508c1d81ae4b4f14544413a9fedf4
ErnestL1n <ernestlin1998@gmail.com>
2021-05-15 23:46:26 +0800

    update

---
+++ Solutions/1859. Sorting the Sentence/1859. Sorting the Sentence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        map<int,string> m;
+        for(int i=0;i<s.size();++i){
+            string tmp="";
+            while(!isdigit(s[i])){
+                tmp+=s[i++];
+            }
+            m[s[i]]=tmp;
+            ++i;
+        }
+        string res="";
+        for(auto [k,v]:m){
+            res+=v;
+            res+=' ';
+        }
+        return res.substr(0,res.size()-1); 
+    }
+};
