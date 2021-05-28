332830f202d238c31ed60b3a8fa58260ac6b9f3b
ErnestL1n <ernestlin1998@gmail.com>
2021-05-28 23:08:08 +0800

    update

--- Solutions/278. First Bad Version/278. First Bad Version.java
+++ Solutions/278. First Bad Version/278. First Bad Version.java
@@ -3,9 +3,9 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        long lo=1,hi=n;
+        int lo=1,hi=n;
         while(lo<=hi){
-            long mid=(lo+hi)/2;
+            int mid=lo+(hi-lo)/2;
             if(isBadVersion(mid))
                 hi=mid-1;
             else
