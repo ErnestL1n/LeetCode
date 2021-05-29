d4857e8d72ad924ae4e70d66aaec3cbbdc6929f1
ErnestL1n <ernestlin1998@gmail.com>
2021-05-29 23:58:31 +0800

    update

--- Solutions/1140. Stone Game II/1140. Stone Game II.go
+++ Solutions/1140. Stone Game II/1140. Stone Game II.go
@@ -4,13 +4,23 @@ func stoneGameII(piles []int) int {
     for i,_:=range(dp){dp[i]=make([]int,n+1)}
     sufsum:=make([]int,n+1)
     for i:=n-1;i>=0;i-=1{
-        sufsum[i]=sufsum[i+1]+piles[i];
+        sufsum[i]=sufsum[i+1]+piles[i]
     }
-    for i:=0;i<=n;i+=1
-            dp[i][n]=sufsum[i];
-        for(int i=n-1;i>=0;--i)
-            for(int j=n-1;j>=1;--j)
-                for(int X=1;X<=2*j&&i+X<=n;++X)
-                    dp[i][j]=Math.max(dp[i][j],sufsum[i]-dp[i+X][Math.max(j,X)]);
-        return dp[0][1];
+    for i:=0;i<=n;i+=1{
+        dp[i][n]=sufsum[i]
+    }
+    for i:=n-1;i>=0;i-=1{
+        for j:=n-1;j>=1;j-=1{
+            for X:=1;X<=2*j&&i+X<=n;X+=1{
+                dp[i][j]=max(dp[i][j],sufsum[i]-dp[i+X][max(j,X)]);
+            }
+        }                
+    }
+    return dp[0][1];
+}
+func max(i,j int)int{
+    if(i>j){
+        return i
+    }
+    return j
 }
