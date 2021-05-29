2a51c8ba9792d8952b63e6e403be8659a58907fd
ErnestL1n <ernestlin1998@gmail.com>
2021-05-29 23:25:32 +0800

    update

--- Solutions/1140. Stone Game II/1140. Stone Game II.java
+++ Solutions/1140. Stone Game II/1140. Stone Game II.java
@@ -9,7 +9,7 @@ class Solution {
             dp[i][n]=sufsum[i];
         for(int i=n-1;i>=0;--i)
             for(int j=n-1;j>=1;--j)
-                for(int X=1;X<=2*j&&i+X<=n;X++)
+                for(int X=1;X<=2*j&&i+X<=n;++X)
                     dp[i][j]=Math.max(dp[i][j],sufsum[i]-dp[i+X][Math.max(j,X)]);
         return dp[0][1];
     }
