eed7b7b2bddd17ff861db59daf5680bf504560b5
ErnestL1n <ernestlin1998@gmail.com>
2021-05-31 22:14:01 +0800

    update

--- Solutions/268. Missing Number/268. Missing Number.java
+++ Solutions/268. Missing Number/268. Missing Number.java
@@ -6,3 +6,12 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int res,i;
+        for(i=0,res=nums.length;i<nums.length;++i)
+            res^=i^nums[i];
+        return res;
+    }
+}
