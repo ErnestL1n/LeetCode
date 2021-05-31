c3029cf53ec129db71720d34e1893ed78b5bb11b
ErnestL1n <ernestlin1998@gmail.com>
2021-05-31 22:10:21 +0800

    update

--- Solutions/268. Missing Number/268. Missing Number.cpp
+++ Solutions/268. Missing Number/268. Missing Number.cpp
@@ -2,8 +2,8 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int res;
-        for(int i=0,res=nums.size();i<nums.size();++i)
+        int res,i;
+        for(i=0,res=nums.size();i<nums.size();++i)
             res+=i-nums[i];
         return res;
     }
