90d05f53f85628d06e5df99a1ec9ad687184da30
ErnestL1n <ernestlin1998@gmail.com>
2021-06-05 23:43:46 +0800

    update

--- Solutions/191. Number of 1 Bits/191. Number of 1 Bits.go
+++ Solutions/191. Number of 1 Bits/191. Number of 1 Bits.go
@@ -1,6 +1,18 @@
 func hammingWeight(n uint32) int {
     res:=0
     for n>0{
+        t:=n&1
+        if t==1{
+            res+=1
+        }
+        n>>=1
+    }
+    return res
+}
+
+func hammingWeight(n uint32) int {
+    res:=0
+    for n>0{
         res+=1
         n&=(n-1)
     }
