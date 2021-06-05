d292c56543372f11a9118240d6fc4b0de9205d2b
ErnestL1n <ernestlin1998@gmail.com>
2021-06-05 23:57:23 +0800

    update

---
+++ Solutions/191. Number of 1 Bits/191. Number of 1 Bits.java
@@ -0,0 +1,25 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int res=0;
+        //in java, we use n!=0
+        while(n!=0){
+            res+=n&1;
+            n>>>=1;
+        }
+        return res;
+    }
+}
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int res=0;
+        //in java, we use n!=0
+        while(n!=0){
+            ++res;
+            n&=(n-1);
+        }
+        return res;
+    }
+}
