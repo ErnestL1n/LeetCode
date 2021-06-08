ffb0709cd1081a54d9896defa13c81ee82236af7
ErnestL1n <ernestlin1998@gmail.com>
2021-06-08 21:07:31 +0800

    update

--- Solutions/1338. Reduce Array Size to The Half/1338. Reduce Array Size to The Half.cpp
+++ Solutions/1338. Reduce Array Size to The Half/1338. Reduce Array Size to The Half.cpp
@@ -1,3 +1,4 @@
+//easy version
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
@@ -10,6 +11,25 @@ public:
         while(acc<n/2){
             acc+=cnt[i++];
             ++res;
+        }
+        return res;
+    }
+};
+
+//hashmap+multiset
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        int res=0,n=arr.size(),cnt=0;
+        unordered_map<int,int> m;
+        multiset<int,greater<int>> s;
+        for(const auto& a:arr)
+            ++m[a];
+        for(const auto& p:m)
+            s.insert(p.second);
+        for(auto it=s.begin();cnt<n/2;++it){
+            ++res;
+            cnt+=*it;
         }
         return res;
     }
