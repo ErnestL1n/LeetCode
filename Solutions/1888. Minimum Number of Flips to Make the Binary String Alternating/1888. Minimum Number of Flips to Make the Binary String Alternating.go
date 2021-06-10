fcaee674efcf5fa16bf7b6a113ccd9df00c532fb
ErnestL1n <ernestlin1998@gmail.com>
2021-06-10 11:59:48 +0800

    update

--- Solutions/1888. Minimum Number of Flips to Make the Binary String Alternating/1888. Minimum Number of Flips to Make the Binary String Alternating.go
+++ Solutions/1888. Minimum Number of Flips to Make the Binary String Alternating/1888. Minimum Number of Flips to Make the Binary String Alternating.go
@@ -39,3 +39,49 @@ func min(a,b int)int{
     }
     return b
 }
+
+
+
+//speed up a little bit
+func minFlips(s string) int {
+    n,cnt1,cnt2:=len(s),0,0
+    res:=n
+    for i:=0;i<2*n;i+=1{
+        c:=s[i%n]=='1'
+        var cc bool
+        if i%2==0{
+            cc=false
+        }else{
+            cc=true
+        }
+        if c!=cc{
+            cnt1+=1
+        }else{
+            cnt2+=1
+        }
+        if i>=n{
+            idx:=i-n
+            if idx%2==0{
+                cc=false
+            }else{
+                cc=true
+            }
+            c=s[idx]=='1'
+            if c!=cc{
+                cnt1-=1
+            }else{
+                cnt2-=1
+            }
+        }
+        if i>=n-1{
+            res=min(res,min(cnt1,cnt2))
+        }
+    }
+    return res
+}
+func min(a,b int)int{
+    if a<b{
+        return a
+    }
+    return b
+}
