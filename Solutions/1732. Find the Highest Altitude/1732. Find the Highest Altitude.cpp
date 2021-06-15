7ec88d15bf096908324793a80d39b51f42eb92d7
ErnestL1n <ernestlin1998@gmail.com>
2021-06-15 23:49:21 +0800

    update

--- Solutions/1732. Find the Highest Altitude/1732. Find the Highest Altitude.cpp
+++ Solutions/1732. Find the Highest Altitude/1732. Find the Highest Altitude.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int res=0,cnt=0;
-        for(int g:gain){
-            cnt+=g;
-            res=max(res,cnt);
+        int res=0,curr=0;
+        for(const auto& g:gain){
+            curr+=g;
+            res=max(res,curr);
         }
         return res;
     }
