8a0c19d6421be93f7241c2ee1e0c337b82123b40
ErnestL1n <ernestlin1998@gmail.com>
2021-06-18 22:08:45 +0800

    update

--- Solutions/1662. Check If Two String Arrays are Equivalent/1662. Check If Two String Arrays are Equivalent.cpp
+++ Solutions/1662. Check If Two String Arrays are Equivalent/1662. Check If Two String Arrays are Equivalent.cpp
@@ -34,3 +34,20 @@ public:
         return arridx1==word1.size() and arridx2==word2.size();
     }
 };
+
+//shorter
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        int arridx1=0,arridx2=0,idx1=0,idx2=0;
+        while(arridx1<word1.size() and arridx2<word2.size()){
+            if(word1[arridx1][idx1]!=word2[arridx2][idx2])
+                return false;
+            idx1=(++idx1)%word1[arridx1].size();
+            idx2=(++idx2)%word2[arridx2].size();
+            if(idx1==0)++arridx1;
+            if(idx2==0)++arridx2;
+        }
+        return arridx1==word1.size() and arridx2==word2.size();
+    }
+};
