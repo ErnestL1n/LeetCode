98903744dac1da1ef32c22b3b423dbf7fb4663e5
ErnestL1n <ernestlin1998@gmail.com>
2021-06-20 23:18:12 +0800

    update

--- Solutions/322. Coin Change/322. Coin Change.cpp
+++ Solutions/322. Coin Change/322. Coin Change.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,amount+1);
+        vector<int> dp(amount+1,amount+9487);
         dp[0]=0;
         for(int i=1;i<=amount;++i)
-            for(int j=0;j<coins.size();++j)
-                if(coins[j]<=i)
-                    dp[i]=min(dp[i],dp[i-coins[j]]+1);
-        return dp[amount]>amount?-1:dp[amount];
+            for(const auto& c:coins)
+                if(i>=c)
+                    dp[i]=min(dp[i],1+dp[i-c]);
+        return dp[amount]==amount+9487?-1:dp[amount];
     }
 };
