6215482f0bba2644d2c5e37ea25be46516ae74f8
ErnestL1n <ernestlin1998@gmail.com>
2021-06-20 23:04:48 +0800

    update

--- Solutions/1903. Largest Odd Number in String/1903. Largest Odd Number in String.cpp
+++ Solutions/1903. Largest Odd Number in String/1903. Largest Odd Number in String.cpp
@@ -12,3 +12,18 @@ public:
         return s;
     }
 };
+
+//AC
+class Solution {
+public:
+    string largestOddNumber(string s){
+        int n=s.size(),len=n;
+        for(int i=n-1;i>=0;--i){
+            if((s[i]-'0')%2==0)
+                len=i;
+            else
+                break;
+        }
+        return s.substr(0,len);
+    }
+};
