46a790976d19803b7d3e0ef6de6de6ea7c4bdb7f
ErnestL1n <ernestlin1998@gmail.com>
2021-06-22 12:30:19 +0800

    update

--- Solutions/1886. Determine Whether Matrix Can Be Obtained By Rotation/1886. Determine Whether Matrix Can Be Obtained By Rotation.cpp
+++ Solutions/1886. Determine Whether Matrix Can Be Obtained By Rotation/1886. Determine Whether Matrix Can Be Obtained By Rotation.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
-        vector<bool> c(4,true);
+        bool a,b,c,d;
+        a=b=c=d=true;
         int n=mat.size();
         for(int i=0;i<n;++i)
             for(int j=0;j<n;++j){
+                // 0 degree
                 if(mat[i][j]!=target[i][j])
-                    c[0]=false;
-                if(mat[i][j]!=target[n-1-j][i])
-                    c[1]=false;
-                if(mat[i][j]!=target[j][n-1-i])
-                    c[2]=false;
-                if(mat[i][j]!=target[n-1-i][n-1-j])
-                    c[3]=false;
+                    a=false;
+                // 90 degree
+                if(mat[j][n-1-i]!=target[i][j])
+                    b=false;
+                // 180 degree
+                if(mat[n-1-i][n-1-j]!=target[i][j])
+                    c=false;
+                // 270 degree
+                if(mat[n-1-j][i]!=target[i][j])
+                    d=false;
             }
-        return c[0] or c[1] or c[2] or c[3];
+        return a or b or c or d;
     }
 };
