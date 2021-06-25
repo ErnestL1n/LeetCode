2fb87e0d6a9064bc4d549fb80d8d46ea280750b0
ErnestL1n <ernestlin1998@gmail.com>
2021-06-25 23:50:34 +0800

    update

---
+++ Solutions/684. Redundant Connection/684. Redundant Connection.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> parent(2001);
+        for(int i=0;i<2001;++i)
+            parent[i]=i;
+        for(const auto& e:edges){
+            int f=e[0],t=e[1];
+            if(find(parent,f)==find(parent,t))
+                return e;
+            else
+                parent[find(parent,f)]=find(parent,t);
+        }
+        return {-1,-1};
+    }
+    int find(vector<int>& parent,int f){
+        if(f!=parent[f])
+            parent[f]=find(parent,parent[f]);
+        return parent[f];
+    }
+};
