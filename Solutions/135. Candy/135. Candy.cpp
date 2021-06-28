6510911fea2b221151281d62a0cc7d55bdaad9ae
ErnestL1n <ernestlin1998@gmail.com>
2021-06-28 12:47:08 +0800

    update

--- Solutions/135. Candy/135. Candy.cpp
+++ Solutions/135. Candy/135. Candy.cpp
@@ -14,3 +14,29 @@ public:
         return res;
     }
 };
+
+//trick solution,one pass
+class Solution {
+public:
+    int candy(vector<int>& r) {
+        int res=1,up=0,down=0,peek=0;
+        for(int i=1;i<r.size();++i){
+            if(r[i]>r[i-1]){
+                peek=++up;
+                down=0;
+                res+=1+up;
+            }else if(r[i]==r[i-1]){
+                down=peek=up=0;
+                res+=1;
+            }
+            else if(r[i]<r[i-1]){
+                ++down;
+                up=0;
+                res+=1+down+(peek>=down?-1:0);
+            }
+        }
+        //output candies order:[1,2,3,1,2,4]
+        //right output order:[1,2,4,3,2,1]
+        return res;
+    }
+};
