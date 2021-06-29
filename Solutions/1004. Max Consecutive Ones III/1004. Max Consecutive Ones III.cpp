ff5fb9287f04210d439e85785b1d421d8d37668f
ErnestL1n <ernestlin1998@gmail.com>
2021-06-29 17:50:35 +0800

    update

---
+++ Solutions/1004. Max Consecutive Ones III/1004. Max Consecutive Ones III.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i=0,j=0;
+        for(;j<nums.size();++j){
+            if(nums[j]==0)
+                --k;
+            if(k<0){
+                if(nums[i]==0){
+                    ++k;
+                }
+                ++i;
+            }
+        }
+        return j-i;
+    }
+};
+
+//clear version
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i=0,j=0;
+        for(;j<nums.size();++j){
+            if(nums[j]==0)
+                --k;
+            if(k<0 and nums[i++]==0)
+                ++k;
+        }
+        return j-i;
+    }
+};
