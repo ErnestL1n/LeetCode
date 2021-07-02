215c0a64fe058266dbbdcc18cbf97a05eec60fc0
ErnestL1n <ernestlin1998@gmail.com>
2021-07-02 13:07:39 +0800

    update

--- Solutions/236. Lowest Common Ancestor of a Binary Tree/236. Lowest Common Ancestor of a Binary Tree.cpp
+++ Solutions/236. Lowest Common Ancestor of a Binary Tree/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -21,3 +21,38 @@ public:
         return root;
     }
 };
+
+
+//bfs iterative
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        queue<TreeNode*> que;
+        que.push(root);
+        unordered_map<TreeNode*,TreeNode*> parent;
+        parent[root]=nullptr;
+        //if parent of p not found or parent of q not found
+        while(parent.find(p)==parent.end() or parent.find(q)==parent.end()){
+            auto cur=que.front();que.pop();
+            if(cur->left){
+                parent[cur->left]=cur;
+                que.push(cur->left);
+            }
+            if(cur->right){
+                parent[cur->right]=cur;
+                que.push(cur->right);
+            }
+        }
+        //elder
+        unordered_set<TreeNode*> ancestors;
+        while(p){
+            ancestors.insert(p);
+            p=parent[p];
+        }
+        //while q's parent not in ancestors
+        while(ancestors.find(q)==ancestors.end()){
+            q=parent[q];
+        }
+        return q;
+    }
+};
