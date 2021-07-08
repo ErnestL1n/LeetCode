a7d2207a3f1d7b22f55fbc6aab9fde018a11d981
ErnestL1n <ernestlin1998@gmail.com>
2021-07-08 11:58:33 +0800

    update

--- Solutions/436. Find Right Interval/436. Find Right Interval.cpp
+++ Solutions/436. Find Right Interval/436. Find Right Interval.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
-    vector<int> findRightInterval(vector<vector<int>>& its) {
-        int n=its.size();
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        vector<int> res(intervals.size());
         map<int,int> m;
-        vector<int> res(n);
-        for(int i=0;i<n;++i)
-            m[its[i][0]]=i;
-        for(int i=0;i<n;++i)
-            res[i]=m.lower_bound(its[i][1])==m.end()?-1:m.lower_bound(its[i][1])->second;
+        for(int i=0;i<intervals.size();++i)
+            m[intervals[i][0]]=i;
+        for(int i=0;i<intervals.size();++i){
+            auto it=m.lower_bound(intervals[i][1]);
+            if(it==m.end())
+                res[i]=-1;
+            else
+                res[i]=it->second;
+        }     
         return res;
     }
 };
