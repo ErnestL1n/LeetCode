6aee7c95eadd908ae6892751d46c5a9f460caef3
ErnestL1n <ernestlin1998@gmail.com>
2021-07-14 23:30:36 +0800

    update

--- Solutions/451. Sort Characters By Frequency/451. Sort Characters By Frequency.cpp
+++ Solutions/451. Sort Characters By Frequency/451. Sort Characters By Frequency.cpp
@@ -5,17 +5,16 @@ public:
         unordered_map<char,int> m;
         for(const auto& c:s)
             ++m[c];
-        //use int(bucket of size i) to access vector<char>(bucket)
+        //create bucket number from "0" to "s.size()+1"
         vector<vector<char>> bucket(s.size()+1);
         for(const auto& [k,v]:m)
             bucket[v].push_back(k);
         string res="";
+        //push_back from the largest size one
         for(int i=bucket.size()-1;i>=0;--i){
-            if(bucket[i].size()){
-                for(const auto& c:bucket[i])
-                    for(int j=0;j<i;++j)
-                        res+=c;
-            }
+            for(char c:bucket[i])
+                for(int j=0;j<i;++j)
+                    res+=c;
         }
         return res;
     }
