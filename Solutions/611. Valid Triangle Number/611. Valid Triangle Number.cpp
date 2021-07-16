ebf643a79e16ee2d1aec7247bb49ac5a11bb74c6
ErnestL1n <ernestlin1998@gmail.com>
2021-07-16 14:26:57 +0800

    Update 611. Valid Triangle Number.cpp

--- Solutions/611. Valid Triangle Number/611. Valid Triangle Number.cpp
+++ Solutions/611. Valid Triangle Number/611. Valid Triangle Number.cpp
@@ -7,6 +7,7 @@ public:
             int lo=0,hi=i-1;
             while(lo<hi){
                 if(nums[lo]+nums[hi]>nums[i]){
+                    //then lo~hi-1、hi、i are valid
                     res+=hi-lo;
                     --hi;
                 }else
