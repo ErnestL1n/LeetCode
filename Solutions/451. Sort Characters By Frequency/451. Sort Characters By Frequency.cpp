2f6c386a670ebca8574ba553fe07ec93f2405b06
ErnestL1n <ernestlin1998@gmail.com>
2021-07-16 14:51:12 +0800

    Update 451. Sort Characters By Frequency.cpp

--- Solutions/451. Sort Characters By Frequency/451. Sort Characters By Frequency.cpp
+++ Solutions/451. Sort Characters By Frequency/451. Sort Characters By Frequency.cpp
@@ -19,3 +19,29 @@ public:
         return res;
     }
 };
+
+//using priority queue
+class foo{
+public:
+    bool operator()(pair<char,int>& a,pair<char,int>& b){
+        return a.second<b.second;
+    }
+};
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> m;
+        for(const auto& c:s)
+            ++m[c];
+        priority_queue<pair<char,int>,vector<pair<char,int>>,foo> pq;
+        for(const auto& [k,v]:m)
+            pq.push({k,v});
+        string res="";
+        while(pq.size()){
+            auto cur=pq.top();pq.pop();
+            while(cur.second--)
+                res+=cur.first;
+        }
+        return res;
+    }
+};
