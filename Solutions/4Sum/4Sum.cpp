3b1628c567793ba23d0699bf2d0881235e6d026a
ErnestL1n <ernestlin1998@gmail.com>
2021-07-17 14:52:09 +0800

    update

--- Solutions/4Sum/4Sum.cpp
+++ Solutions/4Sum/4Sum.cpp
@@ -1,37 +1,3 @@
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        if(nums.size()<2)
-            return res;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-2;++i){
-            if(nums[i]>0)
-                break;
-            if(i>0 and nums[i-1]==nums[i])
-                continue;
-            int lo=i+1,hi=nums.size()-1,target=-nums[i];
-            while(lo<hi){
-                if(nums[lo]+nums[hi]==target){
-                    res.push_back({nums[lo],nums[hi],nums[i]});
-                    while(lo<hi and nums[lo]==nums[lo+1])++lo;
-                    while(lo<hi and nums[hi]==nums[hi-1])--hi;
-                    ++lo;--hi;
-                }else if(nums[lo]+nums[hi]<target){
-                    while(lo<hi and nums[lo]==nums[lo+1])++lo;
-                    ++lo;
-                }
-                else{
-                    while(lo<hi and nums[hi]==nums[hi-1])--hi;
-                    --hi;
-                }
-            }
-        }
-        return res;
-    }
-};
-
-
 //general solution for N>=2
 class Solution {
 public:
