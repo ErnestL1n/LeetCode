1bf1d04abacaa08b8db773ac6c0ab91c0590fd28
ErnestL1n <ernestlin1998@gmail.com>
2021-07-27 23:58:32 +0800

    Update 18. 4Sum.cpp

--- Solutions/18. 4Sum/18. 4Sum.cpp
+++ Solutions/18. 4Sum/18. 4Sum.cpp
@@ -40,3 +40,48 @@ public:
         }
     }
 };
+
+//
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> res;
+        vector<int> tmp;
+        foo(nums,target,4,0,nums.size()-1,res,tmp);
+        return res;
+    }
+    void foo(vector<int>& nums,int target,int N,int l,int r,vector<vector<int>>& res,vector<int>& tmp){
+        long m_ax=(long)nums[r]*N,m_in=(long)nums[l]*N;
+        if(r-l+1<N or N<2 or m_ax<target or m_in>target)
+            return;
+        if(N==2){
+            while(l<r){
+                int sum=nums[l]+nums[r];
+                if(sum==target){
+                    tmp.push_back(nums[l]);
+                    tmp.push_back(nums[r]);
+                    res.push_back(tmp);
+                    ++l;
+                    tmp.pop_back();
+                    tmp.pop_back();
+                    while(l<r and nums[l]==nums[l-1])
+                        ++l;
+            
+                }else if(sum<target){
+                    ++l;
+                }else{
+                    --r;
+                }
+            }
+        }else{
+            for(int i=l;i<=r;++i){
+                if(i==l or (i>l and nums[i]!=nums[i-1])){
+                    tmp.push_back(nums[i]);
+                    foo(nums,target-nums[i],N-1,i+1,r,res,tmp);
+                    tmp.pop_back();
+                }
+            }
+        }
+    }
+};
