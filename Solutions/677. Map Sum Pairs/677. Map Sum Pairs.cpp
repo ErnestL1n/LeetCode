3d66517a42878395ba62145b4be083361484ff45
ErnestL1n <ernestlin1998@gmail.com>
2021-07-30 23:45:11 +0800

    Update 677. Map Sum Pairs.cpp

--- Solutions/677. Map Sum Pairs/677. Map Sum Pairs.cpp
+++ Solutions/677. Map Sum Pairs/677. Map Sum Pairs.cpp
@@ -1,28 +1,35 @@
 class MapSum {
 public:
-    struct trie{
-        trie* ch[26]={};
-        int sum=0;
-    }root;
-    unordered_map<string,int>pairs;
     /** Initialize your data structure here. */
+    struct trie{
+            trie* ch[26]={};
+            int sum=0;
+        }root;
+    unordered_map<string,int> m;
     MapSum() {
         
     }
     
     void insert(string key, int val) {
         auto p=&root;
-        for(auto i=0;i<key.size();p->sum+=val-pairs[key],++i)
-            p=p->ch[key[i]-'a']=p->ch[key[i]-'a']==nullptr?new trie():p->ch[key[i]-'a'];
-        pairs[key]=val;
+        for(int i=0;i<key.size();p->sum+=val-m[key],++i)
+            p=p->ch[key[i]-'a']=p->ch[key[i]-'a']?p->ch[key[i]-'a']:new trie();
+        m[key]=val;
     }
     
     int sum(string prefix) {
         auto p=&root;
-        for(auto i=0;i<prefix.size()&&p!=nullptr;p = p->ch[prefix[i] - 'a'], ++i);
-        return p==nullptr?0:p->sum;
+        for (int i=0; i<prefix.size() and p;p=p->ch[prefix[i]-'a'],++i) ;
+        return p?p->sum:0;
     }
 };
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum* obj = new MapSum();
+ * obj->insert(key,val);
+ * int param_2 = obj->sum(prefix);
+ */
 
 /**
  * Your MapSum object will be instantiated and called as such:
