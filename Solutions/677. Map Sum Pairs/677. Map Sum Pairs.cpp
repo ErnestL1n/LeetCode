dd9f2b0f846124da0dfd0c10a96c11f87596fb8e
ErnestL1n <ernestlin1998@gmail.com>
2021-08-04 15:44:44 +0800

    Update 677. Map Sum Pairs.cpp

--- Solutions/677. Map Sum Pairs/677. Map Sum Pairs.cpp
+++ Solutions/677. Map Sum Pairs/677. Map Sum Pairs.cpp
@@ -10,16 +10,25 @@ public:
         
     }
     
-    void insert(string key, int val) {
-        auto p=&root;
-        for(int i=0;i<key.size();p->sum+=val-m[key],++i)
-            p=p->ch[key[i]-'a']=p->ch[key[i]-'a']?p->ch[key[i]-'a']:new trie();
+    void insert(string key,int val) {
+        trie* p=&root;
+        for(int i=0;i<key.size();++i){
+            if(p->ch[key[i]-'a']!=nullptr){
+                p->ch[key[i]-'a']=p->ch[key[i]-'a'];
+                p=p->ch[key[i]-'a'];
+            }else{
+                p->ch[key[i]-'a']=new trie();
+                p=p->ch[key[i]-'a'];
+            }
+            p->sum+=val-m[key];
+        }
         m[key]=val;
     }
     
     int sum(string prefix) {
-        auto p=&root;
-        for (int i=0; i<prefix.size() and p;p=p->ch[prefix[i]-'a'],++i) ;
+        trie* p=&root;
+        for (int i=0;i<prefix.size() and p!=nullptr;++i)
+            p=p->ch[prefix[i]-'a'];
         return p?p->sum:0;
     }
 };
@@ -37,3 +46,26 @@ public:
  * obj->insert(key,val);
  * int param_2 = obj->sum(prefix);
  */
+
+
+//map solution
+class MapSum {
+public:
+    map<string,int> m;
+    /** Initialize your data structure here. */    
+    void insert(string key, int val) {
+        m[key]=val;
+    }
+
+    int sum(string prefix) {
+        int sum=0,n=prefix.size();
+        // iterator first points to "ap" in map 
+        // n=="ap".size()==2
+        // "ap".substr(0,n)==prefix("ap")
+        for(auto it=m.lower_bound(prefix);it!=m.end() and it->first.substr(0, n)==prefix;++it){
+            cout <<it->first <<"\n";
+            sum+=it->second;
+        }
+        return sum;
+    }
+};
