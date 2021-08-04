c6873725e1a808f15992dcec4be4bbcf17762ce6
ErnestL1n <ernestlin1998@gmail.com>
2021-08-04 15:56:21 +0800

    Update 677. Map Sum Pairs.py

--- Solutions/677. Map Sum Pairs/677. Map Sum Pairs.py
+++ Solutions/677. Map Sum Pairs/677. Map Sum Pairs.py
@@ -1,15 +1,35 @@
+class Trie:
+    def __init__(self):
+        self.ch=defaultdict(Trie)
+        self.sum=0
 class MapSum:
 
-        def __init__(self): 
-            self.d = {}
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root=Trie()
+        self.m=defaultdict(int)
 
-        def insert(self, key, val): 
-            self.d[key] = val
+    def insert(self, key: str, val: int) -> None:
+        p=self.root
+        for c in key:
+            if p.ch[c]!=None:
+                p.ch[c]=p.ch[c]
+                p=p.ch[c]
+            else:
+                p.ch[c]=Trie()
+                p=p.ch[c]
+            p.sum+=val-self.m[key]
+        self.m[key]=val
 
-        def sum(self, prefix):
-            return sum(self.d[i] for i in self.d if i.startswith(prefix))
-        
-
+    def sum(self, prefix: str) -> int:
+        p=self.root
+        for c in prefix:
+            if p.ch[c]==None:
+                break;
+            p=p.ch[c]
+        return p.sum if p!=None else 0
 
 # Your MapSum object will be instantiated and called as such:
 # obj = MapSum()
