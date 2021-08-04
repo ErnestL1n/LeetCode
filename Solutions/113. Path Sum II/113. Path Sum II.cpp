3b5c322cbb0b69f9190e38d849320d85397602b0
ErnestL1n <ernestlin1998@gmail.com>
2021-08-04 23:55:54 +0800

    Update 113. Path Sum II.cpp

--- Solutions/113. Path Sum II/113. Path Sum II.cpp
+++ Solutions/113. Path Sum II/113. Path Sum II.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//dfs recursive
 class Solution {
 public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
@@ -29,5 +30,39 @@ public:
         if(root->right)
             dfs(root->right,sum-root->val,tmp,res);
         tmp.pop_back();
+    }
+};
+
+
+//dfs using stack,iterative
+typedef struct{
+    TreeNode* root;
+    int val;
+    vector<int> vals;
+}Node;
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> res;
+        if(!root)
+            return res;
+        stack<Node> stk;
+        stk.push({root,targetSum-root->val,{root->val}});
+        while(stk.size()){
+            auto curr=stk.top();stk.pop();
+            if(!curr.root->left and !curr.root->right and curr.val==0)
+                res.push_back(curr.vals);
+            if(curr.root->right){
+                curr.vals.push_back(curr.root->right->val);
+                stk.push({curr.root->right,curr.val-curr.root->right->val,curr.vals});
+               curr.vals.pop_back();
+            }
+            if(curr.root->left){
+                curr.vals.push_back(curr.root->left->val);
+                stk.push({curr.root->left,curr.val-curr.root->left->val,curr.vals});
+                curr.vals.pop_back();
+            }
+        }
+        return res;
     }
 };
