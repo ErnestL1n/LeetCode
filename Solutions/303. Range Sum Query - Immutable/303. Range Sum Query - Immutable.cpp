009548f6586940c2459ff269d5d4fa7344890ea1
ErnestL1n <ernestlin1998@gmail.com>
2021-08-16 23:15:02 +0800

    Update 303. Range Sum Query - Immutable.cpp

--- Solutions/303. Range Sum Query - Immutable/303. Range Sum Query - Immutable.cpp
+++ Solutions/303. Range Sum Query - Immutable/303. Range Sum Query - Immutable.cpp
@@ -1,3 +1,4 @@
+//O(n) init and O(n) query solution
 class NumArray {
 public:
     vector<int> arr;
@@ -9,6 +10,26 @@ public:
         return accumulate(begin(arr)+left,begin(arr)+right+1,0,[this](int s,const int& n){return s+n;});
     }
 };
+
+
+//O(n) init and O(1) query solution
+//using prefix sum
+class NumArray {
+public:
+    vector<int> arr;
+    NumArray(vector<int>& nums) {
+        for(int i=1;i<nums.size();++i)
+            nums[i]+=nums[i-1];
+        this->arr=nums;
+    }
+    
+    int sumRange(int left, int right) {
+        if(left==0)
+            return this->arr[right];
+        return this->arr[right]-this->arr[left-1];
+    }
+};
+
 
 /**
  * Your NumArray object will be instantiated and called as such:
