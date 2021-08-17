ddd25534152101d985bf72f11a7d59b2e7922857
ErnestL1n <ernestlin1998@gmail.com>
2021-08-17 22:58:55 +0800

    Update 1448. Count Good Nodes in Binary Tree.cpp

--- Solutions/1448. Count Good Nodes in Binary Tree/1448. Count Good Nodes in Binary Tree.cpp
+++ Solutions/1448. Count Good Nodes in Binary Tree/1448. Count Good Nodes in Binary Tree.cpp
@@ -9,11 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// D&C
 class Solution {
 public:
-int goodNodes(TreeNode* root, int val = INT_MIN) {
-    return root == nullptr ? 0 : (val <= root->val) 
-    + goodNodes(root->left, max(root->val, val))
-        + goodNodes(root->right, max(root->val, val));
-}
+    int goodNodes(TreeNode* root) {
+        int res=0;
+        dfs(res,root,root->val);
+        return res;
+    }
+    void dfs(int& res,TreeNode* root,int max_val){
+        if(!root)
+            return;
+        res+=root->val>=max_val;
+        max_val=max(max_val,root->val);
+        dfs(res,root->left,max_val);
+        dfs(res,root->right,max_val);
+    }
 };
+
+// 1-liner
+class Solution {
+public:
+    int goodNodes(TreeNode* root, int val = INT_MIN) {
+        return root == nullptr ? 0 : (val <= root->val) 
+        + goodNodes(root->left, max(root->val, val))
+            + goodNodes(root->right, max(root->val, val));
+    }
+};
+
