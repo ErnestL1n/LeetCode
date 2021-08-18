211d649980888c872cc7245f38c5a65600a19638
ErnestL1n <ernestlin1998@gmail.com>
2021-08-18 23:36:53 +0800

    Update 91. Decode Ways.cpp

--- Solutions/91. Decode Ways/91. Decode Ways.cpp
+++ Solutions/91. Decode Ways/91. Decode Ways.cpp
@@ -37,3 +37,19 @@ public:
 };
 
 // Bottom-up DP (Space Optimized)
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n=s.size(),dp=0,dp1=1,dp2=0;
+        for(int i=n-1;i>=0;--i){
+            if(s[i]!='0')
+                dp+=dp1;
+            if(i+1<s.size() and (s[i]=='1' or (s[i]=='2' and s[i+1]<='6')))
+                dp+=dp2;
+            dp2=dp1;
+            dp1=dp;
+            dp=0;
+        }
+        return dp1;
+    }
+};
