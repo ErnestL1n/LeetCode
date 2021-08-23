84669cb97ece43e5bea0768c4a1471d60ebc42d5
ErnestL1n <ernestlin1998@gmail.com>
2021-08-23 23:49:48 +0800

    Update 100. Same Tree.cpp

--- Solutions/100. Same Tree/100. Same Tree.cpp
+++ Solutions/100. Same Tree/100. Same Tree.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
@@ -19,5 +20,28 @@ public:
         if(p->val==q->val)
             return isSameTree(p->left,q->left) and isSameTree(p->right,q->right);
         return false;
+    }
+};
+
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        queue<TreeNode*> que;
+        que.push(p);
+        que.push(q);
+        while(que.size()){
+            auto cur1=que.front();que.pop();
+            auto cur2=que.front();que.pop();
+            if(!cur1 and !cur2)
+                continue;
+            else if(!cur1 or !cur2 or cur1->val!=cur2->val)
+                return false;
+            que.push(cur1->left);
+            que.push(cur2->left);
+            que.push(cur1->right);
+            que.push(cur2->right);
+        }
+        return true;
     }
 };
