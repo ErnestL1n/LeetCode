fbb345a1d81c25810b92711935ab1190f70ec596
ErnestL1n <ernestlin1998@gmail.com>
2021-08-25 23:52:09 +0800

    Update 53. Maximum Subarray.cpp

--- Solutions/53. Maximum Subarray/53. Maximum Subarray.cpp
+++ Solutions/53. Maximum Subarray/53. Maximum Subarray.cpp
@@ -9,3 +9,17 @@ public:
         return res;
     }
 };
+
+//dp array
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums.size());
+        int res=dp[0]=nums[0];
+        for(int i=1;i<nums.size();++i){
+            dp[i]=nums[i]+(dp[i-1]>0?dp[i-1]:0);
+            res=max(dp[i],res);
+        }
+        return res;
+    }
+};
