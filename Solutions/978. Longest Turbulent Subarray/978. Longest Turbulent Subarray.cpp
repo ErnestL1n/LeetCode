e69f129da2a3f3efc83c92e2fade9d28ce0e63af
ErnestL1n <ernestlin1998@gmail.com>
2021-09-15 23:54:48 +0800

    Update 978. Longest Turbulent Subarray.cpp

--- Solutions/978. Longest Turbulent Subarray/978. Longest Turbulent Subarray.cpp
+++ Solutions/978. Longest Turbulent Subarray/978. Longest Turbulent Subarray.cpp
@@ -35,3 +35,28 @@ public:
         return res;
     }
 };
+
+// clean version
+// For each A[i]
+// inc: The length of current valid sequence which ends with two increasing numbers
+// dec: The length of current valid sequence which ends with two decreasing numbers
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int inc=1,dec=1,res=1;
+        for(int i=1;i<arr.size();++i){
+            if(arr[i]<arr[i-1]){
+                dec=inc+1;
+                inc=1;
+            }else if(arr[i]>arr[i-1]){
+                inc=dec+1;
+                dec=1;
+            }else{
+                inc=1;
+                dec=1;
+            }
+            res=max(res,max(dec,inc));
+        }
+        return res;
+    }
+};
