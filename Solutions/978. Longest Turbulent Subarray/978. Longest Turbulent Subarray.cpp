8e4bec6f087de9af8a9f6aec010f172ade988688
ErnestL1n <ernestlin1998@gmail.com>
2021-09-17 10:38:11 +0800

    Update 978. Longest Turbulent Subarray.cpp

--- Solutions/978. Longest Turbulent Subarray/978. Longest Turbulent Subarray.cpp
+++ Solutions/978. Longest Turbulent Subarray/978. Longest Turbulent Subarray.cpp
@@ -43,19 +43,18 @@ public:
 class Solution {
 public:
     int maxTurbulenceSize(vector<int>& arr) {
-        int inc=1,dec=1,res=1;
+        int dec=1,inc=1,res=1;
         for(int i=1;i<arr.size();++i){
-            if(arr[i]<arr[i-1]){
-                dec=inc+1;
-                inc=1;
-            }else if(arr[i]>arr[i-1]){
+            if(arr[i]>arr[i-1]){
                 inc=dec+1;
                 dec=1;
-            }else{
+            }else if(arr[i]<arr[i-1]){
+                dec=inc+1;
                 inc=1;
-                dec=1;
+            }else{
+                inc=dec=1;
             }
-            res=max(res,max(dec,inc));
+            res=max(res,max(inc,dec));
         }
         return res;
     }
