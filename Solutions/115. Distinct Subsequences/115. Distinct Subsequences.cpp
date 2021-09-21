f2b601be3e49b8856bf04c0a81b9e143f3182d9a
ErnestL1n <ernestlin1998@gmail.com>
2021-09-21 23:33:47 +0800

    Update 115. Distinct Subsequences.cpp

--- Solutions/115. Distinct Subsequences/115. Distinct Subsequences.cpp
+++ Solutions/115. Distinct Subsequences/115. Distinct Subsequences.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        int n=s.size(),m=t.size();
-        vector<vector<unsigned>> dp(m+1,vector<unsigned>(n+1));
+        int m=t.size(),n=s.size();
+        vector<vector<unsigned>> dp(m+1,vector<unsigned>(n+1)); 
         for(int j=0;j<=n;++j)
             dp[0][j]=1;
-        for(int i=0;i<m;++i)
-            for(int j=0;j<n;++j){
-                if(t[i]==s[j])
-                    dp[i+1][j+1]=dp[i][j]+dp[i+1][j];
-                else
-                    dp[i+1][j+1]=dp[i+1][j];
+        for(int i=1;i<=m;++i)
+            for(int j=1;j<=n;++j){
+                if(t[i-1]==s[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+dp[i][j-1];
+                }else{
+                    dp[i][j]=dp[i][j-1];
+                }
             }
         return dp[m][n];
+    }
+};
+
+//space opt
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m=t.size(),n=s.size();
+        vector<unsigned> dp(m+1);
+        dp[0]=1;
+        for(int j=1;j<=n;++j){
+            unsigned prev=1;
+            for(int i=1;i<=m;++i){
+                unsigned tmp=dp[i];
+                dp[i]=dp[i]+(s[j-1]==t[i-1]?prev:0);
+                prev=tmp;
+            }
+        }
+        return dp[m];
     }
 };
