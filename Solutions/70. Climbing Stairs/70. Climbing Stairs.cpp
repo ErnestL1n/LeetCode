622c888e10e1a8baf1dcaf279b4492767b066bbb
ErnestL1n <ernestlin1998@gmail.com>
2021-09-22 16:46:44 +0800

    Update 70. Climbing Stairs.cpp

--- Solutions/70. Climbing Stairs/70. Climbing Stairs.cpp
+++ Solutions/70. Climbing Stairs/70. Climbing Stairs.cpp
@@ -35,3 +35,14 @@ public:
         return dp[n%2];
     }
 };
+
+// 2021.9.22
+class Solution {
+public:
+    int climbStairs(int n) {
+        int pre2=1,pre1=1,cur=1;
+        for(int i=2;i<=n;++i)
+            cur=pre1+pre2,pre2=pre1,pre1=cur;
+        return cur;
+    }
+};
