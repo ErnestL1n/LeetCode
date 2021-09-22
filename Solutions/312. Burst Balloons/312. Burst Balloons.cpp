f31d0bdad9cc30208cfe56ecd0efcd6dedcf36b3
ErnestL1n <ernestlin1998@gmail.com>
2021-09-22 15:34:03 +0800

    Update 312. Burst Balloons.cpp

--- Solutions/312. Burst Balloons/312. Burst Balloons.cpp
+++ Solutions/312. Burst Balloons/312. Burst Balloons.cpp
@@ -43,3 +43,27 @@ public:
         return dp[i][j];
     }
 };
+
+
+
+
+
+
+//2021.9.22
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n=nums.size();
+        nums.insert(nums.begin(),1);
+        nums.insert(nums.end(),1);
+        vector<vector<int>> dp(n+2,vector<int>(n+2));
+        for(int l=1;l<=n;++l){
+            for(int i=1;i<=n-l+1;++i){
+                int j=i+l-1;
+                for(int k=i;k<=j;++k)
+                    dp[i][j]=max(dp[i][j],dp[i][k-1]+dp[k+1][j]+nums[i-1]*nums[k]*nums[j+1]);
+            }
+        }
+        return dp[1][n];
+    }
+};
