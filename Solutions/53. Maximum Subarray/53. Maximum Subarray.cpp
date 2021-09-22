a10ddbaf207c19f17d9d48217a5dffa0a69b9851
ErnestL1n <ernestlin1998@gmail.com>
2021-09-22 17:19:42 +0800

    Update 53. Maximum Subarray.cpp

--- Solutions/53. Maximum Subarray/53. Maximum Subarray.cpp
+++ Solutions/53. Maximum Subarray/53. Maximum Subarray.cpp
@@ -1,25 +1,30 @@
+//2021.9.22
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int curr=INT_MIN,res=INT_MIN;
-        for(auto n:nums){
-            curr=max(curr,0)+n;
-            res=max(curr,res);
+        int n=nums.size(),res=INT_MIN;
+        //dp[i]=maximum subarray(1~i)
+        //dp[i]=dp[i-1]>0?dp[i-1]+nums[i]:nums[i]
+        vector<int> dp(n+1);
+        for(int i=1;i<=n;++i){
+            dp[i]=dp[i-1]>0?dp[i-1]+nums[i-1]:nums[i-1];
+            res=max(res,dp[i]);
         }
         return res;
     }
 };
 
-//dp array
+//space opt
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        vector<int> dp(nums.size());
-        int res=dp[0]=nums[0];
-        for(int i=1;i<nums.size();++i){
-            dp[i]=nums[i]+(dp[i-1]>0?dp[i-1]:0);
-            res=max(dp[i],res);
+        int n=nums.size();
+        int prev=0,cur=0,res=INT_MIN;
+        for(int i=0;i<n;++i){
+            cur=prev>0?prev+nums[i]:nums[i];
+            res=max(res,cur);
+            prev=cur;
         }
-        return res;
+        return res; 
     }
 };
