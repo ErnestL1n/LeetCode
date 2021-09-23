84d702fb3723939338c9f2203ca6aa214f35d3ac
ErnestL1n <ernestlin1998@gmail.com>
2021-09-23 22:29:45 +0800

    Update 350. Intersection of Two Arrays II.cpp

--- Solutions/350. Intersection of Two Arrays II/350. Intersection of Two Arrays II.cpp
+++ Solutions/350. Intersection of Two Arrays II/350. Intersection of Two Arrays II.cpp
@@ -3,7 +3,16 @@ public:
     vector<int> intersect(vector<int>& a, vector<int>& b) {
         sort(a.begin(), a.end());
         sort(b.begin(), b.end());
-        a.erase(set_intersection(a.begin(), a.end(), b.begin(), b.end(), a.begin()), a.end());
+        int n=a.size();
+        // fill the intersection element begin from a.begin()
+        auto it=set_intersection(a.begin(), a.end(), b.begin(), b.end(), a.begin());
+        a.erase(it, a.end());
+        // for(int i=0;i<a.size();++i)
+        //     cout << a[i] ;
+        // cout << "\n";
+        // for(int i=0;i<n;++i)
+        //     cout << a[i] ;
+        // cout << "\n";
         return a;
     }
 };
