d417f92f391f52fc16612883442190714604a976
ErnestL1n <ernestlin1998@gmail.com>
2021-10-01 20:22:32 +0800

    Update 698. Partition to K Equal Sum Subsets.cpp

--- Solutions/698. Partition to K Equal Sum Subsets/698. Partition to K Equal Sum Subsets.cpp
+++ Solutions/698. Partition to K Equal Sum Subsets/698. Partition to K Equal Sum Subsets.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int sum = 0;
-        for(int num:nums)sum+=num;
-        if(k <= 0 || sum%k != 0)return false;
-        vector<int> visited(nums.size(), 0);
-        return canPartition(nums, visited, 0, k, 0, 0, sum/k);
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if(sum%k!=0)
+            return false;
+        int subset_sum=sum/k;
+        sort(nums.begin(),nums.end(),[&](int a,int b){return a>b;});
+        vector<bool> visited(nums.size());
+        return foo(nums,0,0,subset_sum,visited,k);
     }
-    
-    bool canPartition(vector<int>& nums, vector<int>& visited, int start_index, int k, int cur_sum, int cur_num, int target){
-        if(k==1)return true;
-        if(cur_sum == target && cur_num >0 )return canPartition(nums, visited, 0, k-1, 0, 0, target);
-        for(int i = start_index; i<nums.size(); i++){
-            if(!visited[i]){
-                visited[i] = 1;
-                if(canPartition(nums, visited, i+1, k, cur_sum + nums[i], cur_num++, target))return true;
-                visited[i] = 0;
-            }
+    bool foo(vector<int>& nums,int begin,int cur_sum,int subset_sum,vector<bool>& visited,int rest_k){
+        if(rest_k==1)
+            return true;
+        if(subset_sum==cur_sum)
+            return foo(nums,0,0,subset_sum,visited,rest_k-1);
+        for(int i=begin;i<nums.size();++i){
+            if(!visited[i] and cur_sum+nums[i]<=subset_sum){
+                visited[i]=true;
+                if(foo(nums,i+1,cur_sum+nums[i],subset_sum,visited,rest_k))
+                    return true;
+                visited[i]=false;
+            }    
         }
         return false;
     }
