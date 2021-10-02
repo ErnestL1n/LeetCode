46f697be993bf861e5411fe713650f0c0c761cc6
ErnestL1n <ernestlin1998@gmail.com>
2021-10-02 17:30:21 +0800

    Update 725. Split Linked List in Parts.cpp

--- Solutions/725. Split Linked List in Parts/725. Split Linked List in Parts.cpp
+++ Solutions/725. Split Linked List in Parts/725. Split Linked List in Parts.cpp
@@ -1,20 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    vector<ListNode*> splitListToParts(ListNode* root, int k) {
-        vector<ListNode*> parts(k, nullptr);
-        int len = 0;
-        for (ListNode* node = root; node; node = node->next)
-            len++;
-        int n = len / k, r = len % k; // n : minimum guaranteed part size; r : extra nodes spread to the first r parts;
-        ListNode* node = root, *prev = nullptr;
-        for (int i = 0; node && i < k; i++, r--) {
-            parts[i] = node;
-            for (int j = 0; j < n + (r > 0); j++) {
-                prev = node;
-                node = node->next;
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        vector<ListNode*> res(k);
+        int size=0;
+        auto cur=head;
+        while(cur)
+            ++size,cur=cur->next;
+        cur=head;
+        int sz=size/k,r=size%k;
+        ListNode* pre=nullptr;
+        for(int i=0;cur and i<k;++i,--r){
+            res[i]=cur;
+            for(int j=0;j<(r>0)+sz;++j){
+                pre=cur;
+                cur=cur->next;
             }
-            prev->next = nullptr;
+            pre->next=nullptr;
         }
-        return parts;
+        return res;
     }
 };
