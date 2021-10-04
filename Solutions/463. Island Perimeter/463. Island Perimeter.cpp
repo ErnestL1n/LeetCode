ba09bcfa97e9b72e05f8546f848c583030d8db43
ErnestL1n <ernestlin1998@gmail.com>
2021-10-04 22:05:51 +0800

    Update 463. Island Perimeter.cpp

--- Solutions/463. Island Perimeter/463. Island Perimeter.cpp
+++ Solutions/463. Island Perimeter/463. Island Perimeter.cpp
@@ -13,3 +13,29 @@ public:
         return res;
     }
 };
+
+// 2021/10/4
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int m=grid.size(),n=grid[0].size(),cnt=0;
+        vector<array<int,2>> dirs={{1,0},{-1,0},{0,1},{0,-1}};
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                if(grid[i][j]==1)
+                    cnt+=4;
+                for(int k=0;k<4;++k){
+                    int x=dirs[k][0]+i;
+                    int y=dirs[k][1]+j;
+                    if(x<0 or y<0 or x>=m or y>=n){
+                        continue;
+                    }else{
+                        if(grid[x][y]==1 and grid[i][j]==1)
+                            cnt-=1;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+};
