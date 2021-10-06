7a02d0c603b480d87aa625fbcb97683763c6846d
ErnestL1n <ernestlin1998@gmail.com>
2021-10-06 23:47:00 +0800

    Update 442. Find All Duplicates in an Array.cpp

--- Solutions/442. Find All Duplicates in an Array/442. Find All Duplicates in an Array.cpp
+++ Solutions/442. Find All Duplicates in an Array/442. Find All Duplicates in an Array.cpp
@@ -11,3 +11,22 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+        unordered_set<int> s;
+        for(auto& n:nums){
+            auto it=s.find(n);
+            if(it!=s.end() and ((*it)^n)==0){
+                res.push_back(n);
+                s.erase(it);
+            }else{
+                s.insert(n);
+            }
+        }
+        return res;
+    }
+};
