d9911a9085f16e199a3096ffea2e201ed7d8172e
ErnestL1n <ernestlin1998@gmail.com>
2021-10-10 16:22:48 +0800

    Update 79. Word Search.cpp

--- Solutions/79. Word Search/79. Word Search.cpp
+++ Solutions/79. Word Search/79. Word Search.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
+    int m,n;
     bool exist(vector<vector<char>>& board, string word) {
-        for (unsigned int i = 0; i < board.size(); i++) 
-            for (unsigned int j = 0; j < board[0].size(); j++) 
-                if (dfs(board, i, j, word))
+        m=board.size(),n=board[0].size();
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                if(foo(i,j,0,word,board))
                     return true;
+            }
+        }
         return false;
     }
-
-    bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
-        if (!word.size())
-            return true;
-        if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[0])  
+    bool foo(int x,int y,int start,string& word,vector<vector<char>>& board){
+        if(x<0 or y<0 or x>=m or y>=n)
             return false;
-        char c = board[i][j];
-        board[i][j] = '*';
-        string s = word.substr(1);
-        bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
-        board[i][j] = c;
-        return ret;
+        if(word[start]!=board[x][y])
+            return false;
+        if(start==word.size()-1)
+            return true;
+        auto cur=board[x][y];
+        board[x][y]='*';
+        bool res=foo(x-1,y,start+1,word,board) or 
+            foo(x,y-1,start+1,word,board) or
+            foo(x+1,y,start+1,word,board) or
+            foo(x,y+1,start+1,word,board);
+        board[x][y]=cur;
+        return res;
     }
 };
