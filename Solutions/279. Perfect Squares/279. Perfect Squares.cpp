f4f8acaceaafd436531961c5622014a4260d3578
ErnestL1n <ernestlin1998@gmail.com>
2021-10-14 23:52:17 +0800

    Update 279. Perfect Squares.cpp

--- Solutions/279. Perfect Squares/279. Perfect Squares.cpp
+++ Solutions/279. Perfect Squares/279. Perfect Squares.cpp
@@ -1,3 +1,4 @@
+//normal dp
 class Solution {
 public:
     int numSquares(int n) {
@@ -7,6 +8,22 @@ public:
             for(int j=1;j*j<=i;++j){
                 dp[i]=min(dp[i],dp[i-j*j]+1);
             }
+        }
+        return dp[n];
+    }
+};
+
+//static vector dp
+class Solution {
+public:
+    int numSquares(int n) {
+        static vector<int> dp({0});
+        while(dp.size()<=n){
+            int sz=dp.size(),tmp=INT_MAX;
+            for(int i=1;i*i<=sz;++i){
+                tmp=min(tmp,dp[sz-i*i]+1);
+            }
+            dp.push_back(tmp);
         }
         return dp[n];
     }
