eaf9fedfa231774fc6afe433c2d01a3d0e968d18
ErnestL1n <ernestlin1998@gmail.com>
2021-10-28 23:48:24 +0800

    Update 15. 3Sum.cpp

--- Solutions/15. 3Sum/15. 3Sum.cpp
+++ Solutions/15. 3Sum/15. 3Sum.cpp
@@ -2,27 +2,38 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
-        if(nums.size()<2)
+        if(nums.size()<3){
             return res;
+        }
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-2;++i){
-            if(nums[i]>0)
+            if(nums[i]>0){
                 break;
-            if(i>0 and nums[i-1]==nums[i])
+            }
+            if(i>0 and nums[i]==nums[i-1]){
                 continue;
-            int lo=i+1,hi=nums.size()-1,target=-nums[i];
+            }
+            int lo=i+1,hi=nums.size()-1;
             while(lo<hi){
-                if(nums[lo]+nums[hi]==target){
-                    res.push_back({nums[lo],nums[hi],nums[i]});
-                    while(lo<hi and nums[lo]==nums[lo+1])++lo;
-                    while(lo<hi and nums[hi]==nums[hi-1])--hi;
-                    ++lo;--hi;
-                }else if(nums[lo]+nums[hi]<target){
-                    while(lo<hi and nums[lo]==nums[lo+1])++lo;
+                if(nums[i]+nums[lo]+nums[hi]==0){
+                    res.push_back({nums[i],nums[lo],nums[hi]});
+                    while(lo<hi and nums[lo]==nums[lo+1]){
+                        ++lo;
+                    }    
+                    while(lo<hi and nums[hi]==nums[hi-1]){
+                        --hi;
+                    }
                     ++lo;
-                }
-                else{
-                    while(lo<hi and nums[hi]==nums[hi-1])--hi;
+                    --hi;
+                }else if(nums[i]+nums[lo]+nums[hi]<0){
+                    while(lo<hi and nums[lo]==nums[lo+1]){
+                        ++lo;
+                    }
+                    ++lo;
+                }else{
+                    while(lo<hi and nums[hi]==nums[hi-1]){
+                        --hi;
+                    }
                     --hi;
                 }
             }
