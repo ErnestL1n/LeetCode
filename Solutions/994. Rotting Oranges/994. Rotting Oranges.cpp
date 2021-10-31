9d56c41cbca6227c72063454937b2ff02ded1881
ErnestL1n <ernestlin1998@gmail.com>
2021-10-31 16:50:07 +0800

    Update 994. Rotting Oranges.cpp

--- Solutions/994. Rotting Oranges/994. Rotting Oranges.cpp
+++ Solutions/994. Rotting Oranges/994. Rotting Oranges.cpp
@@ -1,49 +1,38 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) 
-    {
-        
-        vector<int> dir={-1,0,1,0,-1}; //used for finding all 4 adjacent coordinates
-        
-        int m=grid.size();
-        int n=grid[0].size();
-        
-        queue<pair<int,int>> q;
-        int fresh=0; //To keep track of all fresh oranges left
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]==2)
-                    q.push({i,j});
-                if(grid[i][j]==1)
-                    fresh++;
-            }
-        int ans=-1; //initialised to -1 since after each step we increment the time by 1 and initially all rotten oranges started at 0.
-        while(!q.empty())
-        {
-            int sz=q.size();
-            while(sz--)
-            {
-                pair<int,int> p=q.front();
-                q.pop();
-                for(int i=0;i<4;i++)
-                {
-                    int r=p.first+dir[i];
-                    int c=p.second+dir[i+1];
-                    if(r>=0 && r<m && c>=0 && c<n &&grid[r][c]==1)
-                    {
-                        grid[r][c]=2;
-                        q.push({r,c});
-                        fresh--; // decrement by 1 foreach fresh orange that now is rotten
-                    }
-                    
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m=grid.size(),n=grid[0].size(),cnt=0,res=0;
+        queue<pair<int,int>> que;
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                if(grid[i][j]==2){
+                    que.push({i,j});
+                }else if(grid[i][j]==1){
+                    ++cnt;
                 }
             }
-            ans++; //incremented after each minute passes
         }
-        if(fresh>0) return -1; //if fresh>0 that means there are fresh oranges left
-        if(ans==-1) return 0; //we initialised with -1, so if there were no oranges it'd take 0 mins.
-        return ans;
-        
+        if(cnt==0){
+            return 0;
+        }
+        vector<pair<int,int>> dirs={{1,0},{-1,0},{0,1},{0,-1}};
+        while(que.size()){
+            int size=que.size();
+            for(int sz=0;sz<size;++sz){
+                int i=que.front().first,j=que.front().second;
+                que.pop();
+                for(int k=0;k<4;++k){
+                    int x=i+dirs[k].first,y=j+dirs[k].second;
+                    if(x<0 or y<0 or x>=m or y>=n or grid[x][y]==0 or grid[x][y]==2){
+                        continue;
+                    }
+                    grid[x][y]=2;
+                    que.push({x,y});
+                    --cnt;
+                }
+            }
+            ++res;
+        }
+        return cnt==0?res-1:-1;
     }
 };
