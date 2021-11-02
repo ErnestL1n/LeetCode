899e30f4362a559e7f88b4e24d613c67a16c47fb
ErnestL1n <ernestlin1998@gmail.com>
2021-11-03 00:44:17 +0800

    Update 980. Unique Paths III.cpp

--- Solutions/980. Unique Paths III/980. Unique Paths III.cpp
+++ Solutions/980. Unique Paths III/980. Unique Paths III.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
-    int res = 0, empty = 1;
-    void dfs(vector<vector<int>>& grid, int x, int y, int count) {
-        if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() || grid[x][y] == -1) return;
-        
-        if (grid[x][y] == 2) {
-            if(empty == count) res++; 
-            return;
-        }
-        
-        grid[x][y] = -1;
-        
-        dfs(grid, x+1, y, count+1);
-        dfs(grid, x-1, y, count+1);
-        dfs(grid, x, y+1, count+1);
-        dfs(grid, x, y-1, count+1);
-        
-        grid[x][y] = 0;
-        
-    }
-    
     int uniquePathsIII(vector<vector<int>>& grid) {
-        int start_x, start_y;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == 1) start_x = i, start_y = j;
-                else if (grid[i][j] == 0) empty++;
+        // why cnt=1 ?
+        // Because the starting square grid[i][j] == 1 is counted 
+        // besides the squares with a value of 0.
+        int m=grid.size(),n=grid[0].size(),cnt=1,res=0;
+        int sx,sy;
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                if(grid[i][j]==0){
+                    ++cnt;
+                }else if(grid[i][j]==1){
+                    sx=i;
+                    sy=j;
+                }
             }
         }
-        
-        dfs(grid, start_x, start_y, 0);
+        function<void(int,int)> dfs=[&](int x,int y){
+            if(x<0 or y<0 or x>=m or y>=n or grid[x][y]<0){
+                return;
+            }
+            if(grid[x][y]==2){
+                if(cnt==0){
+                    ++res;
+                }
+                return;
+            }
+            grid[x][y]=-1;
+            --cnt;
+            dfs(x,y+1);
+            dfs(x,y-1);
+            dfs(x+1,y);
+            dfs(x-1,y);
+            grid[x][y]=0;
+            ++cnt;
+        };
+        dfs(sx,sy);
         return res;
     }
 };
