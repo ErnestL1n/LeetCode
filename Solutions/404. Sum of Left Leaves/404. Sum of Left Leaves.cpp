c2d199f18892d2c602ab4644a5f748fa967b407a
ErnestL1n <ernestlin1998@gmail.com>
2021-11-04 14:48:57 +0800

    Update 404. Sum of Left Leaves.cpp

--- Solutions/404. Sum of Left Leaves/404. Sum of Left Leaves.cpp
+++ Solutions/404. Sum of Left Leaves/404. Sum of Left Leaves.cpp
@@ -52,3 +52,16 @@ public:
         return res;
     }
 }; 
+
+// clear
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root,bool isleft=false) {
+        if(!root)
+            return 0;
+        if(root->left==nullptr and root->right==nullptr){
+            return isleft?root->val:0;
+        }
+        return sumOfLeftLeaves(root->left,true)+sumOfLeftLeaves(root->right,false);
+    }
+};
