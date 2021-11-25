062824db11fbca710dbc211d4960998bd3c14ffd
ErnestL1n <ernestlin1998@gmail.com>
2021-11-25 14:55:30 +0800

    Update 53. Maximum Subarray.cpp

--- Solutions/53. Maximum Subarray/53. Maximum Subarray.cpp
+++ Solutions/53. Maximum Subarray/53. Maximum Subarray.cpp
@@ -1,67 +1,136 @@
-// O(N^3) BF ,TLE
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define rep(i, n) for (int i = 0; i < n; ++i)
+typedef long long ll;
+template <int MOD>
+struct Fp {
+  long long val;
+  constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+    if (val < 0) val += MOD;
+  }
+  constexpr int getmod() const { return MOD; }
+  constexpr Fp operator-() const noexcept { return val ? MOD - val : 0; }
+  constexpr Fp operator+(const Fp& r) const noexcept { return Fp(*this) += r; }
+  constexpr Fp operator-(const Fp& r) const noexcept { return Fp(*this) -= r; }
+  constexpr Fp operator*(const Fp& r) const noexcept { return Fp(*this) *= r; }
+  constexpr Fp operator/(const Fp& r) const noexcept { return Fp(*this) /= r; }
+  constexpr Fp& operator+=(const Fp& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr Fp& operator-=(const Fp& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr Fp& operator*=(const Fp& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr Fp& operator/=(const Fp& r) noexcept {
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b, swap(a, b);
+      u -= t * v, swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator==(const Fp& r) const noexcept {
+    return this->val == r.val;
+  }
+  constexpr bool operator!=(const Fp& r) const noexcept {
+    return this->val != r.val;
+  }
+  friend constexpr istream& operator>>(istream& is, Fp<MOD>& x) noexcept {
+    is >> x.val;
+    x.val %= MOD;
+    if (x.val < 0) x.val += MOD;
+    return is;
+  }
+  friend constexpr ostream& operator<<(ostream& os, const Fp<MOD>& x) noexcept {
+    return os << x.val;
+  }
+  friend constexpr Fp<MOD> modpow(const Fp<MOD>& r, long long n) noexcept {
+    if (n == 0) return 1;
+    if (n < 0) return modpow(modinv(r), -n);
+    auto t = modpow(r, n / 2);
+    t = t * t;
+    if (n & 1) t = t * r;
+    return t;
+  }
+  friend constexpr Fp<MOD> modinv(const Fp<MOD>& r) noexcept {
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b, swap(a, b);
+      u -= t * v, swap(u, v);
+    }
+    return Fp<MOD>(u);
+  }
+};
+const int MOD = 1e9 + 7;
+// const int MOD = 998244353;
+using mint = Fp<MOD>;
+// EX:
+// vector<mint> dp(n + 1);
+// dp[0] = 1;
+// dp[1] = 2;
+// dp[2] = 7;
+// for (ll i = 3; i <= n; ++i) {
+// dp[i] = ((mint)3 * dp[i - 1] + dp[i - 2] - dp[i - 3]);
+// }
+// cout << dp[n] << "\n";
+
+template <typename T, int D>
+struct VEC : public vector<VEC<T, D-1>> {
+  static_assert(D >= 1, "Vector dimension must be greater than zero!");
+  template <typename... Args>
+  VEC(int n = 0, Args... args)
+      : vector<VEC<T, D-1>>(n, VEC<T, D-1>(args...)) {}
+};
+template <typename T>
+struct VEC<T, 1> : public vector<T> {
+  VEC(int n = 0, const T& val = T()) : vector<T>(n, val) {}
+};
+// EX:
+// ---------------------------------------
+// int a[10][50] initialized with zero
+// VEC<int, 2> a(10, 50);
+// ---------------------------------------
+// double b[n][m][k] initialized with 3.14
+// VEC<double, 3> b(n, m, k, 3.14);
+// ---------------------------------------
+// the third dimension has no value yet
+// VEC<long long, 3> c(5, 5);
+// now c[0][0][0] has a value (100) but others don't
+// c[0][0].push_back(100);
+// ---------------------------------------
+// VEC<int, 4> d(10, 10);
+// now d[2][3][0] is a vector with 100 values of 12345
+// d[2][3].push_back(VEC<int, 1>(100, 12345));
+// ---------------------------------------
+// just blank vector of strings
+// VEC<string, 1> e;
+// ---------------------------------------
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int res=INT_MIN;
-        for(int i=0;i<nums.size();++i)
-            for(int j=i;j<nums.size();++j){
-                int sum=0;
-                for(int k=i;k<=j;++k)
-                    sum+=nums[k];
-                res=max(res,sum);
+        VEC<int,1> dp(nums.size());
+        for(int i=0;i<nums.size();++i){
+            if(i>0 and dp[i-1]>0){
+                dp[i]=dp[i-1]+nums[i];
+            }else{
+                dp[i]=nums[i];
             }
-        return res;
-    }
-};
-
-// O(N^2) prefix sum ,TLE
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int n=nums.size(),res=INT_MIN;
-        if(n==1)
-            return nums[0];
-        vector<int> prefix(n+1);
-        for(int i=0;i<n;++i)
-            prefix[i+1]=prefix[i]+nums[i];
-        for(int i=0;i<=n;++i)
-            cout <<prefix[i] <<" ";
-        for(int i=0;i<n;++i)
-            for(int j=i;j<n;++j)
-                res=max(prefix[j+1]-prefix[i],res);
-        return res;
-    }
-};
-
-
-
-//2021.9.22 DP
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int n=nums.size(),res=INT_MIN;
-        //dp[i]=maximum subarray(1~i)
-        //dp[i]=dp[i-1]>0?dp[i-1]+nums[i]:nums[i]
-        vector<int> dp(n+1);
-        for(int i=1;i<=n;++i){
-            dp[i]=dp[i-1]>0?dp[i-1]+nums[i-1]:nums[i-1];
             res=max(res,dp[i]);
         }
         return res;
-    }
-};
-
-//space opt
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int n=nums.size();
-        int prev=0,cur=0,res=INT_MIN;
-        for(int i=0;i<n;++i){
-            cur=prev>0?prev+nums[i]:nums[i];
-            res=max(res,cur);
-            prev=cur;
-        }
-        return res; 
     }
 };
