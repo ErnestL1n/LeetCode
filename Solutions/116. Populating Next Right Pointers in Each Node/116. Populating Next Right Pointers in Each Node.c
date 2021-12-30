156927d6ce35c80fd157d775bc90f0de14e0cca8
ErnestL1n <ernestlin1998@gmail.com>
2021-12-31 01:14:43 +0800

    Update 116. Populating Next Right Pointers in Each Node.c

--- Solutions/116. Populating Next Right Pointers in Each Node/116. Populating Next Right Pointers in Each Node.c
+++ Solutions/116. Populating Next Right Pointers in Each Node/116. Populating Next Right Pointers in Each Node.c
@@ -8,19 +8,35 @@
  * };
  */
 
+//dfs
 struct Node* connect(struct Node* root) {
-	if (root == NULL) 
-            return NULL;
-        struct Node *pre = root;
-        struct Node* cur = NULL;
-        while(pre->left) {
-            cur = pre;
-            while(cur) {
-                cur->left->next = cur->right;
-                if(cur->next) cur->right->next = cur->next->left;
-                cur = cur->next;
-            }
-            pre = pre->left;
+    if(!root){
+        return NULL;
+    }
+    struct Node* l=root->left,*r=root->right,*n=root->next;
+    if(l){
+        l->next=r;
+        if(n){
+            r->next=n->left;
         }
-        return root;
+        connect(root->left);
+        connect(root->right);
+    }
+    return root;
+}
+
+//bfs
+struct Node* connect(struct Node* root) {
+    struct Node* head=root;
+    for(;root;root=root->left){
+        for(struct Node* cur=root;cur;cur=cur->next){
+            if(cur->left){
+                cur->left->next=cur->right;
+                if(cur->next){
+                    cur->right->next=cur->next->left;
+                }
+            }
+        }
+    }
+    return head;
 }
