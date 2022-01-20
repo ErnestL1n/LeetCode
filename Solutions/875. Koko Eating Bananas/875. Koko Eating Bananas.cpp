72cd1aee610dbe8104475a65de832f62a244db15
ErnestL1n <ernestlin1998@gmail.com>
2022-01-20 23:08:11 +0800

    Create 875. Koko Eating Bananas.cpp

---
+++ Solutions/875. Koko Eating Bananas/875. Koko Eating Bananas.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int lo=1,hi=1e9;
+        while(lo<hi){
+            int mid=lo+hi>>1,sum=0;
+            for(int i=0;i<piles.size();++i){
+                if(mid>piles[i]){
+                    sum+=1;
+                }else{
+                    if(piles[i]%mid==0){
+                        sum+=piles[i]/mid;
+                    }else{
+                        sum+=1+piles[i]/mid;
+                    }
+                }
+            }
+            if(sum>h){
+                lo=mid+1;
+            }else{
+                hi=mid;
+            }
+        }
+        return lo;
+    }
+};
