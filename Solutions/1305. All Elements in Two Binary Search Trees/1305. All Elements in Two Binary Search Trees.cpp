59faa713a43f4caece19e8bd581bcb9f2db44c1d
ErnestL1n <ernestlin1998@gmail.com>
2022-01-31 13:46:00 +0800

    Update 1305. All Elements in Two Binary Search Trees.cpp

--- Solutions/1305. All Elements in Two Binary Search Trees/1305. All Elements in Two Binary Search Trees.cpp
+++ Solutions/1305. All Elements in Two Binary Search Trees/1305. All Elements in Two Binary Search Trees.cpp
@@ -132,40 +132,31 @@ int main() {
 
 
 //update add new implementation,credit to https://leetcode.com/votrubac/
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- 
- 
 class Solution {
 public:
-    void pushleft(stack<TreeNode*>& s,TreeNode* n){
-        while(n!=nullptr)
-            s.push(exchange(n,n->left));
+    void push_left(stack<TreeNode*>& stk,TreeNode* n){
+        while(n){
+            stk.push(exchange(n,n->left));
+        }
+        // while (n != nullptr){
+		    // s.push(n);
+		    // n = n->left;
+		// }
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
         vector<int> res;
         stack<TreeNode*> s1,s2;
-        pushleft(s1,root1);
-        pushleft(s2,root2);
-        while(!s1.empty()||!s2.empty()){
-            stack<TreeNode*>& s=s1.empty()?s2:s2.empty()?s1:s1.top()->val<s2.top()->val?s1:s2;
-            auto n=s.top();s.pop();
-            res.push_back(n->val);
-            if(n->right!=nullptr)
-                pushleft(s,n->right);
+        push_left(s1,root1);
+        push_left(s2,root2);
+        while(s1.size() or s2.size()){
+            auto& s=s1.empty()?s2:s2.empty()?s1:s1.top()->val<s2.top()->val?s1:s2;
+            auto x=s.top();s.pop();
+            res.push_back(x->val);
+            push_left(s,x->right);
         }
         return res;
     }
 };
-*/
 
 
 
