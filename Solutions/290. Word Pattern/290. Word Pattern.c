3a5d6e0785426125f087fffae0c741418d461847
ErnestL1n <ernestlin1998@gmail.com>
2022-02-04 11:40:48 +0800

    Update 290. Word Pattern.c

--- Solutions/290. Word Pattern/290. Word Pattern.c
+++ Solutions/290. Word Pattern/290. Word Pattern.c
@@ -50,80 +50,91 @@ bool wordPattern(char* pattern, char* str) {
 
 
 
-/* wrong version */
-struct psmap {
-  char key;
+/* using uthash */
+typedef struct {
+  char key[2];
   char* val;
   UT_hash_handle hh;
-};
+}psmap;
 
-void deletemap(struct psmap** table) {
-  struct psmap* cur;
-  struct psmap* tmp;
+void deletemap(psmap** table) {
+  psmap* cur;
+  psmap* tmp;
   
-  HASH_ITER(hh, *table, cur, tmp) {
-    HASH_DEL(*table, cur);
+  HASH_ITER(hh,*table,cur,tmp) {
+    HASH_DEL(*table,cur);
     free(cur);
   }
 }
 
-struct spmap {
+typedef struct {
   char* key;
-  char val;
+  char val[2];
   UT_hash_handle hh;
-};
+}spmap;
 
-void deletemap2(struct spmap** table) {
-  struct spmap* cur;
-  struct spmap* tmp;
+void deletemap2(spmap** table) {
+  spmap* cur;
+  spmap* tmp;
   
-  HASH_ITER(hh, *table, cur, tmp) {
-    HASH_DEL(*table, cur);
+  HASH_ITER(hh,*table,cur,tmp) {
+    HASH_DEL(*table,cur);
     free(cur);
   }
 }
 
 char *append(char* str,char c){
-    size_t len = strlen(str);
-   
-    /* one for extra char, one for trailing zero */
+    size_t len=strlen(str);
     char *str2=(char*)malloc(sizeof(char)*(len+2));
-
-    strcpy(str2, str);
-    str2[len] = c;
-    str2[len + 1] = '\0';
-
+    strcpy(str2,str);
+    str2[len]=c;
+    str2[len+1]='\0';
     return str2;
-
 }
 
+
 bool wordPattern(char * pattern, char * ss){
-    struct psmap* ps=NULL,*p;
-    struct spmap* sp=NULL,*s;
+    psmap* ps=NULL,*p;
+    spmap* sp=NULL,*s;
     int sz=0;
     char *sss[strlen(pattern)];
-    char* tmp="";
-        for(int i=0;i<strlen(ss);++i){
-            if(ss[i]==' '){
-                sss[sz++]=tmp;
-                tmp="";
-                continue;
-            }
-            tmp=append(tmp,ss[i]);
-            if(i==strlen(ss)-1)
-                sss[sz++]=tmp;
+    char *tmp="";
+    for(int i=0;i<strlen(ss);++i){
+        if(ss[i]==' '){
+            sss[sz++]=tmp;
+            tmp="";
+            continue;
         }
-
-
+        tmp=append(tmp,ss[i]);
+        if(i==strlen(ss)-1){
+            if(sz>=strlen(pattern)){
+                return false;
+            }
+            sss[sz++]=tmp;
+        }
+    }
+    
+    if(sz!=strlen(pattern)){
+        return false;
+    }
+    
     for(int i=0;i<sz;++i){
-        HASH_FIND_INT(ps,&pattern[i],p);
+        char *tmp=(char *)malloc(2);
+        tmp[0]=pattern[i];
+        tmp[1]='\0';
+        HASH_FIND_STR(ps,tmp,p);
         if(!p){
-            p=(struct map*)malloc(sizeof(*p));
-            p->val=pattern[i];
-            p->key=i;
-            HASH_ADD_INT(ps,val,p);
+            p=(psmap*)malloc(sizeof(psmap));
+            char x[2];
+            x[0]=pattern[i];
+            x[1]='\0';
+            strcpy(p->key,x); 
+            p->val=sss[i];
+            HASH_ADD_STR(ps,key,p);
         }else{
-            if(p->val!=*sss[i]){
+            printf(p->key);
+            printf(p->val);
+            if(strcmp(p->val,sss[i])!=0){
                 deletemap(&ps);
                 deletemap2(&sp);
                 return false;
@@ -131,14 +142,25 @@ bool wordPattern(char * pattern, char * ss){
         }
     }
     for(int i=0;i<sz;++i){
-        HASH_FIND_INT(sp,&sss[i],s);
+        HASH_FIND_STR(sp,sss[i],s);
         if(!s){
-            s=(struct map*)malloc(sizeof(*s));
-            s->val=sss[i];
-            s->key=i;
-            HASH_ADD_INT(sp,val,s);
+            s=(spmap*)malloc(sizeof(spmap));
+            s->key=sss[i];
+            char x[2];
+            x[0]=pattern[i];
+            x[1]='\0';
+            strcpy(s->val,x);
+            // printf(s->key);
+            // printf(s->val);
+            HASH_ADD_KEYPTR(hh,sp,s->key,strlen(s->key),s);
         }else{
-            if(s->val!=pattern[i]){
+            // printf("ok");
+            // printf("%s\n", s->key);
+            // printf("%s\n", s->val);
+            char *tmp=(char *)malloc(2);
+            tmp[0]=pattern[i];
+            tmp[1]='\0';
+            if(strcmp(s->val,tmp)!=0){
                 deletemap(&ps);
                 deletemap2(&sp);
                 return false;
