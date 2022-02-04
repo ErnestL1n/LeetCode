4a052a74c80157917c93fa68d837ff5ab66a5f74
ErnestL1n <ernestlin1998@gmail.com>
2022-02-04 11:39:26 +0800

    Create 525. Contiguous Array.cpp

---
+++ Solutions/525. Contiguous Array/525. Contiguous Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int cnt=0,res=0,n=nums.size();
+        unordered_map<int,int> m;
+        m[0]=0;
+        for(int i=0;i<n;++i){
+            if(nums[i]==0){
+                --cnt;
+            }else{
+                ++cnt;
+            }
+            if(m.find(cnt)!=m.end()){
+                res=max(res,i+1-m[cnt]);
+            }else{
+                m[cnt]=i+1;
+            }
+        }
+        return res;
+    }
+};
