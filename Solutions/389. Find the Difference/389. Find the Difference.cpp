7d581cf545e7380f3dc22f1f72662d31e6b393e5
ErnestL1n <ernestlin1998@gmail.com>
2022-02-07 09:52:15 +0800

    Update 389. Find the Difference.cpp

--- Solutions/389. Find the Difference/389. Find the Difference.cpp
+++ Solutions/389. Find the Difference/389. Find the Difference.cpp
@@ -1,3 +1,4 @@
+//xor
 class Solution {
 public:
     char findTheDifference(string s, string t) {
@@ -7,5 +8,26 @@ public:
             c^=t[i];
         }
         return c;
+    }
+};
+
+//count
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int cnt1[26]={},cnt2[26]={};
+        for(int i=0;i<t.size();++i){
+            if(i!=s.size()){
+                ++cnt1[s[i]-'a'];
+            }
+            ++cnt2[t[i]-'a'];
+        }
+        int res;
+        for(res=0;res<26;++res){
+            if(cnt2[res]-cnt1[res]==1){
+                break;
+            }
+        }
+        return res+'a';
     }
 };
