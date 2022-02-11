e46240f4acd526c426a2145d4ec7bf4913e027e2
ErnestL1n <ernestlin1998@gmail.com>
2022-02-11 23:44:40 +0800

    Update 567. Permutation in String.cpp

--- Solutions/567. Permutation in String/567. Permutation in String.cpp
+++ Solutions/567. Permutation in String/567. Permutation in String.cpp
@@ -1,3 +1,35 @@
+//original writing AC
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int sz=s1.size();
+        vector<int> w1=cnt(s1);
+        for(int i=0,j=i+sz-1;j<s2.size();++i,++j){
+            string x=s2.substr(i,sz);
+            vector<int> tmp=cnt(x);
+            if(check(w1,tmp)){
+                return true;
+            }
+        }
+        return false;
+    }
+    vector<int> cnt(string& x){
+        vector<int> win(26);
+        for(int i=0;i<x.size();++i){
+            ++win[x[i]-'a'];
+        }
+        return win;
+    }
+    bool check(vector<int>& a,vector<int>& b){
+        for(int i=0;i<26;++i){
+            if(a[i]!=b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
