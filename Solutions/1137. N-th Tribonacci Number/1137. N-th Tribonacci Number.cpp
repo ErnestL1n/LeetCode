a5fc957144e308c7d40c28046beb5e5a15a15ad5
ErnestL1n <ernestlin1998@gmail.com>
2022-02-14 23:54:10 +0800

    Update 1137. N-th Tribonacci Number.cpp

--- Solutions/1137. N-th Tribonacci Number/1137. N-th Tribonacci Number.cpp
+++ Solutions/1137. N-th Tribonacci Number/1137. N-th Tribonacci Number.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        int a=0,b=1,c=1,d;
-        if(n<=2)
-            return n==0?a:n==1?b:c;
-        for(int i=0;i<=n-3;++i){
-            d=a+b+c;
-            a=b;
-            b=c;
-            c=d;
+        if(n==0){return 0;}
+        if(n==1){return 1;}
+        if(n==2){return 1;}
+        int a=0,b=1,c=1;
+        for(int i=3;i<=n;++i){
+            int t1=c,t2=b;
+            c=a+b+c;
+            b=t1;
+            a=t2;
         }
-        return d;
+        return c;
     }
 };
