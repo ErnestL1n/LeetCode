0d07535cf9872676b8cc52e5e8049135e16fbf02
ErnestL1n <ernestlin1998@gmail.com>
2022-02-15 23:55:50 +0800

    Update 740. Delete and Earn.cpp

--- Solutions/740. Delete and Earn/740. Delete and Earn.cpp
+++ Solutions/740. Delete and Earn/740. Delete and Earn.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        vector<int> s(10002);
-        for(int i=0;i<nums.size();++i)
-            s[nums[i]]+=nums[i];
-        for(int i=2;i<s.size();++i){
-            s[i]=max(s[i-1],s[i-2]+s[i]);
+        int r=*max_element(nums.begin(),nums.end());
+        vector<int> arr(r+1);
+        for(auto& n:nums){
+            arr[n]+=n;
         }
-        return s[10001];
+        return rob(arr);
+    }
+    int rob(vector<int>& arr){
+        int n=arr.size();
+        vector<int> dp(n);
+        dp[0]=arr[0],dp[1]=max(arr[0],arr[1]);
+        for(int i=2;i<n;++i){
+            dp[i]=max(dp[i-1],dp[i-2]+arr[i]);
+        }
+        return dp[n-1];
     }
 };
