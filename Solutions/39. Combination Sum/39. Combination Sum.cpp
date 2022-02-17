6e49ca304a58ac03627eaf53b45857c6be8752cd
ErnestL1n <ernestlin1998@gmail.com>
2022-02-17 23:54:13 +0800

    Update 39. Combination Sum.cpp

--- Solutions/39. Combination Sum/39. Combination Sum.cpp
+++ Solutions/39. Combination Sum/39. Combination Sum.cpp
@@ -1,26 +1,24 @@
-typedef vector<vector<int>> VVI;
-typedef vector<int> VI;
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        VVI res;
-        VI path;
-        foo(res,path,candidates,target,0);
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        vector<int> tmp;
+        sort(nums.begin(),nums.end());
+        dfs(res,tmp,nums,target,0);
         return res;
     }
-    void foo(VVI& res,VI& path,VI& c,int t,int begin){
-        if(t<=0){
-            if(t==0){
-                res.push_back(path);
-                return;
-            }
+    void dfs(vector<vector<int>>& res,vector<int>& tmp,vector<int>& nums,int target,int st){
+        if(target<0){
             return;
         }
-        for(int i=begin;i<c.size();++i){
-            path.push_back(c[i]);
-            foo(res,path,c,t-c[i],i);
-            path.pop_back();
+        if(target==0){
+            res.push_back(tmp);
+            return;
         }
-        return;
+        for(int i=st;i<nums.size();++i){
+            tmp.push_back(nums[i]);
+            dfs(res,tmp,nums,target-nums[i],i);
+            tmp.pop_back();
+        }
     }
 };
