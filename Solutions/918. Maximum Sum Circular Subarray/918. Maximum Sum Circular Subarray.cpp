901b251bbd7ba5b281ff17e2f479373c8bddb9f2
ErnestL1n <ernestlin1998@gmail.com>
2022-02-18 00:35:43 +0800

    Update 918. Maximum Sum Circular Subarray.cpp

--- Solutions/918. Maximum Sum Circular Subarray/918. Maximum Sum Circular Subarray.cpp
+++ Solutions/918. Maximum Sum Circular Subarray/918. Maximum Sum Circular Subarray.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int total=0,maxSum=nums[0],curMax=0,minSum=nums[0],curMin=0;
-        for (int& n:nums) {
-            curMax=max(curMax+n,n);
-            maxSum=max(maxSum,curMax);
-            curMin=min(curMin+n,n);
-            minSum=min(minSum,curMin);
-            total+=n;
+        int n=nums.size(),sum=0;
+        int curmx=INT_MIN,curmn=INT_MAX;
+        int allmx=INT_MIN,allmn=INT_MAX;
+        for(int i=0;i<n;++i){
+            sum+=nums[i];
+            curmx=i>0 and curmx>0?curmx+nums[i]:nums[i];
+            curmn=i>0 and curmn<0?curmn+nums[i]:nums[i];
+            allmx=max(allmx,curmx);
+            allmn=min(allmn,curmn);
         }
-        return maxSum>0?max(maxSum,total-minSum):maxSum;
+        return allmx>0?max(allmx,sum-allmn):allmx;
     }
 };
