1babcc44a1e3ab3f7546dd61476126f2fd062d65
ErnestL1n <ernestlin1998@gmail.com>
2022-02-21 21:33:40 +0800

    Create 169. Majority Element.cpp

---
+++ Solutions/169. Majority Element/169. Majority Element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
+    }
+};
+
+//trick
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int major=nums[0],cnt=1;
+        for(int i=1;i<nums.size();++i){
+            if(cnt==0){
+                ++cnt;
+                major=nums[i];
+            }else if(major==nums[i]){
+                ++cnt;
+            }else{
+                --cnt;
+            }
+        }
+        return major;
+    }
+};
