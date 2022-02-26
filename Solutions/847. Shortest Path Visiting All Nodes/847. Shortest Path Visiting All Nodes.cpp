0c60684f61b2d3e9d11caf80b13bcd72531a2548
ErnestL1n <ernestlin1998@gmail.com>
2022-02-26 23:55:38 +0800

    Create 847. Shortest Path Visiting All Nodes.cpp

---
+++ Solutions/847. Shortest Path Visiting All Nodes/847. Shortest Path Visiting All Nodes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  int shortestPathLength(vector<vector<int>>& graph) {
+    const int kAns = (1 << (graph.size())) - 1;    
+    queue<pair<int, int>> q;
+    unordered_set<int> visited; // (cur_node << 16) | state
+    for (int i = 0; i < graph.size(); ++i)
+      q.push({i, 1 << i});
+    int steps = 0;
+    while (!q.empty()) {
+      int s = q.size();      
+      while (s--) {
+        auto p = q.front(); 
+        q.pop();
+        int n = p.first;
+        int state = p.second;
+        if (state == kAns) return steps;
+        int key = (n << 16) | state;
+        if (visited.count(key)) continue;
+        visited.insert(key);
+        for (int next : graph[n])
+          q.push({next, state | (1 << next)});
+      }
+      ++steps;
+    }
+    return -1;
+  }
+};
