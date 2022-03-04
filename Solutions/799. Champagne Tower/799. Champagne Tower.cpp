e2ed201311b934f6592717542625003f842f9533
ErnestL1n <ernestlin1998@gmail.com>
2022-03-04 23:11:38 +0800

    Create 799. Champagne Tower.cpp

---
+++ Solutions/799. Champagne Tower/799. Champagne Tower.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> res(101,vector<double>(101));
+        res[0][0]=poured;
+        for(int i=0;i<100;++i){
+            for(int j=0;j<=i;++j){
+                if(res[i][j]>=1){
+                    res[i+1][j]+=(res[i][j]-1)/2.0;
+                    res[i+1][j+1]+=(res[i][j]-1)/2.0;
+                    res[i][j]=1;
+                }
+            }
+        }
+        return res[query_row][query_glass];
+    }
+};
+
+// time space opt
