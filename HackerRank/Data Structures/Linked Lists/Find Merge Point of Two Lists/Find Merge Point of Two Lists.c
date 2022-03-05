191c3f3b81f7ba4b3d7c8a2b5bc1d380710dc250
ErnestL1n <ernestlin1998@gmail.com>
2022-03-05 11:28:24 +0800

    u

--- HackerRank/Data Structures/Linked Lists/Find Merge Point of Two Lists/Find Merge Point of Two Lists.c
+++
@@ -1,8 +0,0 @@
-int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
-    SinglyLinkedListNode* a=head1,*b=head2;
-    while(a!=b){
-        a=a?a->next:head2;
-        b=b?b->next:head1;
-    }
-    return a->data;
-}
