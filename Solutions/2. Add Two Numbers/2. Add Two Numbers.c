b1989698575faa5a3fcc794be71d26d3d37f626e
ErnestL1n <ernestlin1998@gmail.com>
2022-03-10 23:39:09 +0800

    Update 2. Add Two Numbers.c

--- Solutions/2. Add Two Numbers/2. Add Two Numbers.c
+++ Solutions/2. Add Two Numbers/2. Add Two Numbers.c
@@ -8,22 +8,25 @@
 
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
-    struct ListNode* prehead=(struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* prehead=malloc(sizeof(struct ListNode));
+    prehead->val=0;
+    prehead->next=NULL;
     struct ListNode* p=prehead;
     int extra=0;
-    while(l1!=NULL||l2!=NULL||extra!=0){
-        if(l1!=NULL){
+    while(l1 || l2 || extra){
+        if(l1){
             extra+=l1->val;
             l1=l1->next;
         }
-        if(l2!=NULL){
+        if(l2){
             extra+=l2->val;
             l2=l2->next;
         }
-        p->next=(struct ListNode*)malloc(sizeof(struct ListNode));
+        p->next=malloc(sizeof(struct ListNode));
         p->next->val=extra%10;
-        extra/=10;
+        p->next->next=NULL;
         p=p->next;
+        extra/=10;
     }
     p->next=NULL;
     return prehead->next;
