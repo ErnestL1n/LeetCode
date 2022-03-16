289903bf1dd8edd186c4179600a2b69937546a6a
ErnestL1n <ernestlin1998@gmail.com>
2022-03-16 23:51:39 +0800

    Update 946. Validate Stack Sequences.cpp

--- Solutions/946. Validate Stack Sequences/946. Validate Stack Sequences.cpp
+++ Solutions/946. Validate Stack Sequences/946. Validate Stack Sequences.cpp
@@ -1,3 +1,4 @@
+// stack time: O(N),space: O(N)
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
@@ -11,5 +12,20 @@ public:
             }
         }
         return stk.size()==0;
+    }
+};
+
+// use original array: time: O(N),space: O(1)
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int i=0,j=0;
+        for(auto& p:pushed){
+            pushed[i++]=p;
+            while(i>0 and pushed[i-1]==popped[j]){
+                --i,++j;
+            }
+        }
+        return i==0;
     }
 };
