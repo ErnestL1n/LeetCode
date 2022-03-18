ed1faebd44b1582504fb8b36a0271964363be6e2
ErnestL1n <ernestlin1998@gmail.com>
2022-03-18 21:20:35 +0800

    Update 1249. Minimum Remove to Make Valid Parentheses.c

--- Solutions/1249. Minimum Remove to Make Valid Parentheses/1249. Minimum Remove to Make Valid Parentheses.c
+++ Solutions/1249. Minimum Remove to Make Valid Parentheses/1249. Minimum Remove to Make Valid Parentheses.c
@@ -1,4 +1,4 @@
-// MLE
+// MLE realloc
 struct Stack {
   struct Stack *next, *top;
   int val;
@@ -73,4 +73,81 @@ char *minRemoveToMakeValid(char * s){
     res=(char*)realloc(res,j+1);
     res[j]='\0';
     return res;
+}
+
+
+
+
+// use original string
+struct Stack {
+  struct Stack *next, *top;
+  int val;
+  int size;
+};
+
+struct Stack* newnode(int k) {
+  struct Stack* node = (struct Stack*)malloc(sizeof(struct Stack));
+  node->val = k;
+  node->next = NULL;
+  return node;
+}
+
+struct Stack* createstack() {
+  struct Stack* stk = (struct Stack*)malloc(sizeof(struct Stack));
+  stk->top = NULL;
+  stk->size = 0;
+  return stk;
+}
+
+int empty(struct Stack* stk) { return !stk->top; }
+
+void push(struct Stack* stk, int val) {
+  struct Stack* node = newnode(val);
+  ++stk->size;
+  if (stk->top == NULL) {
+    stk->top = node;
+    return;
+  }
+  node->next = stk->top;
+  stk->top = node;
+}
+
+void pop(struct Stack* stk) {
+  if (!stk->top) {
+    printf("stk is empty!!\n");
+    return;
+  }
+  --stk->size;
+  struct Stack* tmp = stk->top;
+  stk->top = stk->top->next;
+  free(tmp);
+}
+
+char *minRemoveToMakeValid(char * s){
+    struct Stack* stk = createstack();
+    for(int i=0;i<strlen(s);++i){
+        if(s[i]=='('){
+            push(stk,i);
+        }
+        if(s[i]==')'){
+            if(stk->size){
+                pop(stk);
+            }else{
+                *(s+i)='#';
+            }
+        }
+    }
+    while(stk->size){
+        *(s+stk->top->val)='#';
+        pop(stk);
+    }
+    int i,j;
+    for(i=0,j=0;i<strlen(s);++i){
+        if(s[i]=='#'){
+            continue;
+        }
+        s[j++]=s[i];
+    }
+    s[j]='\0';
+    return s;
 }
