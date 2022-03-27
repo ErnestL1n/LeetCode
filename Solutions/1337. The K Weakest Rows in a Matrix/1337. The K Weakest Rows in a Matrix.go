f0451402ad9474a0cfb6088f2af9d8bd638137e8
ErnestL1n <ernestlin1998@gmail.com>
2022-03-27 12:25:14 +0800

    Create 1337. The K Weakest Rows in a Matrix.go

---
+++ Solutions/1337. The K Weakest Rows in a Matrix/1337. The K Weakest Rows in a Matrix.go
@@ -0,0 +1,30 @@
+// sort with 2D slice
+func kWeakestRows(mat [][]int, k int) []int {
+    tmp:=make([][]int,len(mat))
+    for i,x:=range(mat){
+        tmp[i]=make([]int,2)
+        for _,y:=range(x){
+            if y==1{
+                tmp[i][0]+=1
+            }
+        }
+        tmp[i][1]=i
+    }
+    var res []int
+    sort.Slice(tmp,func(i,j int)bool{
+        if len(tmp[i])==0 && len(tmp[j])==0{
+            return false
+        }
+        if len(tmp[i])==0 || len(tmp[j])==0{
+            return len(tmp[i])==0
+        }
+        if tmp[i][0]==tmp[j][0]{
+            return tmp[i][1]<tmp[j][1]
+        }
+        return tmp[i][0]<tmp[j][0]
+    })
+    for i:=0;k>0;i,k=i+1,k-1{
+        res=append(res,tmp[i][1])
+    }
+    return res
+}
