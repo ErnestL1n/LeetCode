34cf7f3b158d6beca6663d5643b27020e91d85fd
ErnestL1n <ernestlin1998@gmail.com>
2022-03-30 23:24:00 +0800

    Update 74. Search a 2D Matrix.go

--- Solutions/74. Search a 2D Matrix/74. Search a 2D Matrix.go
+++ Solutions/74. Search a 2D Matrix/74. Search a 2D Matrix.go
@@ -1,15 +1,15 @@
 func searchMatrix(mat [][]int, target int) bool {
     m,n:=len(mat),len(mat[0])
-    l,r:=0,m*n-1
-    for l<=r{
-        mid:=(l+r)/2
+    lo,hi:=0,m*n-1
+    for lo<=hi{
+        mid:=(lo+hi)/2
         if mat[mid/n][mid%n]==target{
             return true
         }
         if mat[mid/n][mid%n]<target{
-            l=mid+1
+            lo=mid+1
         }else{
-            r=mid-1
+            hi=mid-1
         }
     }
     return false
