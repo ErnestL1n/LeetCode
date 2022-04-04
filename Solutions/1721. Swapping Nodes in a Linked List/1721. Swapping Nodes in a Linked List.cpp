2e4f8f6e846cd9c1237c3da0ffc4cbbf69f71a70
ErnestL1n <ernestlin1998@gmail.com>
2022-04-04 10:53:22 +0800

    Update 1721. Swapping Nodes in a Linked List.cpp

--- Solutions/1721. Swapping Nodes in a Linked List/1721. Swapping Nodes in a Linked List.cpp
+++ Solutions/1721. Swapping Nodes in a Linked List/1721. Swapping Nodes in a Linked List.cpp
@@ -1,4 +1,3 @@
-// beginner friendly
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -12,41 +11,9 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* tmp=head,*r;
-        int size=0;
-        while(tmp){
-            ++size;
-            tmp=tmp->next;
-        }
-        int begin=k,end=size-begin+1;
-        if(begin>end)
-            swap(begin,end);
-        tmp=head;
-        int s=1,q;
-        while(s<=end){
-            if(s==begin){
-                q=tmp->val;
-                r=tmp;
-            }
-            if(s==end){
-                r->val=tmp->val;
-                tmp->val=q;
-            }
-            tmp=tmp->next;
-            ++s;
-        }
-        return head;
-    }
-};
-
-
-//trick
-class Solution {
-public:
-    ListNode* swapNodes(ListNode* head, int k) {
-        ListNode *p1=nullptr,*p2=nullptr;
-        for(auto p=head;p!=nullptr;p=p->next){
-            p2=p2==nullptr?nullptr:p2->next;
+        ListNode* p1=nullptr,*p2=nullptr;
+        for(auto p=head;p;p=p->next){
+            p2=p2?p2->next:nullptr;
             if(--k==0){
                 p1=p;
                 p2=head;
