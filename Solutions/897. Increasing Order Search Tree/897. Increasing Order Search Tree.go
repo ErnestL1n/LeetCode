0ff4eb04060c87cc80015e750d5977002ae855ee
ErnestL1n <ernestlin1998@gmail.com>
2022-04-17 16:03:57 +0800

    Update 897. Increasing Order Search Tree.go

--- Solutions/897. Increasing Order Search Tree/897. Increasing Order Search Tree.go
+++ Solutions/897. Increasing Order Search Tree/897. Increasing Order Search Tree.go
@@ -6,6 +6,7 @@
  *     Right *TreeNode
  * }
  */
+ //recursive
 func increasingBST(root *TreeNode) *TreeNode {
     return increasing(root,nil)
 }
@@ -18,4 +19,26 @@ func increasing(cur,tail *TreeNode) *TreeNode {
     cur.Left=nil
     cur.Right=increasing(cur.Right,tail)
     return res
+}
+
+
+//iterative
+func increasingBST(root *TreeNode) *TreeNode {
+    var stk []*TreeNode
+    dummyleft:=new(TreeNode)
+    dummy:=dummyleft
+    for root!=nil || len(stk)>0{
+        if root!=nil{
+            stk=append(stk,root)
+            root=root.Left
+        }else{
+            cur:=stk[len(stk)-1]
+            stk=stk[:len(stk)-1]
+            root=cur.Right
+            cur.Left=nil
+            dummy.Right=cur
+            dummy=dummy.Right
+        }
+    }
+    return dummyleft.Right
 }
