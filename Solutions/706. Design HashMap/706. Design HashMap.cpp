05daa0d296cf6441623fcf7162d4ad0be98f9796
ErnestL1n <ernestlin1998@gmail.com>
2022-04-23 16:42:05 +0800

    Update 706. Design HashMap.cpp

--- Solutions/706. Design HashMap/706. Design HashMap.cpp
+++ Solutions/706. Design HashMap/706. Design HashMap.cpp
@@ -1,33 +1,26 @@
-//simple solution ,c base ,so in c++ it is faster
-#define SIZE 10000
 class MyHashMap {
 public:
-    int hash(int x){
-        return x%SIZE;
-    }
+    int SIZE=1000001;
     int* table;
-    MyHashMap* obj;
-    /** Initialize your data structure here. */
+    MyHashMap *hashmap;
     MyHashMap() {
-        obj=(MyHashMap*)malloc(sizeof(MyHashMap)*SIZE);
-        obj->table=(int*)malloc(sizeof(int)*SIZE);
-        for(int i=0;i<SIZE;++i)
-            obj->table[i]=-1;
+        hashmap=(MyHashMap*)malloc(sizeof(MyHashMap));
+        hashmap->table=(int*)malloc(sizeof(int)*SIZE);
+        for(int i=0;i<SIZE;++i){
+            hashmap->table[i]=-1;
+        }
     }
     
-    /** value will always be non-negative. */
     void put(int key, int value) {
-        obj->table[hash(key)]=value;
+        this->hashmap->table[key]=value;
     }
     
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-        return obj->table[key%SIZE];
+        return this->hashmap->table[key];
     }
     
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
-        obj->table[key%SIZE]=-1;
+        this->hashmap->table[key]=-1;
     }
 };
 
@@ -38,4 +31,3 @@ public:
  * int param_2 = obj->get(key);
  * obj->remove(key);
  */
- 
