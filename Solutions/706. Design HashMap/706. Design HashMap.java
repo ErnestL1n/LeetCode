04a4dbbd4fb45587a1b2a7960945108e2115554a
ErnestL1n <ernestlin1998@gmail.com>
2022-04-23 16:53:48 +0800

    Update 706. Design HashMap.java

--- Solutions/706. Design HashMap/706. Design HashMap.java
+++ Solutions/706. Design HashMap/706. Design HashMap.java
@@ -1,28 +1,20 @@
 class MyHashMap {
-    public int hash(int x){
-        return x%100000;
-    }
-    private int[] table;
-    /** Initialize your data structure here. */
+    int[] table;
     public MyHashMap() {
-        table=new int[100000];
-        for(int i=0;i<100000;++i)
-            table[i]=-1;
+        table=new int[1000001];
+        Arrays.fill(table,-1);
     }
     
-    /** value will always be non-negative. */
     public void put(int key, int value) {
-        table[hash(key)]=value;
+        table[key]=value;
     }
     
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
-        return table[key%100000];
+        return table[key];   
     }
     
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
-        table[key%100000]=-1;
+        table[key]=-1;
     }
 }
 
