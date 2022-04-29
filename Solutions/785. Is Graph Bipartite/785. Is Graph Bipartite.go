4e1b016348279fad58ff5ea175cf39a252355105
ErnestL1n <ernestlin1998@gmail.com>
2022-04-29 23:06:44 +0800

    Update 785. Is Graph Bipartite.go

--- Solutions/785. Is Graph Bipartite/785. Is Graph Bipartite.go
+++ Solutions/785. Is Graph Bipartite/785. Is Graph Bipartite.go
@@ -1,4 +1,28 @@
-
+//bfs
+func isBipartite(graph [][]int) bool {
+    n:=len(graph)
+    colors:=make([]int,n)
+    for i:=0;i<n;i+=1{
+        if colors[i]!=0{
+            continue
+        }
+        colors[i]=1
+        var q []int
+        q=append(q,i)
+        for len(q)>0{
+            cur:=q[0];q=q[1:]
+            for _,x:=range graph[cur]{
+                if colors[x]==0{
+                    colors[x]=-colors[cur]
+                    q=append(q,x)
+                }else if colors[x]!=-colors[cur]{
+                    return false
+                }
+            }
+        }
+    }
+    return true
+}
 
 //dfs
 func isBipartite(graph [][]int) bool {
