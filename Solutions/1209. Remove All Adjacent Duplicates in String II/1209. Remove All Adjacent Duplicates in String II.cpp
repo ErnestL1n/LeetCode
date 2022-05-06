e6d3fc40cec9dcb88ccbe1d19272c327aef5a351
ErnestL1n <ernestlin1998@gmail.com>
2022-05-06 11:01:13 +0800

    Update 1209. Remove All Adjacent Duplicates in String II.cpp

--- Solutions/1209. Remove All Adjacent Duplicates in String II/1209. Remove All Adjacent Duplicates in String II.cpp
+++ Solutions/1209. Remove All Adjacent Duplicates in String II/1209. Remove All Adjacent Duplicates in String II.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        int i = 0, n = s.length();
-        vector<int> count(n);
-        for (int j = 0; j < n; ++j, ++i) {
-            s[i] = s[j];
-            count[i] = i > 0 && s[i - 1] == s[j] ? count[i - 1] + 1 : 1;
-            if (count[i] == k) i -= k;
+        int i=0,n=s.size();
+        vector<int> cnt(n);
+        for(int j=0;j<n;++i,++j){
+            s[i]=s[j];
+            cnt[i]=i>0 and s[i-1]==s[j]?cnt[i-1]+1:1;
+            if(cnt[i]==k){
+                i-=k;
+            }
         }
-        return s.substr(0, i);
+        return s.substr(0,i);
     }
 };
