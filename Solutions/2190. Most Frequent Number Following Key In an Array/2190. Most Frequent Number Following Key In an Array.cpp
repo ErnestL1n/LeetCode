210db194725d5d147c3d1502466dccdb8104c998
ErnestL1n <ernestlin1998@gmail.com>
2022-05-12 23:49:03 +0800

    Create 2190. Most Frequent Number Following Key In an Array.cpp

---
+++ Solutions/2190. Most Frequent Number Following Key In an Array/2190. Most Frequent Number Following Key In an Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mostFrequent(vector<int>& nums, int key) {
+        int cnt[1001]={},res=0;
+        for(int i=1;i<nums.size();++i){
+            if(nums[i-1]==key and ++cnt[nums[i]]>cnt[res]){
+                res=nums[i];
+            }
+        }
+        return res;
+    }
+};
