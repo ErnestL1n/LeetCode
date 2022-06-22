7cc8298212f7de3e9a967c9579928b019259741b
ErnestL1n <ernestlin1998@gmail.com>
2022-06-22 22:52:26 +0800

    Create 2309. Greatest English Letter in Upper and Lower Case.cpp

---
+++ Solutions/2309. Greatest English Letter in Upper and Lower Case/2309. Greatest English Letter in Upper and Lower Case.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string greatestLetter(string s) {
+        int cnt[128]={};
+        for(auto ch:s){
+            ++cnt[ch];
+        }
+        for(auto ch='Z';ch>='A';--ch){
+            if(cnt[ch] and cnt[ch+'a'-'A']){
+                return string(1,ch);
+            }
+        }
+        return "";
+    }
+};
