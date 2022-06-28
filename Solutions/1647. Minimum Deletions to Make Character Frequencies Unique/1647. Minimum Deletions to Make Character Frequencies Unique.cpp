05caee1b0729569ef481dcc5e3a87e3461247673
ErnestL1n <ernestlin1998@gmail.com>
2022-06-28 10:27:00 +0800

    Create 1647. Minimum Deletions to Make Character Frequencies Unique.cpp

---
+++ Solutions/1647. Minimum Deletions to Make Character Frequencies Unique/1647. Minimum Deletions to Make Character Frequencies Unique.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        int cnt[26]={},res=0;
+        unordered_set<int> us;
+        for(auto& c:s){
+            ++cnt[c-'a'];
+        }
+        for(int i=0;i<26;++i){
+            for(;cnt[i]>0 and !us.insert(cnt[i]).second;--cnt[i]){
+                ++res;
+            }
+        }
+        return res;
+    }
+};
