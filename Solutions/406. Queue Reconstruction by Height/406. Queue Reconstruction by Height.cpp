d6de16d8c3535bd44d87b7a52da16653e89bcd9a
ErnestL1n <ernestlin1998@gmail.com>
2022-06-30 10:48:21 +0800

    Update 406. Queue Reconstruction by Height.cpp

--- Solutions/406. Queue Reconstruction by Height/406. Queue Reconstruction by Height.cpp
+++ Solutions/406. Queue Reconstruction by Height/406. Queue Reconstruction by Height.cpp
@@ -10,4 +10,15 @@ public:
         }
         return res;
     }
+};class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0,j=nums.size()-1;
+        int cnt=0;
+        while(i<j){
+            cnt+=nums[j--]-nums[i++];
+        }
+        return cnt;
+    }
 };
