9c82a8d131bc9874c0dc7d85166326e386f43d53
ErnestL1n <ernestlin1998@gmail.com>
2022-08-02 10:50:30 +0800

    Create 2350. Shortest Impossible Sequence of Rolls.cpp

---
+++ Solutions/2350. Shortest Impossible Sequence of Rolls/2350. Shortest Impossible Sequence of Rolls.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int shortestSequence(vector<int>& rolls, int k) {
+        int seq=0,cnt=0,dice[100001]={};
+        for(int& r:rolls){
+            if(dice[r]==seq){
+                dice[r]=seq+1;
+                if(++cnt%k==0){
+                    ++seq;
+                }
+            }
+        }
+        return seq+1;
+    }
+};
